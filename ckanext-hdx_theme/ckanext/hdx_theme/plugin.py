c4c556c8b4bceafbc47b9dfd72470e4b103e9083
ckan <ckan@ckan.(none)>
2014-03-04 21:38:26 -0500

    overrode home controller to list all groups

--- ckanext-hdx_theme/ckanext/hdx_theme/plugin.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/plugin.py
@@ -1,9 +1,16 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+
+
 class HDXThemePlugin(plugins.SingletonPlugin):
 	plugins.implements(plugins.IConfigurer)
+	plugins.implements(plugins.IRoutes, inherit=True)
 
 	def update_config(self, config):
 		toolkit.add_template_directory(config, 'templates')
 		toolkit.add_public_directory(config, 'public')
+
+	def before_map(self, map):
+		map.connect('home', '/', controller='ckanext.hdx_theme.splash_page:SplashPageController', action='index')
+		return map
