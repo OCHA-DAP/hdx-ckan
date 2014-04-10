ed31168ade86cc71b1551fa501383f9cb325c7fa
Alexandru Artimon <aalecs@gmail.com>
2014-04-10 13:01:43 +0300

    customized request registration page

--- ckanext-register/ckanext/register/request.py
+++ ckanext-register/ckanext/register/request.py
@@ -13,7 +13,18 @@ get_action = logic.get_action
 NotFound = logic.NotFound
 NotAuthorized = logic.NotAuthorized
 
+def send_mail(name, email, org, reason = ''):
+    body = 'New user registration request\n' \
+           'Full Name: {fn}\n' \
+           'Email: {mail}\n' \
+           'Organisation: {org}\n' \
+           'Reason: {reason}\n' \
+           '(This is an automated mail)' \
+           ''.format(fn=name, mail=email, org=org, reason=reason)
 
+    mailer.mail_recipient("Registration", "test@test.com", "New user registration", body)
+
+    return
 
 class RequestController(ckan.controllers.user.UserController):
 
@@ -27,17 +38,24 @@ class RequestController(ckan.controllers.user.UserController):
         #    abort(401, _('Unauthorized to request new registration.'))
 
         if request.method == 'POST':
+            name = request.params.get('fullname')
             email = request.params.get('email')
-            h.log.info('Email is:'+email)
+            org = request.params.get('org')
+            reason = request.params.get('reason')
 
-            if email:
+            h.log.info('Request access for {name} ({email}) of {org} with reason: {reason}'.format(name = name, email = email, org = org, reason=reason))
+
+            if name and email and org:
                 try:
-                    #mailer.send_reset_link(user_obj)
+                    send_mail(name, email, org, reason)
                     h.flash_success(_('Please check your inbox for '
                                     'a reset code.'))
                     #h.redirect_to('/')
                 except mailer.MailerException, e:
                     h.flash_error(_('Could not send reset link: %s') %
                                   unicode(e))
+            else:
+                h.flash_error(_('Please fill all the fields!'))
+                return
 
         return base.render('user/request_register.html', cache_force=True)
