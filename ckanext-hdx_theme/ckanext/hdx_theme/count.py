79fed3d44f6a974809bd2eee5c83941347114210
Alexandru Artimon <aalecs@gmail.com>
2014-04-14 16:53:20 +0300

    #149 - fix the problem with the number of groups being shown

--- ckanext-hdx_theme/ckanext/hdx_theme/count.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/count.py
@@ -5,16 +5,16 @@ import sqlalchemy
 
 class CountController(base.BaseController):
 
-    def _get_count(self, table):
-        q = sqlalchemy.text(''' select count(*) from {name} where state='active';'''.format(name=table))
+    def _get_count(self, table, type):
+        q = sqlalchemy.text(''' select count(*) from {name} where state='active' and type='{type}';'''.format(name=table, type=type))
         result = model.Session.connection().execute(q, entity_id=id).scalar()
         return result
 
     def dataset(self):
-        return json.dumps({'count': self._get_count('package')})
+        return json.dumps({'count': self._get_count('package', 'dataset')})
 
     def country(self):
-        return json.dumps({'count': self._get_count('"group"')})
+        return json.dumps({'count': self._get_count('"group"', 'group')})
 
     def source(self):
         return json.dumps({'count': 0})
