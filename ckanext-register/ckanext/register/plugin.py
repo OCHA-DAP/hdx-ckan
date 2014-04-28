498723f732e80b15333573abb37eeb8000670080
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2014-04-28 23:18:22 +0300

    changes to the login #179 #178

--- ckanext-register/ckanext/register/plugin.py
+++ ckanext-register/ckanext/register/plugin.py
@@ -8,24 +8,24 @@ def user_create(context, data_dict=None):
 
 
 class DisableUserRegistration(plugins.SingletonPlugin):
-    plugins.implements(plugins.IAuthFunctions)
+#     plugins.implements(plugins.IAuthFunctions)
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IRoutes, inherit=True)
 
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
 
-    def get_auth_functions(self):
-        return {'user_create': user_create}
+#     def get_auth_functions(self):
+#         return {'user_create': user_create}
 
     def before_map(self, map):
-        map.connect('/user/request',
-                    controller='ckanext.register.request:RequestController',
-                    action='request')
+#        Re-enabling user self-registration
+#         map.connect('/user/request',
+#                     controller='ckanext.register.request:RequestController',
+#                     action='request')
 
         map.connect('/user/register',
                     controller='ckanext.register.request:RequestController',
                     action='register')
-
         return map
 
