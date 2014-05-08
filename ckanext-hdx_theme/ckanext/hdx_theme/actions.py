8632e41de7d6e39317b45bbc3a82c4d96d403255
Dan Mihaila <danmihaila@gmail.com>
2014-05-08 17:30:16 +0300

    #228

---
+++ ckanext-hdx_theme/ckanext/hdx_theme/actions.py
@@ -0,0 +1,81 @@
+import logging
+import datetime
+
+from pylons import config
+import sqlalchemy
+
+import ckan.lib.dictization
+import ckan.logic as logic
+import ckan.lib.dictization.model_dictize as model_dictize
+import ckan.model.misc as misc
+import ckan.plugins as plugins
+import ckan.lib.plugins as lib_plugins
+import ckan.new_authz as new_authz
+
+from ckan.common import _
+
+_check_access = logic.check_access
+
+def organization_list_for_user(context, data_dict):
+    '''Return the list of organizations that the user is a member of.
+
+    :param permission: the permission the user has against the returned organizations
+      (optional, default: ``edit_group``)
+    :type permission: string
+
+    :returns: list of dictized organizations that the user is authorized to edit
+    :rtype: list of dicts
+
+    '''
+    model = context['model']
+    user = context['user']
+
+    _check_access('organization_list_for_user',context, data_dict)
+    sysadmin = new_authz.is_sysadmin(user)
+
+    orgs_q = model.Session.query(model.Group) \
+        .filter(model.Group.is_organization == True) \
+        .filter(model.Group.state == 'active')
+
+    if not sysadmin:
+        # for non-Sysadmins check they have the required permission
+
+        permission = data_dict.get('permission', 'edit_group')
+
+        roles = ckan.new_authz.get_roles_with_permission(permission)
+
+        if not roles:
+            return []
+        user_id = new_authz.get_user_id_for_username(user, allow_none=True)
+        if not user_id:
+            return []
+
+        q = model.Session.query(model.Member) \
+            .filter(model.Member.table_name == 'user') \
+            .filter(model.Member.capacity.in_(roles)) \
+            .filter(model.Member.table_id == user_id)
+
+        group_ids = []
+        for row in q.all():
+            group_ids.append(row.group_id)
+
+        if not group_ids:
+            return []
+
+        orgs_q = orgs_q.filter(model.Group.id.in_(group_ids))
+
+    orgs_list_complete = orgs_q.all()
+    orgs_list = model_dictize.group_list_dictize(orgs_list_complete, context)
+    
+#to be used in case we want to display the created field
+#    org_list_map ={}
+#    for it in orgs_list_complete:
+#        org_list_map[it.id]=it
+#    for it in orgs_list:
+#        id=it['id']
+#        org = org_list_map[id]
+#        it['created']=org.created.isoformat()
+    return orgs_list
+
+
+    
--- ckanext-hdx_theme/ckanext/hdx_theme/helpers.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers.py
@@ -3,7 +3,8 @@ from ckan.common import (
      c, request
 )
 import ckan.model as model
-
+import sqlalchemy
+import ckan.logic as logic
 
 downloadable_formats = {
     'csv', 'xls', 'txt', 'jpg', 'jpeg', 'png', 'gif', 'zip', 'xml'
@@ -23,16 +24,13 @@ def get_facet_items_dict(facet, limit=10, exclude_active=False):
     else:
         return (facets,no_items)
     
-    
-
-def get_last_modifier_user(package_id):
-    pkg_list  = model.Session.query(model.Package).filter(model.Package.id == package_id).all()
-    pkg = pkg_list[0]
-    rev_id = pkg.latest_related_revision.id
+def get_last_modifier_user(rev_id):
     act_list = model.Session.query(model.Activity).filter(model.Activity.revision_id == rev_id).all()
-    act = act_list[0]
-    usr_id = act.user_id
-    return model.User.get(usr_id)
+    if len(act_list)>0:
+        act = act_list[0]
+        usr_id = act.user_id
+        return model.User.get(usr_id)
+    return 'hdx'
 
 def get_filtered_params_list(params):
     result = []
@@ -40,4 +38,33 @@ def get_filtered_params_list(params):
         if key not in {'q','sort'} :
             result.append((key,value))
     return result;
+
+def get_last_revision_package(package_id):
+    pkg_list  = model.Session.query(model.Package).filter(model.Package.id == package_id).all()
+    pkg = pkg_list[0]
+    return pkg.latest_related_revision.id
+
+def get_last_revision_group(group_id):
+    grp_list  = model.Session.query(model.Group).filter(model.Group.id == group_id).all()
+    grp = grp_list[0]
+    last_rev = grp.all_related_revisions[0][0]
+    return last_rev.id
+
+def get_group_followers(grp_id):
+    result = logic.get_action('group_follower_count')(
+            {'model': model, 'session': model.Session},
+            {'id': grp_id})
+    return result
+
+def get_group_members(grp_id):
+    member_list = logic.get_action('member_list')(
+            {'model': model, 'session': model.Session},
+            {'id': grp_id})
+    result = len(member_list)
+    return result
+
+
+
+
+
     
