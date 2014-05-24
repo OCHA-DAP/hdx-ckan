943becacfdbca31ef200b91b0b6ec4f93f9d4576
Alexandru Artimon <aalecs@gmail.com>
2014-05-24 17:20:43 +0300

    fixes #381 - Add Colombia to the focus countries list drop down on the home page

--- ckanext-hdx_theme/ckanext/hdx_theme/count.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/count.py
@@ -3,21 +3,54 @@ from ckan import model
 from ckan.lib import base
 import sqlalchemy
 
+
 class CountController(base.BaseController):
 
-    def _get_count(self, table, type):
-        isPrivate='';
+
+    def _get_count(self, table, type, extra):
+        isPrivate=''
+        extraQ=''
         if type == 'dataset':
             isPrivate=' and private = FALSE';
-        q = sqlalchemy.text(''' select count(*) from {name} where state='active' and type='{type}' {isPrivate};'''.format(name=table, type=type, isPrivate=isPrivate))
+
+        if extra:
+            extraQ = "where state='active' and type='{type}' {isPrivate}".format(type=type, isPrivate=isPrivate)
+
+        q = sqlalchemy.text(''' select count(*) from {name} {extra};'''.format(name=table, extra=extraQ))
         result = model.Session.connection().execute(q, entity_id=id).scalar()
         return result
 
     def dataset(self):
-        return json.dumps({'count': self._get_count('package', 'dataset')})
+        return json.dumps({'count': self._get_count('package', 'dataset', True)})
 
     def country(self):
-        return json.dumps({'count': self._get_count('"group"', 'group')})
+        return json.dumps({'count': self._get_count('"group"', 'group', True)})
+
+    def organization(self):
+        return json.dumps({'count': self._get_count('"group"', 'organization', True)})
 
     def source(self):
         return json.dumps({'count': 20})
+
+    def tag(self):
+        return json.dumps({'count': self._get_count('tag', 'tag', False)})
+
+    def format(self):
+        q = sqlalchemy.text('''select count(distinct(format)) from resource where not format is null;''')
+        result = model.Session.connection().execute(q, entity_id=id).scalar()
+        return json.dumps({'count': result})
+
+    def license(self):
+        q = sqlalchemy.text('''select count(distinct(license_id)) from package where not license_id is null;''')
+        result = model.Session.connection().execute(q, entity_id=id).scalar()
+        return json.dumps({'count': result})
+
+    list = {
+        'groups':country,
+        'tags':tag,
+        'organization':organization,
+        'res_format':format,
+        'license_id':license
+    }
+
+
