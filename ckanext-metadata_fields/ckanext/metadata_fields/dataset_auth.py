9141d3c72362e10ee8c88b5beab25d74f6acf91c
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2014-06-11 12:39:15 +0300

    #532 - putting session into context if not exists - adding missing imports

--- ckanext-metadata_fields/ckanext/metadata_fields/dataset_auth.py
+++ ckanext-metadata_fields/ckanext/metadata_fields/dataset_auth.py
@@ -1,12 +1,15 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckan.new_authz as new_authz
 
 def package_create(context, data_dict=None):
 	#return {'success':False, 'msg':'Testing this shit'}
 
-	user_name = context['user']
-	#Is the user logged in?
-	convert_user_name_or_id_to_id = toolkit.get_converter('convert_user_name_or_id_to_id')
+    user_name = context['user']
+    #Is the user logged in?
+    convert_user_name_or_id_to_id = toolkit.get_converter('convert_user_name_or_id_to_id')
+    if not 'session' in context and 'model' in context:
+        context['session'] = context['model'].Session
 
 	try:
 		user_id = convert_user_name_or_id_to_id(user_name, context)
@@ -19,8 +22,8 @@ def package_create(context, data_dict=None):
 
 	data_dict = data_dict or {}
 	org_id = data_dict.get('organization_id')
-	if org_id and not new_authz.has_user_permission_for_group_or_org(org_id, user, 'create_dataset'):
-		return {'success': False, 'msg': _('User %s not authorized to add dataset to this organization') % user}
+	if org_id and not new_authz.has_user_permission_for_group_or_org(org_id, user_id, 'create_dataset'):
+		return {'success': False, 'msg': _('User %s not authorized to add dataset to this organization') % user_id}
 	
 	return {'success':True}
 
