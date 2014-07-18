0679509c25cdab40ec23d15f3b3a3da2d2740108
Marianne Bellotti <marianne.bellotti@gmail.com>
2014-07-18 15:34:36 -0400

    closes #933

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/hdx-resource-upload-field.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/hdx-resource-upload-field.js
@@ -32,7 +32,7 @@ this.ckan.module('hdx-resource-upload-field', function (jQuery, _, i18n) {
       },
       template: [
         '<span class="resource-upload-field" style="float: left; overflow: visible">',
-        '   <input type="file"  onclick="$(\'label.type-file\').click();"></input>',
+        '   <input type="file"  onclick="file_upload_selected();"></input>',
         '   <input id="field-resource-type-upload" type="radio" name="resource_type" value="file.upload"></input>',
         '   <label class="radio inline type-file" for="field-resource-type-upload"></label>',
         '</span>'
