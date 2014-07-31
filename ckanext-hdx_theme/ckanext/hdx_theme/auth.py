6898db074bc7723bd3cbed924d2cf0210120bc1b
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2014-08-01 02:03:29 +0300

    #1000 auth functions for all org related requests
    + refactoring
    + nose tests

--- ckanext-hdx_theme/ckanext/hdx_theme/auth.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/auth.py
@@ -4,15 +4,34 @@ import ckan.logic as logic
 from ckan.lib.base import _
 
 
-def hdx_basic_user_info(context, data_dict):
+def _simple_logged_in_auth(fail_message):
     logged_in = new_authz.auth_is_loggedin_user()
     if logged_in:
         return {'success': True}
     else:
         return {'success': False,
-                'msg': _("You must be logged in to access basic organization member info.")}
-
+                'msg': fail_message}
 
 @logic.auth_sysadmins_check
 def group_member_create(context, data_dict):
     return {'success': False, 'msg': _('Nobody can add a member to a country in HDX')}
+
+
+def hdx_basic_user_info(context, data_dict):
+    return _simple_logged_in_auth(_("You must be logged in to access basic \
+                            organization member info."))
+
+
+def hdx_send_new_org_request(context, data_dict):
+    return _simple_logged_in_auth(_("You must be logged in to send a new \
+                            organization request."))
+
+
+def hdx_send_editor_request_for_org(context, data_dict):
+    return _simple_logged_in_auth(_("You must be logged in to send a request \
+                            for being an editor."))
+
+
+def hdx_send_request_membership(context, data_dict):
+    return _simple_logged_in_auth(_("You must be logged in to send a  \
+                            membership request."))
