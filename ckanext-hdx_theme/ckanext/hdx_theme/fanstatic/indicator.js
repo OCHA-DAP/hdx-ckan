434c7906645c1b2008bdb48ff93ce3d74a9a5328
Alexandru Artimon <aalecs@gmail.com>
2014-09-04 13:09:32 +0300

    #1202 - indicator graph demo with mock data

---
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/indicator.js
@@ -0,0 +1,84 @@
+ckan.module('hdx-indicator-graph', function ($, _) {
+  return {
+    initialize: function(){
+      var CHART_COLORS = ['1ebfb3', '117be1', 'f2645a', '555555', 'ffd700'];
+      var c3_chart, chart_config, data, elementId, alldata;
+      alldata = initialData.results;
+
+      elementId = '#' + $(this.el).attr('id');
+
+      //Let's select 10 data points that have different values
+      data = [];
+      for (var alldataEl in alldata){
+        if (data.length > 9)
+          break;
+        var found = false;
+        for (var dataEl in data){
+          if (data[dataEl].value === alldata[alldataEl].value){
+            found = true;
+            break;
+          }
+        }
+        if (!found || (alldata.length - alldataEl + data.length < 10)){
+          if (alldata[alldataEl]['locationName'].length > 20)
+            alldata[alldataEl]['locationName'] = alldata[alldataEl]['locationName'].substring(0, 18) + '...';
+
+          data.push(alldata[alldataEl]);
+        }
+      }
+
+      chart_config = {
+        bindto: elementId,
+        padding: {
+          top: 40
+        },
+        color: {
+          pattern: CHART_COLORS
+        },
+        data: {
+          json: data,
+          keys: {
+            x: 'locationName',
+            value: ['value']
+          },
+          names: {
+            value: 'Indicator Name '
+          },
+          type: 'bar'
+        },
+        axis: {
+          x: {
+            type: 'category'
+          },
+          y: {
+            label: {
+              text: this.options.label,
+              position: 'outer-middle'
+            },
+            tick: {
+              format: d3.format(',')
+            }
+          }
+        },
+        tooltip: {
+          format: {
+            title: function (d) {
+              return '';
+            }
+          }
+        },
+        grid: {
+          y: {
+            show: true
+          }
+        }
+      };
+      c3_chart = c3.generate(chart_config);
+
+    },
+    options: {
+    	label: "",
+      code: ""
+    }
+  }
+});
