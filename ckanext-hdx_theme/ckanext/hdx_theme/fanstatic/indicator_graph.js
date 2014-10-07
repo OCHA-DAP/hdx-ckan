be1d6ffbf90cfd4052520c4cc1dedf2dd2945305
Alexandru Artimon <aalecs@gmail.com>
2014-10-07 13:24:05 +0300

    #1373 - get the period type data from the service and add the values to the dropdown            - build correct data request url when a year is selected

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/indicator_graph.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/indicator_graph.js
@@ -29,6 +29,8 @@ ckan.module('hdx-indicator-graph', function ($, _) {
       COUNTRY_DESC: "Alphabet Z-A"
     },
     _sort_order: "VALUE_DESC",
+    _period_type: "LATEST_YEAR_BY_COUNTRY",
+    _period_type_default: "LATEST_YEAR_BY_COUNTRY",
     _onClick: function(){
       /**
        * Click Only callback
@@ -52,6 +54,8 @@ ckan.module('hdx-indicator-graph', function ($, _) {
         this.elementId = '#' + this.options.container;
       var elementId = this.elementId;
 
+      //init year filter dropdown
+      this._init_year_filter_dropdown();
       //init chart with no data yet
       var chart_config = this._build_chart_config(elementId, this);
       this.c3_chart = c3.generate(chart_config);
@@ -78,17 +82,61 @@ ckan.module('hdx-indicator-graph', function ($, _) {
 
       this._internal_load_data_build_graph();
     },
+    _init_year_filter_dropdown: function (){
+      if (this.options.side_panel != "") {
+
+        var indicatorCode, sourceCode;
+        indicatorCode = indicatorCodeMapping[this.options.name];
+        sourceCode = indicatorSourceMapping[this.options.name];
+        //prepare source aux for url
+        var urlSourceAux = "";
+        if (sourceCode != "")
+          urlSourceAux = "&s="+sourceCode;
+
+        jQuery.ajax({
+          url: "/api/action/hdx_get_indicator_available_periods?it=" + indicatorCode + urlSourceAux,
+          success: $.proxy(this._init_year_filter_dropdown_items, this)
+        });
+//          this._init_year_filter_dropdown_items();
+      }
+    },
+    _init_year_filter_dropdown_items: function (json){
+      var container = $("#"+this.options.side_panel_years);
+      if (json.success){
+        var data = json.result.results;
+//        var data = [2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992, 1991, 1990, 1989, 1988, 1987, 1986, 1985, 1984, 1983, 1982, 1981, 1980, 1979, 1978, 1977, 1976, 1975, 1974, 1973, 1972, 1971, 1970, 1969, 1968, 1967, 1966, 1965, 1964, 1963, 1962, 1961, 1960, 1959, 1958, 1957, 1956, 1955, 1954, 1953, 1952, 1951, 1950, 1949, 1948, 1947, 1946, 1945, 1944, 1943, 1942, 1941, 1940, 1939, 1938, 1937, 1936, 1935, 1934, 1933, 1932, 1931, 1930, 1929, 1928, 1927, 1926, 1925, 1924, 1923, 1922, 1921, 1920, 1919, 1918, 1917, 1916, 1915, 1914, 1913, 1912, 1911, 1910, 1909, 1908, 1907, 1906, 1905, 1904, 1903, 1902, 1901, 1900];
+
+        for (var i = 0; i < 10 && i < data.length; i++){
+          var item = data[i];
+          container.append("<option value='" + item + "'>" + item + "</option>");
+        }
+      }
+      container.data("ckanModule", this);
+      container.on("change", function(){
+        var year = $(this).select2().val();
+        var module = $(this).data("ckanModule");
+
+        module._period_type = year;
+        module._internal_load_data_build_graph();
+      })
+    },
     _internal_load_data_build_graph: function (){
       var indicatorCode, sourceCode;
       indicatorCode = indicatorCodeMapping[this.options.name];
       sourceCode = indicatorSourceMapping[this.options.name];
+
+      //
+      var periodTypeAux = "&periodType=" + this._period_type;
+      if (this._period_type != this._period_type_default)
+        periodTypeAux = "&minTime="+ this._period_type +"&maxTime=" + this._period_type;
+
       //prepare source aux for url
       var urlSourceAux = "";
       if (sourceCode != "")
         urlSourceAux = "&s="+sourceCode;
       //get the data synchronously from the server
       jQuery.ajax({
-        url: "/api/action/hdx_get_indicator_values?it=" + indicatorCode + urlSourceAux + "&periodType=LATEST_YEAR_BY_COUNTRY&sorting="+this._sort_order,
+        url: "/api/action/hdx_get_indicator_values?it=" + indicatorCode + urlSourceAux + periodTypeAux + "&sorting="+this._sort_order,
         success: $.proxy(this._data_ajax_success, this),
 //        complete: $.proxy(this._data_ajax_complete, this),
         async:false
@@ -366,7 +414,8 @@ ckan.module('hdx-indicator-graph', function ($, _) {
       parent_selector: "",
       side_panel: "",
       side_panel_locations: "",
-      side_panel_sortorder: ""
+      side_panel_sortorder: "",
+      side_panel_years: ""
     }
   }
 });
