61e2fbd9fe117d5354384292002f08968246749c
Marianne Bellotti <marianne.bellotti@gmail.com>
2014-10-13 08:33:50 -0400

    closes #1422

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/filter.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/filter.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
+  if(localStorage.getItem('hdx_filter') == 1){
+    $("#search_bar_content").slideDown(300, function() {
+          return $("#search_filter_btn span").text('-');
+        });
+  }
   $('#filter_dropdown').on('change','select', function(){
       window.location = $(this).val();
   });
 
   $("#search_filter_btn").click(function() {
+      if(localStorage.getItem('hdx_filter')==1){
+        localStorage.setItem('hdx_filter', 0);
+      }else{
+        localStorage.setItem('hdx_filter', 1);
+      }
       if ($("#search_filter_btn span").text() === '+') {
         return $("#search_bar_content").slideDown(300, function() {
           return $("#search_filter_btn span").text('-');
