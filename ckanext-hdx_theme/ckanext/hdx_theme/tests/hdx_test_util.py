5ded898ea18eec23a3403f48a773b5f03ba68d48
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2014-11-12 16:55:17 +0200

    #1678 fixing problemw with form parameter and adding nose tests

--- ckanext-hdx_theme/ckanext/hdx_theme/tests/hdx_test_util.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/tests/hdx_test_util.py
@@ -22,3 +22,21 @@ def test_strings_in_text(text, search_strings,
 
     for item in search_strings:
         assert item in section, item + ' not in text section'
+
+
+def count_string_occurences(text, search_item,
+                            begin_str=None, end_str=None):
+    start = 0
+    end = len(text)
+    if begin_str:
+        start = text.find(begin_str)
+    if end_str:
+        end = text.find(end_str, start)
+
+    assert start >= 0
+    assert end >= 0
+
+    section = text[start + len(begin_str):end]
+    count = section.count(search_item)
+
+    return count
