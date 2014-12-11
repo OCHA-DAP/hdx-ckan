d65efbabcfca60a1aaea5a2b5594e9c28e0dbc32
Alexandru Artimon <aalecs@gmail.com>
2014-12-11 12:26:18 +0200

    #1862 - Colombia Page: adding data from datastores            - change to date format

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/crisis-colombia/crisis-colombia.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/crisis-colombia/crisis-colombia.js
@@ -113,7 +113,7 @@ function drawGraph2() {
             tick: {
               rotate: 30,
               culling: false,
-              format: '%b %d, %Y'
+              format: '%b %Y'
             }
           },
           y: {
