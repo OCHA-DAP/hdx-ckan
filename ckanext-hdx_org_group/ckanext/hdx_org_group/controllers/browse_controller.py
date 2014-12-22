bc638a5b8162bd193eb8b77f5e767033ceff4e15
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2014-12-23 01:39:17 +0200

    #1997 fetching ind / datasets count info for countries from solr

--- ckanext-hdx_org_group/ckanext/hdx_org_group/controllers/browse_controller.py
+++ ckanext-hdx_org_group/ckanext/hdx_org_group/controllers/browse_controller.py
@@ -30,14 +30,26 @@ class BrowseController(base.BaseController):
         return base.render('browse/browse.html')
 
     def get_countries(self):
+
+        context = {'model': model, 'session': model.Session,
+                   'user': c.user or c.author, 'for_view': True,
+                   'auth_user_obj': c.userobj}
+        dataset_count_dict = self._get_dataset_counts(context, 'dataset')
+        indicator_count_dict = self._get_dataset_counts(context, 'indicator')
+
         all_countries = get_action('cached_group_list')()
 
         all_countries_world_1st = []
         for country in all_countries:
-            if country['name'] == 'world':
+            code = country['name']
+
+            if code == 'world':
                 all_countries_world_1st.insert(0, country)
             else:
                 all_countries_world_1st.append(country)
+
+            country['dataset_count'] = dataset_count_dict.get(code, None)
+            country['indicator_count'] = indicator_count_dict.get(code, None)
 
         return all_countries_world_1st
 
@@ -87,3 +99,17 @@ class BrowseController(base.BaseController):
             log.error('ERROR getting vocabulary named Topics: %r' %
                       str(e.extra_msg))
         return all_topics
+
+    def _get_dataset_counts(self, context, package_type):
+        search = {
+            'q': None,
+            'fq': '+extras_indicator: 1' if package_type == 'indicator' else '-extras_indicator: 1',
+            'facet.field': ['groups'],
+            'facet.limit': 1000,
+            'rows': 1,
+        }
+        result = get_action('package_search')(context, search)
+        if 'facets' in result and 'groups' in result['facets']:
+            return result['facets']['groups']
+        else:
+            return {}
