a3dcd8497dd1049dca79695fbcd1735b489e251c
Alexandru Artimon <aalecs@gmail.com>
2015-01-23 15:10:09 +0200

    fixes #2096 - Search and Dataset List Pages: Move the filter button                  - generating new font set with the two new icons                  - styling up filter icon                  - adapt js to new element toggle

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/filter.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/filter.js
@@ -16,18 +16,20 @@ $(document).ready(function(){
 	});
 
 	$("#search_filter_btn").click(function() {
-//		if(localStorage.getItem('hdx_filter')==1){
-//		localStorage.setItem('hdx_filter', 0);
-//		}else{
-//		localStorage.setItem('hdx_filter', 1);
-//		}
-		if ($("#search_filter_btn span").text() === '+') {
+
+        var filter_btn = $("#search_filter_btn");
+
+		if (filter_btn.hasClass("icon-open_filter_button")) {
 			return $("#search_bar_content").slideDown(300, function() {
-				return $("#search_filter_btn span").text('-');
+                filter_btn.removeClass("icon-open_filter_button");
+                filter_btn.addClass("icon-close_filter_button");
+				return filter_btn;
 			});
 		} else {
 			return $("#search_bar_content").slideUp(300, function() {
-				return $("#search_filter_btn span").text('+');
+                filter_btn.removeClass("icon-close_filter_button");
+                filter_btn.addClass("icon-open_filter_button");
+				return filter_btn;
 			});
 		}
 	});
