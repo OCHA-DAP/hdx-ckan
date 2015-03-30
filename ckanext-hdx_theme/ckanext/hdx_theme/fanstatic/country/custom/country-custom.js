18fe8095e1188253e5af12eb36eca6239d8e6655
Alexandru Artimon <aalecs@gmail.com>
2015-03-30 16:13:58 +0300

    #2428 - Custom location page: predefined visualizations & integration            - add tick rotation on X axis

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/country/custom/country-custom.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/country/custom/country-custom.js
@@ -41,6 +41,9 @@ function prepareGraph(element, data, colX, colXType, colXFormat, colY, graphType
         },
         axis: {
             x: {
+                tick: {
+                  rotate: 20
+                }
             }
         }
     };
