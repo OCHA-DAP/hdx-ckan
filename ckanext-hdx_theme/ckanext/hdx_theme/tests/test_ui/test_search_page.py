a2cdabaf2687e13e7497017fcc34b939298414df
Alexandru Artimon <aalecs@gmail.com>
2015-06-11 16:34:26 +0300

    fail test 2

--- ckanext-hdx_theme/ckanext/hdx_theme/tests/test_ui/test_search_page.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/tests/test_ui/test_search_page.py
@@ -17,26 +17,26 @@ class TestSearchPage(hdx_test_base.HdxBaseTest):
     @classmethod
     def _load_plugins(cls):
         hdx_test_base.load_plugin('hdx_search hdx_theme')
-        
+
     def test_active_tab(self):
         all_page = self._getSearchPage('')
-        assert '<li class="mx-tab-item-all active">' in str(all_page.response), 'Page needs to have all tab selected'
+        assert 'FAIL<li class="mx-tab-item-all active">' in str(all_page.response), 'Page needs to have all tab selected'
         assert '<li class="mx-tab-item-indicators active">' not in str(all_page.response), 'Page needs to have all tab selected'
         assert '<li class="mx-tab-item-feature-pages active">' not in str(all_page.response), 'Page needs to have all tab selected'
         assert '<li class="mx-tab-item-datasets active">' not in str(all_page.response), 'Page needs to have all tab selected'
-        
+
         indicator_page = self._getSearchPage('', 1)
         assert '<li class="mx-tab-item-all active">' not in str(indicator_page.response), 'Page needs to have indicators tab selected'
         assert '<li class="mx-tab-item-indicators active">' in str(indicator_page.response), 'Page needs to have indicators tab selected'
         assert '<li class="mx-tab-item-feature-pages active">' not in str(indicator_page.response), 'Page needs to have indicators tab selected'
         assert '<li class="mx-tab-item-datasets active">' not in str(indicator_page.response), 'Page needs to have indicators tab selected'
-        
+
         dataset_page = self._getSearchPage('', 0)
         assert '<li class="mx-tab-item-all active">' not in str(dataset_page.response), 'Page needs to have datasets tab selected'
         assert '<li class="mx-tab-item-indicators active">' not in str(dataset_page.response), 'Page needs to have datasets tab selected'
         assert '<li class="mx-tab-item-feature-pages active">' not in str(dataset_page.response), 'Page needs to have datasets tab selected'
         assert '<li class="mx-tab-item-datasets active">' in str(dataset_page.response), 'Page needs to have datasets tab selected'
-    
+
     def _getSearchPage(self, query, ext_indicator=None, apikey=None):
         page = None
         if ext_indicator != None:
