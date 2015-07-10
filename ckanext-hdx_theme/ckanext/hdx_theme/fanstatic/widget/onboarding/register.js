c23c1b889f8f8fed2404c41471142c1fe5c4f652
Alexandru Artimon <aalecs@gmail.com>
2015-07-10 15:12:01 +0300

    #2997 - Onboarding: new registration module - UI            - on register also login with the newly created user

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/widget/onboarding/register.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/widget/onboarding/register.js
@@ -6,7 +6,19 @@ $(document).ready(function(){
         $.post("/user/register_details", $this.serialize(), function(result_data){
             var result = JSON.parse(result_data);
             if (result.success){
-                closeCurrentWidget($this);showOnboardingWidget('#registeredPopup');
+                var actions = {
+                    login: $("#register-username").val(),
+                    password: $("#register-password").val(),
+                    ajax: true
+                };
+                $.post("/login_generic", $this.serialize(), function(result_data){
+                    var result = JSON.parse(result_data);
+                    if (result.success){
+                        closeCurrentWidget($this);showOnboardingWidget('#registeredPopup');
+                    } else {
+                        alert("Can't login with the newly created account!");
+                    }
+                });
             } else {
                 alert("Can't register: " + result.error.message);
             }
