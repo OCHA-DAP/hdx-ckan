b767f58cf9ac128f1768f74545e2c5449201b6b6
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2015-07-13 11:56:25 +0300

    #2999 Do nothing if the "hdx.gis.layer_import_url" is not set
    - also removing setting from commong config

--- ckanext-hdx_package/ckanext/hdx_package/actions/create.py
+++ ckanext-hdx_package/ckanext/hdx_package/actions/create.py
@@ -4,11 +4,13 @@ Created on Jul 07, 2015
 @author: alexandru-m-g
 '''
 
+from pylons import config
 from string import lower
 
 import ckan.logic.action.create as core_create
 import ckan.logic as logic
 import ckanext.hdx_package.helpers.geopreview as geopreview
+
 
 _get_or_bust = logic.get_or_bust
 get_action = logic.get_action
@@ -20,11 +22,13 @@ def resource_create(context, data_dict):
     It was modified to trigger the geopreview creation process.
     '''
 
-    geopreview.add_init_shape_info_data_if_needed(data_dict)
+    do_geo_preview = config.get('hdx.gis.layer_import_url')
+    if do_geo_preview:
+        geopreview.add_init_shape_info_data_if_needed(data_dict)
 
     result_dict = core_create.resource_create(context, data_dict)
 
-    if lower(result_dict.get('format', '')) in geopreview.GIS_FORMATS:
+    if do_geo_preview and lower(result_dict.get('format', '')) in geopreview.GIS_FORMATS:
         started_successfully = geopreview.start_geo_transformation_process(context, result_dict)
         if not started_successfully:
             get_action('resource_update')(context, result_dict)
