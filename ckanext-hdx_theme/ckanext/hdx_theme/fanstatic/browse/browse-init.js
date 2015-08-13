e07f0de3f2e005adcabe6969230bd83012137751
Alexandru Artimon <aalecs@gmail.com>
2015-08-13 16:23:17 +0300

    fixes #3378 - Locations list page: /group UI

---
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/browse/browse-init.js
@@ -0,0 +1,11 @@
+(function() {
+  //fixed page menu
+  $(window).scroll(browse_by_menu);
+  browse_by_menu();
+
+  var countDatasets = prepareCount();
+  prepareMap(countDatasets);
+  prepareCountryList(countDatasets);
+
+  initSortingWidget();
+}).call(this);
