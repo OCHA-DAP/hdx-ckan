66cd63b37e050135b1b021c259ee7715d45c9be2
Marianne Bellotti <marianne.bellotti@gmail.com>
2015-09-28 10:08:27 -0400

    adding authentication to package_purge also some error catching while we're at it

--- ckanext-hdx_package/ckanext/hdx_package/actions/delete.py
+++ ckanext-hdx_package/ckanext/hdx_package/actions/delete.py
@@ -5,13 +5,26 @@ Created on September 25, 2015
 '''
 
 import ckan.model as model
+import ckan.logic
+
+_check_access = ckan.logic.check_access
+NotFound = ckan.logic.NotFound
+_get_or_bust = ckan.logic.get_or_bust
 
 def hdx_dataset_purge(context, data_dict):
 	'''
 	Permenantly and completely delete a dataset from HDX
 	'''
-	dataset_ref = data_dict['id'] or data_dict['name']
+	model = context['model']
+	user = context['user']
+
+	_check_access('package_delete',context, data_dict)
+    
+	dataset_ref = _get_or_bust(data_dict, 'id')
 	dataset = model.Package.get(unicode(dataset_ref))
+	if dataset is None:
+		raise NotFound('Dataset "{id}" was not found.'.format(id=dataset_ref))
+
 	name = dataset.name
 	rev = model.repo.new_revision()
 	try:
