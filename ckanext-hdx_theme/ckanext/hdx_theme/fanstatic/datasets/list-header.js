95bc213f33a93f221ed59c9adaa4b3a4ef39e8be
Alexandru Artimon <aalecs@gmail.com>
2015-10-14 17:11:56 +0300

    #3555 - Rationalize the different ways we display dataset search results

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/datasets/list-header.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/datasets/list-header.js
@@ -21,7 +21,31 @@ $(document).ready(function(){
     });
 
     //$(".list-header-filters select.filter-item").attr("style", "");
+    $(".list-header-filters form").submit(function(event){
+        //
+        var $this = $(this);
 
+        var params = $("select.filter-item").serialize();
+        $("input.checkbox-filter:checked").each(function(idx, el){
+            if (params !== ""){
+                params += "&";
+            }
+            params += $(el).attr("name");
+            params += "=1";
+        });
+        console.log(params);
+        event.preventDefault();
+        var base = $("#base-filter-location").text().trim();
+        window.location = base + "?" + params;
+    });
+    $(".list-header-filters a.reset-action").click(function(){
+        $(this).closest("form").resetForm();
+        $(this).closest("form").find('select').multipleSelect('refresh');
+    });
+
+    $(".list-header-filters a.main-action").click(function(){
+        $(this).closest("form").submit();
+    });
 });
 
 function determineEnabled(origin){
