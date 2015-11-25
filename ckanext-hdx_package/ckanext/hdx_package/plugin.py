b0032f8314b35b8e0d7c3bb702aaa49bcf6768b3
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2015-11-25 13:26:30 +0200

    #3846 refactoring topline data access and creating new "key figures" view

--- ckanext-hdx_package/ckanext/hdx_package/plugin.py
+++ ckanext-hdx_package/ckanext/hdx_package/plugin.py
@@ -427,3 +427,37 @@ class HDXGeopreviewPlugin(plugins.SingletonPlugin):
     def form_template(self, context, data_dict):
         return 'new_views/geopreview_view_form.html'
 
+
+class HDXKeyFiguresPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IResourceView, inherit=True)
+
+    def info(self):
+        return {
+            'name': 'hdx_key_figures_view',
+            'title': 'Key Figures',
+            'filterable': False,
+            'preview_enabled': True,
+            'requires_datastore': True,
+            'iframed': True,
+            'default_title': p.toolkit._('Key Figures')
+        }
+
+    def can_view(self, data_dict):
+        resource = data_dict['resource']
+        return (resource.get('datastore_active') or
+                resource.get('url') == '_datastore_only_resource')
+
+    def setup_template_variables(self, context, data_dict):
+        import ckanext.hdx_crisis.dao.location_data_access as location_data_access
+        id = data_dict['resource']['id']
+        key_figures = location_data_access.get_formatted_topline_numbers(id)
+
+        return {
+            'key_figures': key_figures
+        }
+
+    def view_template(self, context, data_dict):
+        return 'new_views/key_figures_view.html'
+
+    def form_template(self, context, data_dict):
+        return 'new_views/key_figures_view_form.html'
--- ckanext-hdx_package/setup.py
+++ ckanext-hdx_package/setup.py
@@ -27,6 +27,8 @@ setup(
         hdx_package=ckanext.hdx_package.plugin:HDXPackagePlugin
         hdx_chart_views=ckanext.hdx_package.plugin:HDXChartViewsPlugin
         hdx_geopreview_view=ckanext.hdx_package.plugin:HDXGeopreviewPlugin
+        hdx_key_figures_view=ckanext.hdx_package.plugin:HDXKeyFiguresPlugin
+
 
         [paste.paster_command]
         hdx-migrate = ckanext.hdx_package.command:MigrateCommand
