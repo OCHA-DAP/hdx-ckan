0e90b0dca0188fd1d5295da7f176c03ae4db4442
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2015-12-09 02:12:44 +0200

    #3887 adding logic for "edit" mode in js module
    + some explanations

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/contribute_flow/contribute_flow_main.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/contribute_flow/contribute_flow_main.js
@@ -4,23 +4,32 @@ ckan.module('contribute_flow_main', function($, _) {
 	return {
 		initialize : function() {
             var formId = this.options.form_id;
+            var dataset_id = this.options.dataset_id;
             var contributeGlobal = {
                 'getDatasetIdPromise': function() {
                     var deferred = new $.Deferred();
-                    if ( !this.hasOwnProperty('_datasetId')) {
-                        var promise = $.post('/contribute/new', this.getFormValues());
-                        $.when(promise).done(function(data, status, xhr) {
-                                if (data.data && data.data.id) {
-                                    contributeGlobal._datasetId = data.data.id;
-                                    deferred.resolve(data.data.id);
+
+                    if ( !this.hasOwnProperty('_datasetId')) { // This is the first time that we look for the ID
+
+                        if ( dataset_id ) { // If we're in "edit" mode the ID was injected in options
+                            this._datasetId = dataset_id;
+                            deferred.resolve(this._datasetId);
+                        }
+                        else {  // We're in the "new" mode and we need to create the initial dataseet
+                            var promise = $.post('/contribute/new', this.getFormValues());
+                            $.when(promise).done(function (data, status, xhr) {
+                                    if (data.data && data.data.id) {
+                                        contributeGlobal._datasetId = data.data.id;
+                                        deferred.resolve(data.data.id);
+                                    }
+                                    else {
+                                        deferred.resolve(null);
+                                    }
                                 }
-                                else {
-                                    deferred.resolve(null);
-                                }
-                            }
-                        );
+                            );
+                        }
                     }
-                    else {
+                    else { // We already have the dataset ID
                         deferred.resolve(this._datasetId);
                     }
                     return deferred.promise();
@@ -47,7 +56,8 @@ ckan.module('contribute_flow_main', function($, _) {
 
         },
         options: {
-            form_id: 'create_dataset_form'
+            form_id: 'create_dataset_form',
+            dataset_id: null
         }
 
 	};
