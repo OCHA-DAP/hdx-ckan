c120b5ee50ae079c9e632781d6b727dda5283959
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2015-12-15 00:22:31 +0200

    #3884 changes on error-block styling - when/how is it displayed or hidden

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/contribute_flow/form_element_manager.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/contribute_flow/form_element_manager.js
@@ -4,18 +4,17 @@ ckan.module('hdx_form_element_manager', function($, _) {
     return {
         initialize: function () {
             var errorEl = this.el.find('.error-block');
+            var errorWrapperEl = this.el.find('.controls');
             var elementName = this.options.element_name;
             this.sandbox.subscribe('hdx-form-validation',
                 function (message) {
                     if (message.type == 'reset' ) {
-                        errorEl.addClass('hdx-invisible-element');
-                        errorEl.removeClass('hdx-visible-element');
+                        errorWrapperEl.removeClass('error');
                         errorEl.html('');
                     }
                     else
                         if (message.elementName == elementName) {
-                            errorEl.removeClass('hdx-invisible-element');
-                            errorEl.addClass('hdx-visible-element');
+                            errorWrapperEl.addClass('error');
 
                             var existingText = errorEl.html().trim();
                             var newText = existingText ? existingText + ", " + message.errorInfo : message.errorInfo;
