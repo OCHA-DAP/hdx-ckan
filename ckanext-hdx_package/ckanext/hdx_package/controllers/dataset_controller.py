a08efc630925377e5eaf36efb60c210eb6ed4050
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2016-06-09 01:01:00 +0300

    #4228 First draft of tracking download and page view events in mixpanel +  refactoring for google analytics (cherry picked from commit b629a21)

--- ckanext-hdx_package/ckanext/hdx_package/controllers/dataset_controller.py
+++ ckanext-hdx_package/ckanext/hdx_package/controllers/dataset_controller.py
@@ -704,8 +704,10 @@ class DatasetController(PackageController):
         template = template[:template.index('.') + 1] + format
 
         # set dataset type for google analytics - modified by HDX
-        c.ga_dataset_type = self._google_analytics_dataset_type(c.pkg_dict)
-        c.ga_location = self._google_analytics_location(c.pkg_dict)
+        # c.ga_dataset_type = self._google_analytics_dataset_type(c.pkg_dict)
+        c.analytics_is_cod = self._analytics_is_cod(c.pkg_dict)
+        c.analytics_is_indicator = self._analytics_is_indicator(c.pkg_dict)
+        c.analytics_group_names, c.analytics_group_ids = self._analytics_location(c.pkg_dict)
 
         # changes done for indicator
         act_data_dict = {'id': c.pkg_dict['id'], 'limit': 7}
@@ -770,31 +772,26 @@ class DatasetController(PackageController):
 
         assert False, "We should never get here"
 
-    def _google_analytics_dataset_type(self, pkg_dict):
-        type = 'standard'
-        tags = [tag.get('name', '') for tag in pkg_dict.get('tags', [])]
-
+    def _analytics_is_indicator(self, pkg_dict):
         if int(pkg_dict.get('indicator', 0)) == 1:
-            type = 'indicator'
+            return 'true'
+        return 'false'
+
+    def _analytics_is_cod(self, pkg_dict):
+        tags = [tag.get('name', '') for tag in pkg_dict.get('tags', [])]
         if 'cod' in tags:
-            type = 'cod~indicator' if type == 'indicator' else 'cod'
+            return 'true'
+        return 'false'
 
-        return type
-
-    def _google_analytics_location(self, pkg_dict):
-        limit = 15
+    def _analytics_location(self, pkg_dict):
         locations = pkg_dict.get('groups', [])
-        if len(locations) >= limit:
-            result = 'many'
-        else:
-            locations = [item.get('name', '') for item in locations]
-            locations.sort()
-            result = "~".join(locations)
+        location_names = []
+        location_ids = []
+        for l in sorted(locations, key=lambda item: item.get('name', '')):
+            location_names.append(l.get('name', ''))
+            location_ids.append(l.get('id', ''))
 
-        if not result:
-            result = 'none'
-
-        return result
+        return json.dumps(location_names), json.dumps(location_ids)
 
     def _get_org_extras(self, org_id):
         """
@@ -1101,8 +1098,10 @@ class DatasetController(PackageController):
         c.resource['has_views'] = len(resource_views) > 0
 
         # set dataset type for google analytics - modified by HDX
-        c.ga_dataset_type = self._google_analytics_dataset_type(c.package)
-        c.ga_location = self._google_analytics_location(c.package)
+        # c.ga_dataset_type = self._google_analytics_dataset_type(c.package)
+        c.analytics_is_cod = self._analytics_is_cod(c.package)
+        c.analytics_is_indicator = self._analytics_is_indicator(c.package)
+        c.analytics_group_names, c.analytics_group_ids = self._analytics_location(c.package)
 
         current_resource_view = None
         view_id = request.GET.get('view_id')
--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/google-analytics.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/google-analytics.js
@@ -2,12 +2,23 @@
 
 function setUpResourcesTracking(){
   $('.ga-download').on('click', function(){
-    //var rTitle = $(this).parents(".resource-item").find(".heading").attr("title");
-    //var dTitle = $(".itemTitle").text().trim();
     var rTitle = $(this).find(".ga-download-resource-title").text().trim();
-    var dTitle = $(this).find(".ga-download-dataset-title").text().trim();
+    // var dTitle = $(this).find(".ga-download-dataset-title").text().trim();
+    var dTitle = analyticsInfo.datasetName;
     ga('send', 'event', 'resource', 'download', rTitle + " (" + dTitle +")");
     ga('send', 'event', 'dataset', 'resource-download', dTitle);
+
+    mixpanel.track("resource download", {
+        "resource name": rTitle,
+        "dataset name": dTitle,
+        "dataset id": analyticsInfo.datasetId,
+        "org name": analyticsInfo.organizationName,
+        "org id": analyticsInfo.organizationId,
+        "group names": analyticsInfo.groupNames,
+        "group ids": analyticsInfo.groupIds,
+        "is cod": analyticsInfo.isCod,
+        "is indicator": analyticsInfo.isIndicator
+    });
   });
 
   $('.ga-share').on('click', function(){
