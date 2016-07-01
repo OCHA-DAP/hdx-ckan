cc40e11bfd811199e27d907f42ddb5f2642e5052
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2016-07-01 17:07:51 +0300

    HDX-4247 sending also GA events for clicks

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/google-analytics.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/google-analytics.js
@@ -1,110 +1,127 @@
-$(function setUpSearchTracking() {
-    var formEl = $("#dataset-filter-form");
-    if (formEl.length > 0) {
-        var mixpanelMapping = {
-            'q': {
-                'name': 'search term',
-                'isList': false,
-                'mandatory': true
-            },
-            'tags': {
-                'name': 'tag filters',
-                'isList': true,
-                'mandatory': true
-            },
-            'res_format': {
-                'name': 'format filters',
-                'isList': true,
-                'mandatory': true
-            },
-            'organization': {
-                'name': 'org filters',
-                'isList': true,
-                'mandatory': true
-            },
-            'groups': {
-                'name': 'location filters',
-                'isList': true,
-                'mandatory': true
-            },
-            /*'ext_page_size': {
-                'name': 'items per page',
-                'isList': false,
-                'mandatory': false
-            },
-            'sort': {
-                'name': 'sorting',
-                'isList': false,
-                'mandatory': false
-            },*/
-            'ext_cod': {
-                'name': 'cod filter',
-                'isList': false,
-                'mandatory': true
-            }
-        };
+$(
+    /**
+     * We're only sending mixpanel events. GA deals with search differently / automatically
+     */
+    function setUpSearchTracking() {
+        var formEl = $("#dataset-filter-form");
+        if (formEl.length > 0) {
+            var mixpanelMapping = {
+                'q': {
+                    'name': 'search term',
+                    'isList': false,
+                    'mandatory': true
+                },
+                'tags': {
+                    'name': 'tag filters',
+                    'isList': true,
+                    'mandatory': true
+                },
+                'res_format': {
+                    'name': 'format filters',
+                    'isList': true,
+                    'mandatory': true
+                },
+                'organization': {
+                    'name': 'org filters',
+                    'isList': true,
+                    'mandatory': true
+                },
+                'groups': {
+                    'name': 'location filters',
+                    'isList': true,
+                    'mandatory': true
+                },
+                /*'ext_page_size': {
+                 'name': 'items per page',
+                 'isList': false,
+                 'mandatory': false
+                 },
+                 'sort': {
+                 'name': 'sorting',
+                 'isList': false,
+                 'mandatory': false
+                 },*/
+                'ext_cod': {
+                    'name': 'cod filter',
+                    'isList': false,
+                    'mandatory': true
+                }
+            };
 
-        var numberOfResults = parseInt($('#analytics-number-of-results').text().trim()) || 0;
+            var numberOfResults = parseInt($('#analytics-number-of-results').text().trim()) || 0;
 
-        var paramList = formEl.serializeArray();
-        var mixpanelEventMeta = {
-            "page title": analyticsInfo.pageTitle,
-            "number of results": numberOfResults
-            /*"org name": analyticsInfo.organizationName,
-            "org id": analyticsInfo.organizationId,
-            "group names": analyticsInfo.groupNames,
-            "group ids": analyticsInfo.groupIds*/
-        };
-        var sendTrackingEvent = false;
-        for (var i = 0; i < paramList.length; i++) {
-            var param = paramList[i];
-            var mappingInfo = mixpanelMapping[param.name];
-            var paramValue = param.value.trim();
-            if (mappingInfo && paramValue) {
-                populateMetadata(mixpanelEventMeta, mappingInfo, paramValue);
-                sendTrackingEvent = sendTrackingEvent || mappingInfo.mandatory;
+            var paramList = formEl.serializeArray();
+            var mixpanelEventMeta = {
+                "page title": analyticsInfo.pageTitle,
+                "number of results": numberOfResults
+                /*"org name": analyticsInfo.organizationName,
+                 "org id": analyticsInfo.organizationId,
+                 "group names": analyticsInfo.groupNames,
+                 "group ids": analyticsInfo.groupIds*/
+            };
+            var sendTrackingEvent = false;
+            for (var i = 0; i < paramList.length; i++) {
+                var param = paramList[i];
+                var mappingInfo = mixpanelMapping[param.name];
+                var paramValue = param.value.trim();
+                if (mappingInfo && paramValue) {
+                    populateMetadata(mixpanelEventMeta, mappingInfo, paramValue);
+                    sendTrackingEvent = sendTrackingEvent || mappingInfo.mandatory;
+                }
             }
-        }
-        if (sendTrackingEvent){
-            var reResult = /ext_search_source=([^&]+)(&|$)/.exec(location.href);
-            if (reResult && reResult.length > 1) {
-                mixpanelEventMeta["search box location"] = reResult[1];
+            if (sendTrackingEvent) {
+                var reResult = /ext_search_source=([^&]+)(&|$)/.exec(location.href);
+                if (reResult && reResult.length > 1) {
+                    mixpanelEventMeta["search box location"] = reResult[1];
+                }
+                else {
+                    mixpanelEventMeta["search box location"] = "in-page";
+                }
+                console.log(JSON.stringify(mixpanelEventMeta));
+                mixpanel.track("search", mixpanelEventMeta);
+
             }
             else {
-                mixpanelEventMeta["search box location"] = "in-page";
+                console.log("No mandatory properties found. Not sending search event to mixpanel.");
             }
-            console.log(JSON.stringify(mixpanelEventMeta));
-            mixpanel.track("search", mixpanelEventMeta);
         }
-        else {
-            console.log("No mandatory properties found. Not sending search event to mixpanel.");
-        }
-    }
 
-    /**
-     * Populates the object that is sent to mixpanel for one <form> parameter
-     * @param mixpanelEventMeta {Object} map of property-values to be sent to mixpanel
-     * @param mappingInfo {{name:string, isList: boolean, mandatory: boolean}} information about how the param should be formatted
-     * @param paramValue {string} the value of the <form> parameter
-     */
-    function populateMetadata(mixpanelEventMeta, mappingInfo, paramValue) {
-        if (mappingInfo.isList) {
-            mixpanelEventMeta[mappingInfo.name] = mixpanelEventMeta[mappingInfo.name] ?
-                mixpanelEventMeta[mappingInfo.name] : [];
-            mixpanelEventMeta[mappingInfo.name].push(paramValue);
-        }
-        else {
-            mixpanelEventMeta[mappingInfo.name] = paramValue;
+        /**
+         * Populates the object that is sent to mixpanel for one <form> parameter
+         * @param mixpanelEventMeta {Object} map of property-values to be sent to mixpanel
+         * @param mappingInfo {{name:string, isList: boolean, mandatory: boolean}} information about how the param should be formatted
+         * @param paramValue {string} the value of the <form> parameter
+         */
+        function populateMetadata(mixpanelEventMeta, mappingInfo, paramValue) {
+            if (mappingInfo.isList) {
+                mixpanelEventMeta[mappingInfo.name] = mixpanelEventMeta[mappingInfo.name] ?
+                    mixpanelEventMeta[mappingInfo.name] : [];
+                mixpanelEventMeta[mappingInfo.name].push(paramValue);
+            }
+            else {
+                mixpanelEventMeta[mappingInfo.name] = paramValue;
+            }
         }
     }
-});
+);
 
 $(
     function setupLinkClickEvent() {
         $(window.document).on("hdx-link-clicked", function(event, data){
 
-            var callback = function () {
-                if (data.destinationUrl){
+            var gaDeferred = new $.Deferred();
+            var mixpanelDeferred = new $.Deferred();
+
+            var wasCallbackExecuted = false;
+            /**
+             * The callback function opens the link after the analytics events are sent.
+             * We need to make sure that this function is called exactly once even if
+             * sending the analytics events to GA and/or Mixpanel failed
+             */
+            var originalAction = function () {
+                if (!wasCallbackExecuted && data.destinationUrl){
+                    wasCallbackExecuted = true;
+                    console.log("Executing original click action");
                     if (!data.target){
                         window.location.href = data.destinationUrl;
                     }
@@ -113,6 +130,7 @@ $(
                     }
                 }
             };
+
 
             var metadata = {
                 "page title": analyticsInfo.pageTitle
@@ -124,7 +142,26 @@ $(
                 metadata["link type"] = data.linkType;
             }
 
-            mixpanel.track("link click", metadata, callback);
+            mixpanel.track("link click", metadata, function(){
+                console.log("Finished sending click event to mixpanel");
+                mixpanelDeferred.resolve(true);
+            });
+            var eventCategory = (metadata["link type"] || "") + " link";
+            var eventAction = metadata["destionation url"];
+            var eventLabel = metadata["page title"] || "";
+            ga('send', 'event', eventCategory, eventAction, eventLabel, {
+                hitCallback: function () {
+                    console.log("Finished sending click event to GA");
+                    gaDeferred.resolve(true);
+                }
+            });
+            $.when(gaDeferred.promise(), mixpanelDeferred.promise()).done(originalAction);
+            setTimeout(function () {
+                console.log("Trying to do original action anyway in case it was not triggered normally");
+                originalAction();
+            }, 500);
+
+
         });
     }
 );
