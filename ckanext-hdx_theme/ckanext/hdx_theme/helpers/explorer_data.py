9db5edd9df2c09dd30c5b69639224da998a7493c
danmihaila <danmihaila@gmail.com>
2016-07-22 14:47:54 +0300

    restore explorer_data

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/explorer_data.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/explorer_data.py
@@ -1,8 +1,4 @@
 explorer_data = {
     'hdxsprint-displacement-tracking-matrix': ['1', '2'],
     'conflict-events': ['3', '4'],
-    'lcb-displaced': ['3', '4'],
-    'lake-chad-basin-baseline-population': ['3', '4'],
-    'lake-chad-basin-fts-appeal-data': ['3', '4'],
-    'market-monitor': ['3', '4'],
 }
