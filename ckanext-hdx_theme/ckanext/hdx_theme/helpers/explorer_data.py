d0b98febba49e390b31157da2d8b02c3e761912e
danmihaila <danmihaila@gmail.com>
2016-07-22 13:36:36 +0300

    lake chad and south sudan crises, update of datasets

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/explorer_data.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/explorer_data.py
@@ -1,4 +1,8 @@
 explorer_data = {
     'hdxsprint-displacement-tracking-matrix': ['1', '2'],
     'conflict-events': ['3', '4'],
+    'lcb-displaced': ['3', '4'],
+    'lake-chad-basin-baseline-population': ['3', '4'],
+    'lake-chad-basin-fts-appeal-data': ['3', '4'],
+    'market-monitor': ['3', '4'],
 }
