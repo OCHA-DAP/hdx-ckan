1d9d7c0bb9832abee0c387950c9ea3c6eaf8f6bf
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2016-07-25 14:37:21 +0300

    HDX-4246 dealing with case where there's no description

--- ckanext-hdx_package/ckanext/hdx_package/controllers/dataset_controller.py
+++ ckanext-hdx_package/ckanext/hdx_package/controllers/dataset_controller.py
@@ -756,9 +756,10 @@ class DatasetController(PackageController):
                           {'url': 'http://www.humanitarianresponse.info', 'name': 'HumanitarianResponse'},
                           {'url': 'http://fts.unocha.org', 'name': 'OCHA Financial Tracking Service'}]
 
-        #Constructing the email body
+        # Constructing the email body
+        notes = c.pkg_dict.get('notes') if c.pkg_dict.get('notes') else _('No description available')
         c.pkg_dict['social_mail_body'] = _('Description:%0D%0A') + h.markdown_extract(
-            c.pkg_dict.get('notes')) + ' %0D%0A'
+            notes) + ' %0D%0A'
 
         cnt_members_list = {}
         template_data = {}
