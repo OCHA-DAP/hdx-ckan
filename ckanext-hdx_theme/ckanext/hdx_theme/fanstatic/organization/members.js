df4309b7c05a15fc42b6d11e0f08632ac9f5a135
Alexandru Artimon <aalecs@gmail.com>
2016-08-24 11:37:54 +0300

    HDX-4898 - Membership org page: template changes

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/organization/members.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/organization/members.js
@@ -3,4 +3,49 @@ $(document).ready(function(){
     e.preventDefault();
     $(this).tab('show');
   });
+
+  $(".approval-actions .action-approve a").click(function(){
+    var role = $(this).attr('data-role');
+    var memberId = $(this).attr('data-member-id');
+    $.ajax({
+      method: "POST",
+      url: "/action/approve",
+      data: {
+        "member": memberId,
+        "role": role,
+        "approve": true
+      },
+      dataType: "json"
+    })
+      .done(function(){
+        $(this).parents('.approval-actions').find('.approved-message .approved-role').text(role);
+        $(this).parents('.approval-request').toggleClass('request-approved');
+      })
+      .fail(function(){
+        alert("Your request failed!")
+      });
+  });
+
+  $(".approval-actions .action-decline").click(function(){
+    var memberId = $(this).attr('data-member-id');
+    $.ajax({
+      method: "POST",
+      url: "/action/decline",
+      data: {
+        "member": memberId,
+        "reject": true
+      },
+      dataType: "json"
+    })
+      .done(function(){
+        $(this).parents('.approval-actions').find('.approved-message').text("Membership request declined!");
+        $(this).parents('.approval-request').toggleClass('request-approved');
+      })
+      .fail(function(){
+        alert("Your request failed!")
+      });
+
+    return false;
+  });
+
 });
