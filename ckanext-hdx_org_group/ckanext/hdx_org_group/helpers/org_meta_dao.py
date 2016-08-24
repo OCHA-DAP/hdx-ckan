14d6a49b767fbea4acb916687f0e478bde22c96c
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2016-08-24 14:52:42 +0300

    HDX-4899 making sure to user the org id instead of org name for permission check

--- ckanext-hdx_org_group/ckanext/hdx_org_group/helpers/org_meta_dao.py
+++ ckanext-hdx_org_group/ckanext/hdx_org_group/helpers/org_meta_dao.py
@@ -117,7 +117,7 @@ class OrgMetaDao(search_controller.HDXSearchController):
     def fetch_permissions(self):
         self._fetched_permissions = True
         self.allow_basic_user_info = self.__check_access('hdx_basic_user_info')
-        self.allow_req_membership = not ckan_helpers.user_in_org_or_group(self.id) and self.allow_basic_user_info
+        self.allow_req_membership = not ckan_helpers.user_in_org_or_group(self.org_dict['id']) and self.allow_basic_user_info
 
         self.allow_edit = self.__check_access('organization_update', {'id': self.id})
         self.allow_add_dataset = self.__check_access('package_create',
