7e48cc85a1b63ab44ce2f54ab0426a55bac295a2
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2017-01-09 19:09:19 +0200

    HDX-5068 - event listener for hxl preview "save" + showing "edit" only when user has the rights

--- ckanext-hdx_hxl_preview/ckanext/hdx_hxl_preview/fanstatic/hdx_hxl_preview.js
+++ ckanext-hdx_hxl_preview/ckanext/hdx_hxl_preview/fanstatic/hdx_hxl_preview.js
@@ -7,23 +7,26 @@ ckan.module('hdx-hxl-preview', function ($, _) {
                 element.style.height = (newHeight < maxHeight ? newHeight : maxHeight) + 'px';
                 console.log("hxlpreview: Height is " + element.style.height);
             };
+            var iframeEl = this.el[0];
+            var iBodyHeight;
             $('iframe.hxl-preview-iframe').load(function () {
-
-                    var self = this;
-                    var iBody = this.contentWindow.document.body;
+                    var iBody = iframeEl.contentWindow.document.body;
                     var iBodyHeight = iBody.offsetHeight;
-                    setHeight(self, iBodyHeight);
+                    setHeight(iframeEl, iBodyHeight);
                     // this.style.height = iBodyHeight + 'px';
-                    var checkResize = function () {
-                        if (iBody.offsetHeight != iBodyHeight) {
-                            iBodyHeight = iBody.offsetHeight;
-                            setHeight(self, iBodyHeight);
-                            // self.style.height = iBodyHeight + 'px';
-                        }
-                    };
-                    var timer = setInterval(checkResize, 500);
                 }
             );
+
+            var checkResize = function () {
+                var iBody = iframeEl.contentWindow.document.body;
+                if (iBody.offsetHeight != iBodyHeight) {
+                    iBodyHeight = iBody.offsetHeight;
+                    setHeight(iframeEl, iBodyHeight);
+                    // self.style.height = iBodyHeight + 'px';
+                }
+            };
+            var timer = setInterval(checkResize, 500);
+            console.log("hxlpreview: resize timer initialized");
         },
         options: {
             //placement: 'top',
--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/widget/popup/popup-hxl.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/widget/popup/popup-hxl.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
     $('#hxl-preview-edit-trigger').on('click', function(){
        var url = $('#hxl-preview-edit-url').text();
-       var iframe = '<iframe data-module="hdx-hxl-preview" style="width: 100%; border-style: none" src="' + url +
+       var iframe = '<iframe id="hxl-edit-iframe" data-module="hdx-hxl-preview" style="width: 100%; border-style: none" src="' + url +
            '" class="hxl-preview-iframe"> </iframe>';
        $('#hxl-preview-edit-container').html(iframe);
        $('#hxl-preview-popup').show();
+    });
+
+    $(document.body).on('hxlPreviewSaved', function(event){
+        console.log('hxlpreview: "saved" event received');
+        $('#hxl-view-iframe')[0].contentWindow.location.reload();
+        $('#hxl-preview-popup').hide();
     });
 
 
