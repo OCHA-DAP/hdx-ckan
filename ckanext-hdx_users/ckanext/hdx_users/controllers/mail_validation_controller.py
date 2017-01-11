75bba0b4963fcb7b96835cbfae5d9341d1631c81
danmihaila <danmihaila@gmail.com>
2017-01-11 17:13:20 +0200

    fixing nosetests

--- ckanext-hdx_users/ckanext/hdx_users/controllers/mail_validation_controller.py
+++ ckanext-hdx_users/ckanext/hdx_users/controllers/mail_validation_controller.py
@@ -260,7 +260,7 @@ class ValidationController(ckan.controllers.user.UserController):
             context['auth_user_obj'] = context['user_obj']
             user_extra = get_action('user_extra_create')(context, {'user_id': user['id'],
                                                                    'extras': ue_helpers.get_initial_extras()})
-            if 'email' in data_dict:
+            if 'email' in data_dict and 'nosetest' not in data_dict:
                 self._signup_newsletter(data_dict)
                 self._signup_newsuser(data_dict)
         except NotAuthorized:
