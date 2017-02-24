89ea4fdc3a2954678938f6304d1f3cebb197799a
Dan Mihaila <danmihaila@gmail.com>
2017-02-24 11:27:36 +0200

    New navigation updates closes #HDX-5130, #HDX-5129, #HDX-5128 - Search "el nino" -> Search suggestion link goes to https://test-data.humdata.org/user - search "lake chad" -> doesn't surface search suggestion - rename 'crisis page' in the search suggestion + dropdown

--- ckanext-hdx_search/ckanext/hdx_search/command.py
+++ ckanext-hdx_search/ckanext/hdx_search/command.py
@@ -37,62 +37,66 @@ class FeatureSearchCommand(p.toolkit.CkanCommand):
             print 'Error: command "{0}" not recognized'.format(cmd)
             print self.usage
 
+
 def buildIndex(path):
-        '''
-        Grab all Organizations, Groups, and Vocabulary Topics and write a
-        json file for lunr.js to search against
-        '''
-        index = list()
-        try:
-            crises = config.get('hdx.crises').split(", ")
-        except:
-            crises = ['ebola', 'nepal-earthquake']
-        groups = Session.execute('select name, title, is_organization from "group" where state=\'active\'')
-        for name, title, is_org in groups:
-            if is_org:
-                page_type = 'organisation'
-                url = h.url_for(controller='organization',
-                                    action='read',
-                                    id=name,
-                                    qualified=True)
-            else:
-                if name in crises:
-                    continue
-                page_type = "location"
-                
+    '''
+    Grab all Organizations, Groups, and Vocabulary Topics and write a
+    json file for lunr.js to search against
+    '''
+    index = list()
+    try:
+        crises = config.get('hdx.crises').split(", ")
+    except:
+        crises = ['ebola', 'nepal-earthquake']
+    groups = Session.execute('select name, title, is_organization from "group" where state=\'active\'')
+    for name, title, is_org in groups:
+        if is_org:
+            page_type = 'organisation'
+            url = h.url_for(controller='organization',
+                            action='read',
+                            id=name,
+                            qualified=True)
+        else:
+            if name in crises:
+                continue
+            page_type = "location"
 
-                url = h.url_for(controller='group',
-                                    action='read',
-                                    id=name,
-                                    qualified=True)
-            index.append({'title':title, 'url': url, 'type':page_type})
+            url = h.url_for(controller='group',
+                            action='read',
+                            id=name,
+                            qualified=True)
+        index.append({'title': title, 'url': url, 'type': page_type})
 
-        ## I hate this, but given the way we did crisis
-        ## I think this is the only way to go. Please update
-        ## when new crisis are added
+    ## I hate this, but given the way we did crisis
+    ## I think this is the only way to go. Please update
+    ## when new crisis are added
 
-        index.append({'title':'Ebola', 'url':h.url_for(controller='ckanext.hdx_crisis.controllers.ebola_custom_location_controller:EbolaCustomLocationController',
-                    action='read',qualified=True), 'type':'crisis'})
-        index.append({'title':'Nepal Earthquake', 'url':h.url_for(controller='ckanext.hdx_crisis.controllers.custom_location_controller:CustomLocationController',
-                    action='read', id='nepal-earthquake', qualified=True), 'type':'crisis'})
-        index.append({'title':'El Nino', 'url':h.url_for(controller='ckanext.hdx_pages.controllers.custom_page:PagesController',
-                    action='read_crisis', id='elnino', qualified=True), 'type':'crisis'})
+    index.append({'title': 'Ebola', 'url': h.url_for(
+        controller='ckanext.hdx_crisis.controllers.ebola_custom_location_controller:EbolaCustomLocationController',
+        action='read', qualified=True), 'type': 'event'})
+    index.append({'title': 'Nepal Earthquake', 'url': h.url_for(
+        controller='ckanext.hdx_crisis.controllers.custom_location_controller:CustomLocationController',
+        action='read', id='nepal-earthquake', qualified=True), 'type': 'event'})
+    index.append({'title': 'El Nino',
+                  'url': h.url_for(controller='ckanext.hdx_pages.controllers.custom_page:PagesController',
+                                   action='read_event', id='elnino', qualified=True), 'type': 'event'})
+    index.append({'title': 'Lake Chad',
+                  'url': h.url_for(controller='ckanext.hdx_pages.controllers.custom_page:PagesController',
+                                   ation='read_event', id='lake-chad', qualified=True), 'type': 'event'})
 
+    ## UNCOMMENT THIS TO ENABLE TOPIC PAGES AS WELL
+    # topic = Session.execute("select id from vocabulary where name='Topics'")
+    # topic = [id for id in topic]
+    # topics = Session.execute("select id, name from tag where vocabulary_id='%s'" % (topic[0][0]))
+    # for id, name in topics:
+    #     url = h.url_for(controller='tag',
+    #                             action='read',
+    #                             id=id,
+    #                             qualified=True)
+    #     index.append({'title':name.capitalize(), 'url': url, 'type': 'topic'})
 
-
-        ## UNCOMMENT THIS TO ENABLE TOPIC PAGES AS WELL
-        # topic = Session.execute("select id from vocabulary where name='Topics'")
-        # topic = [id for id in topic]
-        # topics = Session.execute("select id, name from tag where vocabulary_id='%s'" % (topic[0][0]))
-        # for id, name in topics:
-        #     url = h.url_for(controller='tag',
-        #                             action='read',
-        #                             id=id,
-        #                             qualified=True)
-        #     index.append({'title':name.capitalize(), 'url': url, 'type': 'topic'})
-
-        dir_path = os.path.abspath(path)
-        f = open(dir_path+'/feature-index.js', 'w')
-        file_body = json.dumps(index)
-        file_body = 'var feature_index='+file_body+';'
-        f.write(file_body)
+    dir_path = os.path.abspath(path)
+    f = open(dir_path + '/feature-index.js', 'w')
+    file_body = json.dumps(index)
+    file_body = 'var feature_index=' + file_body + ';'
+    f.write(file_body)
