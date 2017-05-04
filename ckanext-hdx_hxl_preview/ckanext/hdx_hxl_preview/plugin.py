b4d49682d0fe992702b38d3126a2afc77c0e015c
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2017-05-04 17:28:56 +0300

    renaming HXL Preview to Smart Charts

--- ckanext-hdx_hxl_preview/ckanext/hdx_hxl_preview/plugin.py
+++ ckanext-hdx_hxl_preview/ckanext/hdx_hxl_preview/plugin.py
@@ -51,13 +51,13 @@ class HdxHxlPreviewPlugin(plugins.SingletonPlugin):
 
         return {
             'name': 'hdx_hxl_preview',
-            'title': 'HXL Preview',
+            'title': 'Smart Charts',
             'filterable': False,
             'preview_enabled': True,
             'schema': schema,
             'requires_datastore': False,
             'iframed': False,
-            'default_title': _('HXL Preview')
+            'default_title': _('Smart Charts')
         }
 
     def setup_template_variables(self, context, data_dict):
