821be1d6357f2def1a5385f8c9d9b51c87b3844b
Alexandru Artimon <aalecs@gmail.com>
2017-06-08 15:52:03 +0300

    HDX-5312 - CKAN 2.6: Moving HDX Request data extension - UI

---
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/requestdata/remove-class.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    var div = document.getElementsByClassName('col-xs-12 paddingLeftHack paddingRightHack');
+    div[2].classList.remove('col-xs-12', 'paddingLeftHack', 'paddingRightHack');
+});
