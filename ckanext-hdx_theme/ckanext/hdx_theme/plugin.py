9dd9ef3c4b28c299ab9ac60e6e78b4d44ec43678
Dan Mihaila <danmihaila@gmail.com>
2017-07-04 14:16:24 +0300

    related #HDX-5398 - removing get_action from template as it is deprecated - removing any related_items reference from template (read_base)

--- ckanext-hdx_theme/ckanext/hdx_theme/plugin.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/plugin.py
@@ -224,6 +224,7 @@ class HDXThemePlugin(plugins.SingletonPlugin):
             'hdx_get_layer_info': hdx_helpers.hdx_get_layer_info,
             'hdx_get_carousel_list': hdx_helpers.hdx_get_carousel_list,
             'hdx_get_frequency_by_value': hdx_helpers.hdx_get_frequency_by_value,
+            'hdx_dataset_follower_count': hdx_helpers.hdx_dataset_follower_count,
         }
 
     def get_actions(self):
