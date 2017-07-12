91bcc1814a0d7b2720b6fae7df61d8da4c039f33
Dan Mihaila <danmihaila@gmail.com>
2017-07-12 18:03:15 +0300

    related to #HDX-5346 and #HDX-5419 - methodology behaves now similar with expected frequency data

--- ckanext-hdx_package/ckanext/hdx_package/plugin.py
+++ ckanext-hdx_package/ckanext/hdx_package/plugin.py
@@ -425,7 +425,8 @@ class HDXPackagePlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
             'file_types': [tk.get_validator('not_empty'), tk.get_converter('convert_to_extras')],
             'num_of_rows': [tk.get_validator('ignore_missing'), tk.get_validator('is_positive_integer'),
                             tk.get_converter('convert_to_extras')],
-            'data_update_frequency': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')]
+            'data_update_frequency': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')],
+            'methodology': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')]
         })
 
         schema.pop('license_id')
@@ -446,7 +447,8 @@ class HDXPackagePlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
             'file_types': [tk.get_converter('convert_from_extras'), tk.get_validator('not_empty')],
             'num_of_rows': [tk.get_converter('convert_from_extras'), tk.get_validator('ignore_missing'),
                             tk.get_validator('is_positive_integer')],
-            'data_update_frequency': [tk.get_validator('convert_from_extras'), tk.get_converter('ignore_missing')]
+            'data_update_frequency': [tk.get_validator('convert_from_extras'), tk.get_converter('ignore_missing')],
+            'methodology': [tk.get_validator('convert_from_extras'), tk.get_converter('ignore_missing')]
         })
 
 class HDXAnalyticsPlugin(plugins.SingletonPlugin):
