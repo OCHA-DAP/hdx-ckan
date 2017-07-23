0ae0b44be77032668797237f4ed5f416d1e01fb2
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2017-07-24 02:41:35 +0300

    HDX-5395 refresh solr based on both pageviews and downloads from MP

--- ckanext-hdx_theme/ckanext/hdx_theme/cli/cli.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/cli/cli.py
@@ -22,22 +22,88 @@ class CustomLessCompile(cli.CkanCommand):
 class AnalyticsChangesReindex(cli.CkanCommand):
     summary = 'Reindexing datasets that have changed due to analytics'
 
-    def command(self):
-        from ckanext.hdx_theme.util.jql import downloads_per_dataset
-        from ckan.lib.search import rebuild
+    def _find_potential_datasets_in_solr(self):
+        from ckan.lib.search.query import make_connection
+        dataseta_meta_map = {}
+        data_dict = {
+            'q': '*:*',
+            'fl': 'id pageviews_last_14_days total_res_downloads',
+            'fq': 'total_res_downloads:[1 TO *] OR pageviews_last_14_days: [1 TO *]',
+            'start': 0,
+            'rows': 1000000
+        }
+        try:
+            conn = make_connection(decode_dates=False)
+            search_result = conn.search(**data_dict)
+            dataseta_meta_map = {d['id']: {'pageviews': d.get('pageviews_last_14_days', 0),
+                                           'downloads': d.get('total_res_downloads', 0)}
+                                 for d in search_result.docs}
+        except Exception, e:
+            self.log.warn('Error in searching solr {}'.format(str(e)))
+
+        return dataseta_meta_map
+
+    def _find_potential_datasets_in_mixpanel(self):
+        from ckanext.hdx_theme.util.jql import downloads_per_dataset_all_cached, \
+            pageviews_per_dataset_last_14_days_cached
         from ckan.common import config
+
+        dataset_2_downloads_map = downloads_per_dataset_all_cached()
+        dataset_2_pageviews_map = pageviews_per_dataset_last_14_days_cached()
+
+        return dataset_2_pageviews_map, dataset_2_downloads_map
+
+    def _decide_which_datasets_need_update(self, solr_ds, pageviews_ds, downloads_ds):
+        '''
+        :param solr_ds:
+        :type solr_ds: dict
+        :param pageviews_ds:
+        :type pageviews_ds: dict
+        :param downloads_ds:
+        :type downloads_ds: dict
+        :return:
+        '''
+        ds_to_update = []
+
+        for id, meta_data in solr_ds.items():
+            pv_value = pageviews_ds.get(id, 0)
+            dw_value = downloads_ds.get(id, 0)
+
+            if pv_value != meta_data.get('pageviews') or dw_value != meta_data.get('downloads'):
+                ds_to_update.append(id)
+
+        for id in pageviews_ds:
+            if id and id not in solr_ds:
+                ds_to_update.append(id)
+
+        for id in downloads_ds:
+            if id and id not in solr_ds:
+                ds_to_update.append(id)
+
+        return ds_to_update
+
+
+    def command(self):
+
+        from ckan.lib.search import rebuild
+
         from ckan.logic import NotFound
 
-        hours_to_check = int(config.get('hdx.analytics.hours_to_check_for_refresh', 24))
-
         self._load_config()
+        # hours_to_check = int(config.get('hdx.analytics.hours_to_check_for_refresh', 24))
 
         self.log = logging.getLogger(__name__)
 
-        dataset_2_downloads_map = downloads_per_dataset(hours_to_check)
+        solr_ds = self._find_potential_datasets_in_solr()
+        pageviews_ds, downloads_ds = self._find_potential_datasets_in_mixpanel()
 
-        for dataset_id in dataset_2_downloads_map.keys():
-            self.log.info('Rebuilding index for dataset {}'.format(dataset_id))
+        ds_to_update = self._decide_which_datasets_need_update(solr_ds, pageviews_ds, downloads_ds)
+
+        total = len(ds_to_update)
+        self.log.info('Rebuilding index for {} datasets.'.format(total))
+
+        for idx, dataset_id in enumerate(ds_to_update):
+            self.log.info('Rebuilding index for dataset {}. {}/{}'.format(dataset_id, idx, total))
             try:
                 rebuild(dataset_id)
                 self.log.info('Done')
