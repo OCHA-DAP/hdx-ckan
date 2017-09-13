dcd5c641865e80d0cb96314ae5aba07a4bd509cf
Dan Mihaila <danmihaila@gmail.com>
2017-09-13 15:05:35 +0300

    HDX-5414

--- ckanext-hdx_package/ckanext/hdx_package/actions/delete.py
+++ ckanext-hdx_package/ckanext/hdx_package/actions/delete.py
@@ -10,27 +10,6 @@ log = logging.getLogger(__name__)
 
 
 def hdx_dataset_purge(context, data_dict):
-    # '''
-    # Permenantly and completely delete a dataset from HDX
-    # '''
-    # model = context['model']
-    # user = context['user']
-    #
-    # _check_access('package_delete', context, data_dict)
-    #
-    # dataset_ref = _get_or_bust(data_dict, 'id')
-    # dataset = model.Package.get(unicode(dataset_ref))
-    # if dataset is None:
-    #     raise NotFound('Dataset "{id}" was not found.'.format(id=dataset_ref))
-    #
-    # name = dataset.name
-    # rev = model.repo.new_revision()
-    # try:
-    #     dataset.purge()
-    #     model.repo.commit_and_remove()
-    #     return True
-    # except:
-    #     return False
     return dataset_purge(context, dict)
 
 
@@ -44,7 +23,33 @@ def dataset_purge(context, data_dict):
     pkg = model.Package.get(id)
     is_requested_data_type = _is_requested_data_type(pkg)
 
-    core_delete.dataset_purge(context, data_dict)
+    # core_delete.dataset_purge(context, data_dict)
+
+    from sqlalchemy import or_
+
+    context['package'] = pkg
+    if pkg is None:
+        raise NotFound('Dataset was not found')
+
+    _check_access('package_delete', context, data_dict)
+
+    members = model.Session.query(model.Member) \
+                   .filter(model.Member.table_id == pkg.id) \
+                   .filter(model.Member.table_name == 'package')
+    if members.count() > 0:
+        for m in members.all():
+            m.purge()
+
+    for r in model.Session.query(model.PackageRelationship).filter(
+            or_(model.PackageRelationship.subject_package_id == pkg.id,
+                model.PackageRelationship.object_package_id == pkg.id)).all():
+        r.purge()
+
+    pkg = model.Package.get(id)
+    # no new_revision() needed since there are no object_revisions created
+    # during purge
+    pkg.purge()
+    model.repo.commit_and_remove()
 
     if is_requested_data_type:
         toolkit.get_action("requestdata_request_delete_by_package_id")(context, {'package_id': id})
