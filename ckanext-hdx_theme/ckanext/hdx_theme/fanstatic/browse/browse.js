152157ca7ec0d5b43495382de2afba8c7f6731f2
Alexandru Artimon <aalecs@gmail.com>
2018-05-28 20:57:27 +0300

    HDX-5892 - /group page - modal are not displayed well

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/browse/browse.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/browse/browse.js
@@ -56,7 +56,7 @@ function prepareCountryList(countDatasets) {
 }
 
 function prepareMap(countDatasets, openNewWindow){
-  var closeTooltip, country, countryLayer, country_id, feature, featureClicked, first_letter, getStyle, highlightFeature, k, line, map, mapID, onEachFeature, openURL, popup, resetFeature, topLayer, topPane, v, _i, _j, _len, _len1, _ref;
+  var closeTooltip, country, countryLayer, country_id, feature, featureClicked, first_letter, getStyle, highlightFeature, k, line, map, mapID, onEachFeature, openURL, popup, resetFeature, topLayer, topPane, v, _i, _j, _len, _len1, _ref, closePopupTimeout;
   //mapID = 'yumiendo.ijchbik8';
   const openTarget = openNewWindow ? "_blank" : "_self";
   openURL = function(url) {
@@ -67,6 +67,7 @@ function prepareMap(countDatasets, openNewWindow){
   }, 100);
   highlightFeature = function(e) {
     var countryID, layer;
+    clearTimeout(closePopupTimeout);
     layer = e.target;
     countryID = layer.feature.id;
     layer.setStyle({
@@ -103,6 +104,9 @@ function prepareMap(countDatasets, openNewWindow){
     var layer;
     layer = e.target;
     layer.setStyle(getStyle(layer.feature));
+    closePopupTimeout = setTimeout(function() {
+      map.closePopup();
+    }, 200);
   };
   featureClicked = function(e) {
     var code, layer;
