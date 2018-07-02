6d4adae64cfd3e9f300ea4e9464c88e9c433934b
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2018-07-03 01:07:29 +0300

    HDX-5737 updating translations from 2.6 to 2.7

--- ckan/public/base/i18n/en_AU.js
+++ ckan/public/base/i18n/en_AU.js
@@ -1,147 +1,156 @@
 {
   "": {
-    "domain": "ckan",
-    "lang": "en_AU",
+    "domain": "ckan", 
+    "lang": "en_AU", 
     "plural-forms": "nplurals=2; plural=(n != 1);"
-  },
+  }, 
+  "Add Filter": [
+    null, 
+    "Add Filter"
+  ], 
   "An Error Occurred": [
-    null,
+    null, 
     "An Error Occurred"
-  ],
+  ], 
   "Are you sure you want to perform this action?": [
-    null,
+    null, 
     "Are you sure you want to perform this action?"
-  ],
+  ], 
   "Cancel": [
-    null,
+    null, 
     "Cancel"
-  ],
+  ], 
   "Confirm": [
-    null,
+    null, 
     "Confirm"
-  ],
+  ], 
   "Edit": [
-    null,
+    null, 
     "Edit"
-  ],
+  ], 
   "Failed to load data API information": [
-    null,
+    null, 
     "Failed to load data API information"
-  ],
+  ], 
   "File": [
-    null,
+    null, 
     "File"
-  ],
+  ], 
+  "Filters": [
+    null, 
+    "Filters"
+  ], 
   "Follow": [
-    null,
+    null, 
     "Follow"
-  ],
+  ], 
+  "Graph": [
+    null, 
+    "Graph"
+  ], 
   "Hide": [
-    null,
+    null, 
     "Hide"
-  ],
+  ], 
   "Image": [
-    null,
+    null, 
     "Image"
-  ],
+  ], 
   "Input is too short, must be at least one character": [
-    null,
-    "Input is too short, must be at least one character"
-  ],
+    "Input is too short, must be at least %(num)d characters", 
+    "", 
+    ""
+  ], 
   "Link": [
-    null,
+    null, 
     "Link"
-  ],
+  ], 
   "Link to a URL on the internet (you can also link to an API)": [
-    null,
+    null, 
     "Link to a URL on the internet (you can also link to an API)"
-  ],
+  ], 
   "Loading...": [
-    null,
+    null, 
     "Loading..."
-  ],
+  ], 
+  "Map": [
+    null, 
+    "Map"
+  ], 
   "No matches found": [
-    null,
+    null, 
     "No matches found"
-  ],
+  ], 
   "Please Confirm Action": [
-    null,
+    null, 
     "Please Confirm Action"
-  ],
+  ], 
   "Remove": [
-    null,
+    null, 
     "Remove"
-  ],
-  "Reorder resource view": [
-    null,
-    "Reorder resource view"
-  ],
-  "Reorder resources": [
-    null,
-    "Reorder resources"
-  ],
+  ], 
   "Resource uploaded": [
-    null,
+    null, 
     "Resource uploaded"
-  ],
+  ], 
   "Save order": [
-    null,
+    null, 
     "Save order"
-  ],
+  ], 
   "Saving...": [
-    null,
+    null, 
     "Saving..."
-  ],
+  ], 
   "Show more": [
-    null,
+    null, 
     "Show more"
-  ],
+  ], 
   "Start typing…": [
-    null,
+    null, 
     "Start typing…"
-  ],
+  ], 
   "There are unsaved modifications to this form": [
-    null,
+    null, 
     "There are unsaved modifications to this form"
-  ],
+  ], 
   "There is no API data to load for this resource": [
-    null,
+    null, 
     "There is no API data to load for this resource"
-  ],
+  ], 
   "URL": [
-    null,
+    null, 
     "URL"
-  ],
+  ], 
   "Unable to authenticate upload": [
-    null,
+    null, 
     "Unable to authenticate upload"
-  ],
+  ], 
   "Unable to get data for uploaded file": [
-    null,
-    "Unable to get data for uploaded file"
-  ],
+    null, 
+    "Unable to get data for uploaded resource"
+  ], 
   "Unable to upload file": [
-    null,
-    "Unable to upload file"
-  ],
+    null, 
+    "Unable to upload resource"
+  ], 
   "Unfollow": [
-    null,
+    null, 
     "Unfollow"
-  ],
+  ], 
   "Upload": [
-    null,
+    null, 
     "Upload"
-  ],
+  ], 
   "Upload a file": [
-    null,
-    "Upload a file"
-  ],
+    null, 
+    "Upload a resource"
+  ], 
   "Upload a file on your computer": [
-    null,
+    null, 
     "Upload a file on your computer"
-  ],
+  ], 
   "You are uploading a file. Are you sure you want to navigate away and stop this upload?": [
-    null,
+    null, 
     "You are uploading a file. Are you sure you want to navigate away and stop this upload?"
   ]
 }
