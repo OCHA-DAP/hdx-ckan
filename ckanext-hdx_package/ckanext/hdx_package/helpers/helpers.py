45f7d11e39783876d402c5ea2d48a5e024864a6f
Dan Mihaila <danmihaila@gmail.com>
2018-07-05 17:44:27 +0300

    fixing nose tests for caching

--- ckanext-hdx_package/ckanext/hdx_package/helpers/helpers.py
+++ ckanext-hdx_package/ckanext/hdx_package/helpers/helpers.py
@@ -41,9 +41,9 @@ def build_additions(groups):
     for g in groups:
         try:
             if 'name' in g:
-                grp_list = cached_group_iso_to_title()
-                country = grp_list[g.get('name')] if g.get('name') in grp_list else g.get('name')
-                countries.append(country)
+                # grp_list = cached_group_iso_to_title()
+                # country = grp_list[g.get('name')] if g.get('name') in grp_list else g.get('name')
+                countries.append(cached_group_iso_to_title()[g.get('name')])
         except Exception, e:
             ex_msg = e.message if hasattr(e, 'message') else str(e)
             log.error(ex_msg)
