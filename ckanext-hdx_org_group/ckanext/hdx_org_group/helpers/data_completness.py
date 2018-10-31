d00f92c8e748daa2c57ada7e7061483e6fcbe3b9
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2018-10-31 15:42:38 +0200

    HDX-6186 integrating completeness from overrides

--- ckanext-hdx_org_group/ckanext/hdx_org_group/helpers/data_completness.py
+++ ckanext-hdx_org_group/ckanext/hdx_org_group/helpers/data_completness.py
@@ -4,6 +4,7 @@ import ckan.logic as logic
 
 from ckanext.hdx_package.helpers.freshness_calculator import FreshnessCalculator, FRESHNESS_PROPERTY
 
+GOODNESS_PROPERTY = 'is_good'
 
 class DataCompletness(object):
 
@@ -105,11 +106,19 @@ class DataCompletness(object):
 
     @staticmethod
     def __add_metadata_overrides(dataset, overrides_map):
+        '''
+        This needs to be called only after freshness is populated on the dataset
+
+        :param dataset:
+        :param overrides_map:
+        :return:
+        '''
         override = overrides_map.get(dataset['name'])
 
         if override:
-            dataset['completeness'] = override.get('display_state') == 'complete'
             dataset['completeness_comment'] = override.get('comments')
+            dataset['is_complete'] = override.get('display_state') == 'complete'
+            dataset[GOODNESS_PROPERTY] = dataset.get(FRESHNESS_PROPERTY, False) and dataset.get('is_complete')
 
     @staticmethod
     def __add_dataset_to_map(map, dataset):
@@ -129,80 +138,80 @@ class DataCompletness(object):
     def __calculate_stats_for_dataseries(ds):
         datasets = ds.get('datasets', [])
         total_datasets_num = len(datasets)
-        fresh_datasets_num = reduce(lambda acc, item: acc + (1 if item.get(FRESHNESS_PROPERTY) else 0),
+        good_datasets_num = reduce(lambda acc, item: acc + (1 if item.get(GOODNESS_PROPERTY) else 0),
                                     datasets, 0)
         ds['stats'] = {
             'total_datasets_num': total_datasets_num,
-            'fresh_datasets_num': fresh_datasets_num,
+            'good_datasets_num': good_datasets_num,
             'state': 'empty' if total_datasets_num == 0
-                                else 'not_fresh' if fresh_datasets_num < total_datasets_num else 'fresh'
+                                else 'not_good' if good_datasets_num < total_datasets_num else 'good'
         }
 
     @staticmethod
     def __calculate_stats_for_category(category, category_dataset_map):
         total_datasets_num = len(category_dataset_map)
-        fresh_datasets_num = reduce(lambda acc, item: acc + (1 if item.get(FRESHNESS_PROPERTY) else 0),
+        good_datasets_num = reduce(lambda acc, item: acc + (1 if item.get(GOODNESS_PROPERTY) else 0),
                                     category_dataset_map.values(), 0)
 
-        dataset_freshness_percentage = 0 if total_datasets_num == 0 else float(fresh_datasets_num) / total_datasets_num
+        dataset_goodness_percentage = 0 if total_datasets_num == 0 else float(good_datasets_num) / total_datasets_num
 
         dataseries = category.get('data_series', [])
         total_dataseries_num  = len(dataseries)
-        fresh_dataseries_num = 0
-        not_fresh_dataseries_num = 0
+        good_dataseries_num = 0
+        not_good_dataseries_num = 0
         empty_dataseries_num = 0
 
         for ds in dataseries:
             state = ds.get('stats', {}).get('state')
             if state == 'empty':
                 empty_dataseries_num += 1
-            elif state == 'not_fresh':
-                not_fresh_dataseries_num += 1
-            elif state == 'fresh':
-                fresh_dataseries_num += 1
+            elif state == 'not_good':
+                not_good_dataseries_num += 1
+            elif state == 'good':
+                good_dataseries_num += 1
 
-        dataseries_fresh_percentage = 0 if total_dataseries_num == 0 else \
-                        float(fresh_dataseries_num) / total_dataseries_num
-        dataseries_not_fresh_percentage = 0 if total_dataseries_num == 0 else \
-            float(not_fresh_dataseries_num) / total_dataseries_num
+        dataseries_good_percentage = 0 if total_dataseries_num == 0 else \
+                        float(good_dataseries_num) / total_dataseries_num
+        dataseries_not_good_percentage = 0 if total_dataseries_num == 0 else \
+            float(not_good_dataseries_num) / total_dataseries_num
         dataseries_empty_percentage = 0 if total_dataseries_num == 0 else \
             float(empty_dataseries_num) / total_dataseries_num
 
         category['stats'] = {
             'total_datasets_num': total_datasets_num,
-            'fresh_datasets_num': fresh_datasets_num,
-            'dataset_freshness_percentage': dataset_freshness_percentage,
+            'good_datasets_num': good_datasets_num,
+            'dataset_goodness_percentage': dataset_goodness_percentage,
             'total_dataseries_num': total_dataseries_num,
-            'fresh_dataseries_num': fresh_dataseries_num,
-            'not_fresh_dataseries_num': not_fresh_dataseries_num,
+            'good_dataseries_num': good_dataseries_num,
+            'not_good_dataseries_num': not_good_dataseries_num,
             'empty_dataseries_num': empty_dataseries_num,
-            'dataseries_fresh_percentage': dataseries_fresh_percentage,
-            'dataseries_not_fresh_percentage': dataseries_not_fresh_percentage,
+            'dataseries_good_percentage': dataseries_good_percentage,
+            'dataseries_not_good_percentage': dataseries_not_good_percentage,
             'dataseries_empty_percentage': dataseries_empty_percentage,
             'state': 'empty' if total_dataseries_num == 0 or empty_dataseries_num > 0
-                                else 'not_fresh' if fresh_dataseries_num < total_dataseries_num else 'fresh',
+                                else 'not_good' if good_dataseries_num < total_dataseries_num else 'good',
         }
 
         state = category['stats']['state']
-        category['stats']['state_flag'] = 'blue' if state == 'fresh' else 'red' if state == 'empty' else ''
+        category['stats']['state_flag'] = 'blue' if state == 'good' else 'red' if state == 'empty' else ''
 
     @staticmethod
     def __calculate_stats_general(config, all_dataset_map, org_map):
         categories = config.get('categories', [])
         total_dataseries_num = reduce(
             lambda acc, item: acc + item.get('stats', {}).get('total_dataseries_num', 0), categories, 0)
-        fresh_dataseries_num = reduce(
-            lambda acc, item: acc + item.get('stats', {}).get('fresh_dataseries_num', 0), categories, 0)
-        not_fresh_dataseries_num = reduce(
-            lambda acc, item: acc + item.get('stats', {}).get('not_fresh_dataseries_num', 0), categories, 0)
+        good_dataseries_num = reduce(
+            lambda acc, item: acc + item.get('stats', {}).get('good_dataseries_num', 0), categories, 0)
+        not_good_dataseries_num = reduce(
+            lambda acc, item: acc + item.get('stats', {}).get('not_good_dataseries_num', 0), categories, 0)
         empty_dataseries_num = reduce(
             lambda acc, item: acc + item.get('stats', {}).get('empty_dataseries_num', 0), categories, 0)
 
-        dataseries_fresh_percentage = 0 if total_dataseries_num == 0 else \
-            float(fresh_dataseries_num) / total_dataseries_num
+        dataseries_good_percentage = 0 if total_dataseries_num == 0 else \
+            float(good_dataseries_num) / total_dataseries_num
 
-        dataseries_not_fresh_percentage = 0 if total_dataseries_num == 0 else \
-            float(not_fresh_dataseries_num) / total_dataseries_num
+        dataseries_not_good_percentage = 0 if total_dataseries_num == 0 else \
+            float(not_good_dataseries_num) / total_dataseries_num
 
         dataseries_empty_percentage = 0 if total_dataseries_num == 0 else \
             float(empty_dataseries_num) / total_dataseries_num
@@ -211,18 +220,18 @@ class DataCompletness(object):
             'total_datasets_num': len(all_dataset_map),
             'org_num': len(org_map),
             'total_dataseries_num': total_dataseries_num,
-            'fresh_dataseries_num': fresh_dataseries_num,
-            'not_fresh_dataseries_num': not_fresh_dataseries_num,
+            'good_dataseries_num': good_dataseries_num,
+            'not_good_dataseries_num': not_good_dataseries_num,
             'empty_dataseries_num': empty_dataseries_num,
-            'dataseries_fresh_percentage': dataseries_fresh_percentage,
-            'dataseries_not_fresh_percentage': dataseries_not_fresh_percentage,
+            'dataseries_good_percentage': dataseries_good_percentage,
+            'dataseries_not_good_percentage': dataseries_not_good_percentage,
             'dataseries_empty_percentage': dataseries_empty_percentage,
             'state': 'empty' if total_dataseries_num == 0 or empty_dataseries_num > 0
-                                else 'not_fresh' if fresh_dataseries_num < total_dataseries_num else 'fresh'
+                                else 'not_good' if good_dataseries_num < total_dataseries_num else 'good'
 
         }
 
         state = config['stats']['state']
-        config['stats']['state_flag'] = 'blue' if state == 'fresh' else 'red' if state == 'empty' else ''
+        config['stats']['state_flag'] = 'blue' if state == 'good' else 'red' if state == 'empty' else ''
 
 
