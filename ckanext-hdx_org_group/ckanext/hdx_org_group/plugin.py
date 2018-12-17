adef8d2899dce6640fee60c69511ef3ff58d020a
Dan Mihaila <danmihaila@gmail.com>
2018-12-17 12:57:03 +0200

    HDX-6263 - add flag for featured section

--- ckanext-hdx_org_group/ckanext/hdx_org_group/plugin.py
+++ ckanext-hdx_org_group/ckanext/hdx_org_group/plugin.py
@@ -332,6 +332,7 @@ class HDXGroupPlugin(plugins.SingletonPlugin, lib_plugins.DefaultGroupForm):
                        'custom_loc': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')],
                        'customization': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')],
                        'activity_level': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')],
+                       'featured_section': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')],
                        'key_figures': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')],
                        'data_completeness': [tk.get_validator('ignore_missing'), tk.get_converter('convert_to_extras')]
                        })
@@ -353,6 +354,11 @@ class HDXGroupPlugin(plugins.SingletonPlugin, lib_plugins.DefaultGroupForm):
         schema.update({'customization': [tk.get_converter('convert_from_extras'), tk.get_validator('ignore_missing')]})
         schema.update({
             'activity_level': [
+                tk.get_converter('convert_from_extras'),
+                tk.get_validator('ignore_missing')]
+        })
+        schema.update({
+            'featured_section': [
                 tk.get_converter('convert_from_extras'),
                 tk.get_validator('ignore_missing')]
         })
