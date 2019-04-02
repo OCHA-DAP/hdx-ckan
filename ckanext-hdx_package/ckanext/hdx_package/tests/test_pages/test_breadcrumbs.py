b89aa54e85cb2b7b23434aa827533f2779f9110a
Alexandru Artimon <aalecs@gmail.com>
2019-04-02 17:51:13 +0300

    HDX-6427 - add/register breadcrumb of CKAN (breadcrumb gets displayed in search)          - fix tests

--- ckanext-hdx_package/ckanext/hdx_package/tests/test_pages/test_breadcrumbs.py
+++ ckanext-hdx_package/ckanext/hdx_package/tests/test_pages/test_breadcrumbs.py
@@ -6,11 +6,11 @@ Created on Nov 12, 2014
 
 import logging as logging
 
-import ckan.lib.helpers as h
-
 import ckanext.hdx_theme.tests.hdx_test_base as hdx_test_base
-import ckanext.hdx_theme.tests.hdx_test_with_inds_and_orgs as hdx_test_with_inds_and_orgs
 import ckanext.hdx_theme.tests.hdx_test_util as hdx_test_util
+import ckanext.hdx_theme.tests.hdx_test_with_inds_and_orgs as hdx_test_with_inds_and_orgs
+
+import ckan.lib.helpers as h
 
 log = logging.getLogger(__name__)
 
@@ -25,9 +25,9 @@ class TestBreadcrumbs(hdx_test_with_inds_and_orgs.HDXWithIndsAndOrgsTest):
         result = self.app.get(url)
         page = str(result.response)
 
-        begin_str = '<ol class="breadcrumb">'
+        begin_str = '<ol class="breadcrumb" vocab="https://schema.org/" typeof="BreadcrumbList">'
         end_str = '</ol>'
-        search_strings = ['/dataset?ext_indicator=1', 'Test Indicator 1', 'test_indicator_1']
+        search_strings = ['/dataset', 'Test Indicator 1', 'test_indicator_1']
 
         hdx_test_util.are_strings_in_text(page, search_strings, begin_str, end_str)
 
@@ -36,7 +36,7 @@ class TestBreadcrumbs(hdx_test_with_inds_and_orgs.HDXWithIndsAndOrgsTest):
         result = self.app.get(url)
         page = str(result.response)
 
-        begin_str = '<ol class="breadcrumb">'
+        begin_str = '<ol class="breadcrumb" vocab="https://schema.org/" typeof="BreadcrumbList">'
         end_str = '</ol>'
         search_strings = ['/dataset', 'Test Dataset 1', 'test_dataset_1']
 
