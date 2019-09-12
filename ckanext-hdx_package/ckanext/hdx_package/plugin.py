4708d09f403e1e391318bbe0cb8b6982e7764c09
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2019-09-12 15:31:37 +0300

    HDX-6597 adding helper for displaying formatted overdue date

--- ckanext-hdx_package/ckanext/hdx_package/plugin.py
+++ ckanext-hdx_package/ckanext/hdx_package/plugin.py
@@ -315,6 +315,7 @@ class HDXPackagePlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
             'filesize_format': hdx_helpers.filesize_format,
             'generate_mandatory_fields': hdx_helpers.generate_mandatory_fields,
             'hdx_check_add_data': hdx_helpers.hdx_check_add_data,
+            'hdx_get_due_overdue_date': hdx_helpers.hdx_get_due_overdue_date,
             'hdx_get_last_modification_date': hdx_helpers.hdx_get_last_modification_date
         }
 
