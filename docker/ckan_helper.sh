68d62b07ba3ca07abf097f78ac6179a0a2764dd7
Serban Teodorescu <teodorescu.serban@gmail.com>
2019-11-14 11:39:59 +0200

    [HDX-6681] all s3 links should be over https

--- docker/ckan_helper.sh
+++ docker/ckan_helper.sh
@@ -18,7 +18,7 @@ if [ "$S3_GENERATE" == "yes" ]; then
     SF=${SF}'ckanext.s3filestore.aws_access_key_id = '${AWS_ACCESS_KEY_ID}'\n'
     SF=${SF}'ckanext.s3filestore.aws_secret_access_key = '${AWS_SECRET_ACCESS_KEY}'\n'
     SF=${SF}'ckanext.s3filestore.aws_bucket_name = '${AWS_BUCKET_NAME}'\n'
-    SF=${SF}'ckanext.s3filestore.host_name = http://s3.'${REGION_NAME}'.amazonaws.com\n'
+    SF=${SF}'ckanext.s3filestore.host_name = https://s3.'${REGION_NAME}'.amazonaws.com\n'
     SF=${SF}'ckanext.s3filestore.region_name= '${REGION_NAME}'\n'
     SF=${SF}'ckanext.s3filestore.signature_version = s3v4\n'
     SF=${SF}$(cat /srv/ckan/common-config-ini.txt | grep -E "^ckan.plugins =")' s3filestore\n'
