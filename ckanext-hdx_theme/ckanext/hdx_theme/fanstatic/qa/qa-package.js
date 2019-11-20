73f2b63150a8ef2e50333570fc474020a08f97d7
Alexandru Artimon <aalecs@gmail.com>
2019-11-20 21:26:10 +0200

    HDX-6656 - [ECHO] UI

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/qa/qa-package.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/qa/qa-package.js
@@ -1,6 +1,8 @@
 $(document).ready(() => {
   $(".qa-package-item").on("click", (ev) => {
     $('.qa-package-details').hide();
+    $('.qa-package-item.open').removeClass('open');
+    $(ev.currentTarget).addClass('open');
     let index = $(ev.currentTarget).attr('data-index');
     $(`#qa-package-details-${index}`).show();
   });
