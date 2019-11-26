a24ef3e6ea835b520c61b1b94b5b06fa2f6d7c8f
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2019-11-26 17:16:30 +0200

    HDX-6657 blocking downloads for quarantine in new s3 plugin

--- ckanext-hdx_package/ckanext/hdx_package/helpers/download_wrapper.py
+++ ckanext-hdx_package/ckanext/hdx_package/helpers/download_wrapper.py
@@ -1,17 +1,19 @@
-import ckan.controllers.package as package_controller
+# import ckan.controllers.package as package_controller
+import ckanext.s3filestore.controller as s3_resource_controller
 
 from ckanext.hdx_package.helpers.analytics import resource_download_with_analytics
 from ckanext.hdx_package.helpers.quarantine import resource_download_with_quarantine_check
 
-
+Controller = s3_resource_controller.S3Controller
 before_download_functions = [resource_download_with_quarantine_check,resource_download_with_analytics]
+
 
 def wrap_resource_download_function():
     '''
     Changes the original resource_download function from the package controller with a version that
     wraps the original function but also enqueues the tracking events
     '''
-    original_resource_download = package_controller.PackageController.resource_download
+    original_resource_download = Controller.resource_download
 
     def new_resource_download(self, id, resource_id, filename=None):
 
@@ -20,4 +22,4 @@ def wrap_resource_download_function():
 
         return original_resource_download(self, id, resource_id, filename)
 
-    package_controller.PackageController.resource_download = new_resource_download
+    Controller.resource_download = new_resource_download
