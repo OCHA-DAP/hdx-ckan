199b6f0c2ff09cac66e851a1e7c07c78b1bc9699
Serban Teodorescu <teodorescu.serban@gmail.com>
2020-01-08 15:28:55 +0200

    stop caring about /srv/cache

--- docker/ckan_helper.sh
+++ docker/ckan_helper.sh
@@ -43,7 +43,7 @@ lunr_dir=/srv/ckan/ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/search/lunr
 [ -f $lunr_dir/feature-index.js ] || touch $lunr_dir/feature-index.js
 
 # make sure cache dir permissions are correct
-chown -R www-data:www-data ${HDX_CACHE_DIR}
+#chown -R www-data:www-data ${HDX_CACHE_DIR}
 
 #python /srv/ckan/docker/helper.py
 
