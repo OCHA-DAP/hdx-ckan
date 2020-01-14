bb01c4bcc2a1cbe156d4e03111bfc8df665e1e6b
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2020-01-14 11:02:05 +0200

    HDX-6721 changing bucket name logic for logs

--- ckanext-hdx_search/ckanext/hdx_search/helpers/qa_s3.py
+++ ckanext-hdx_search/ckanext/hdx_search/helpers/qa_s3.py
@@ -12,5 +12,5 @@ log = logging.getLogger(__name__)
 class LogS3(BaseS3Uploader):
 
     def get_s3_bucket(self, bucket_name):
-        self.bucket_name = config.get('hdx.qa.s3.log_bucket_name', 'hdx-ckan-filestore-dev-log')
+        self.bucket_name = config.get('ckanext.s3filestore.aws_bucket_name', 'hdx-ckan-filestore-dev') + '-log'
         return super(LogS3, self).get_s3_bucket(bucket_name)
