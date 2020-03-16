3b841baf21e6c37b3b990ee0d30758ab6db27e18
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2020-03-16 20:02:19 +0200

    HDX-6854 Separating extra tabs into a dropdown menu

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/widget/popup/popup.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/widget/popup/popup.js
@@ -2,7 +2,7 @@ $(document).ready(function(e){
     $(".popup:not(.popup-close-only-x)").on("click", function(){
        $(this).hide();
     });
-    $(".popup .content").on("click", function(e){
+    $(".popup:not(.popup-close-only-x) .content").on("click", function(e){
         e.stopPropagation(); //do not propagate click
     });
     $(".popup i.close").on("click", function(e){
