c08d9f9ff5e4420a92df009c97d23046a12b4ec4
Dan Mihaila <danmihaila@gmail.com>
2020-04-01 15:27:27 +0300

    HDX-6874 refactor get page list into a helper

--- ckanext-hdx_package/ckanext/hdx_package/controllers/dataset_controller.py
+++ ckanext-hdx_package/ckanext/hdx_package/controllers/dataset_controller.py
@@ -14,6 +14,7 @@ from ckanext.hdx_theme.helpers import helpers as hdx_helpers
 from ckanext.hdx_theme.util.jql import downloads_per_dataset_per_week_last_24_weeks_cached
 from ckanext.hdx_theme.util.light_redirect import check_redirect_needed
 from ckanext.hdx_theme.util.mail import simple_validate_email
+import ckanext.hdx_package.helpers.custom_pages as cp_h
 from pylons import config
 
 import ckan.authz as authz
@@ -750,11 +751,7 @@ class DatasetController(PackageController):
         c.stats_downloads_last_weeks = downloads_last_weeks
 
         #tags&custom_pages
-        _page_list = []
-        for tag in c.pkg_dict.get('tags'):
-            _list = get_action('page_list_by_tag_id')(context, {'id': tag.get('id')})
-            _page_list.extend(_list)
-        c.pkg_dict['page_list'] = {v['id']: v for v in _page_list}.values()
+        c.pkg_dict['page_list'] = cp_h.hdx_get_page_list_for_dataset(context, c.pkg_dict)
 
         _dataset_preview = None
         if 'resources' in c.pkg_dict:
--- ckanext-hdx_package/ckanext/hdx_package/views/light_dataset.py
+++ ckanext-hdx_package/ckanext/hdx_package/views/light_dataset.py
@@ -6,6 +6,7 @@ import ckan.plugins.toolkit as tk
 from ckan.common import _, config, g, request
 
 import ckanext.hdx_package.helpers.analytics as analytics
+import ckanext.hdx_package.helpers.custom_pages as cp_h
 from ckanext.hdx_search.controller_logic.search_logic import SearchLogic
 
 from ckanext.hdx_theme.util.light_redirect import check_redirect_needed
@@ -36,7 +37,7 @@ def read(id):
 
     dataset_dict = get_action('package_show')(context, data_dict)
     analytics_dict = _compute_analytics(dataset_dict)
-    dataset_dict['page_list'] = _compute_events(context, dataset_dict)
+    dataset_dict['page_list'] = cp_h.hdx_get_page_list_for_dataset(context, dataset_dict)
 
     template_data = {
         'dataset_dict': dataset_dict,
@@ -73,13 +74,6 @@ def _compute_analytics(dataset_dict):
     result['analytics_dataset_availability'] = analytics.dataset_availability(dataset_dict)
     return result
 
-
-def _compute_events(context, dataset_dict):
-    _page_list = []
-    for tag in dataset_dict.get('tags'):
-        _list = get_action('page_list_by_tag_id')(context, {'id': tag.get('id')})
-        _page_list.extend(_list)
-    return {v['id']: v for v in _page_list}.values()
 
 hdx_light_search.add_url_rule(u'', view_func=search)
 hdx_light_dataset.add_url_rule(u'', view_func=search)
