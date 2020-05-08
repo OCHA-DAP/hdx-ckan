f3ebeb567e3002fa77bc11682dd39a458aa7ca57
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2020-05-08 20:00:04 +0300

    HDX-6914 new fields and logic to support resource grouping

--- ckanext-hdx_package/ckanext/hdx_package/helpers/date_helper.py
+++ ckanext-hdx_package/ckanext/hdx_package/helpers/date_helper.py
@@ -4,48 +4,73 @@ import ckan.plugins.toolkit as tk
 
 Invalid = tk.Invalid
 _ = tk._
+h = tk.h # helpers
 
 
-def daterange_parser(daterange_string, for_solr):
-    '''
+class DaterangeParser(object):
 
-    :param daterange_string: string representing a daterange
-    :type daterange_string: basestring
-    :return:  returns a tuple of strings (star,end) if format is correct, otherwise raises Invalid
-    :rtype: tuple(str,str)
-    '''
+    def __init__(self, daterange_string):
+        '''
+        :param daterange_string: string representing a daterange
+        :type daterange_string: basestring
+        '''
 
-    format_err_msg = _('daterange does not have the format: [start_datetime TO end_datetime]')
-    empty_err_msg = _('daterange does not have the format: [start_datetime TO end_datetime]')
-    start_date_err_msg = _('start datetime needs to be in isoformat, ex: 2019-01-21T12:25:05.955199')
-    end_date_err_msg = _('end datetime needs to be "*" or in isoformat, ex: 2019-01-21T12:25:05.955199')
+        self.format_err_msg = _('date range does not have the format: [start_datetime TO end_datetime]')
+        self.empty_err_msg = _('date range does not have the format: [start_datetime TO end_datetime]')
+        self.start_date_err_msg = _('start datetime needs to be in isoformat, ex: 2019-01-21T12:25:05.955199')
+        self.end_date_err_msg = _('end datetime needs to be "*" or in isoformat, ex: 2019-01-21T12:25:05.955199')
+        self.end_date_before_start_date_err_msg = _('end datetime needs to be after start datetime')
 
-    if not daterange_string:
-        raise Invalid(empty_err_msg)
-    if not daterange_string.startswith('[') or not daterange_string.endswith(']'):
-        raise Invalid(format_err_msg)
+        super(DaterangeParser, self).__init__()
 
-    stripped_value = daterange_string[1:-1]
-    parts = stripped_value.split()
-    if len(parts) != 3:
-        raise Invalid(format_err_msg)
+        if not daterange_string:
+            raise Invalid(self.empty_err_msg)
+        if not daterange_string.startswith('[') or not daterange_string.endswith(']'):
+            raise Invalid(self.format_err_msg)
 
-    try:
-        start_date = dateutil_parser.parse(parts[0])
-        start_date_str = start_date.isoformat()[:23] + ('Z' if for_solr else '')
-    except Exception as e:
-        raise Invalid(start_date_err_msg)
+        stripped_value = daterange_string[1:-1]
+        parts = stripped_value.split()
+        if len(parts) != 3:
+            raise Invalid(self.format_err_msg)
 
-    if parts[1] != 'TO':
-        raise Invalid(format_err_msg)
-
-    if parts[2] != '*':
         try:
-            end_date = dateutil_parser.parse(parts[2])
-            end_date_str = end_date.isoformat()[:23] + ('Z' if for_solr else '')
+            self.start_date = dateutil_parser.parse(parts[0])
         except Exception as e:
-            raise Invalid(end_date_err_msg)
-    else:
-        end_date_str = '*'
+            raise Invalid(self.start_date_err_msg)
 
-    return '[{} TO {}]'.format(start_date_str, end_date_str)
+        if parts[1] != 'TO':
+            raise Invalid(self.format_err_msg)
+
+        if parts[2] != '*':
+            self.end_date_is_infinity = False
+            try:
+                self.end_date = dateutil_parser.parse(parts[2])
+            except Exception as e:
+                raise Invalid(self.end_date_err_msg)
+            if self.end_date < self.start_date:
+                raise Invalid(self.end_date_before_start_date_err_msg)
+        else:
+            self.end_date_is_infinity = True
+
+    # def is_valid(self):
+    #     if self.start_date and (self.end_date_is_infinity or self.end_date):
+    #         return True
+    #     return False
+
+    def compute_daterange_string(self, for_solr):
+        start_date_str = self.start_date.isoformat()[:23] + ('Z' if for_solr else '')
+        if self.end_date_is_infinity:
+            end_date_str = '*'
+        else:
+            end_date_str = self.end_date.isoformat()[:23] + ('Z' if for_solr else '')
+        return '[{} TO {}]'.format(start_date_str, end_date_str)
+
+    # def human_readable(self):
+    #     start_date_str = h.render_datetime(self.start_date)
+    #     if self.end_date_is_infinity:
+    #         end_date_str = _('OPEN ENDED')
+    #     else:
+    #         end_date_str = h.render_datetime(self.end_date)
+    #
+    #     return '{} - {}'.format(start_date_str, end_date_str)
+
--- ckanext-hdx_search/ckanext/hdx_search/plugin.py
+++ ckanext-hdx_search/ckanext/hdx_search/plugin.py
@@ -1,4 +1,4 @@
-import re
+import logging, re
 import datetime
 import unicodedata
 import ckan.logic
@@ -11,7 +11,7 @@ import ckanext.hdx_search.actions.actions as actions
 import ckanext.hdx_search.model as search_model
 import ckanext.hdx_search.helpers.search_history as search_history
 import ckanext.hdx_search.helpers.solr_query_helper as solr_query_helper
-from ckanext.hdx_package.helpers.date_helper import daterange_parser
+from ckanext.hdx_package.helpers.date_helper import DaterangeParser
 
 from ckanext.hdx_package.helpers.freshness_calculator import get_calculator_instance,\
     UPDATE_STATUS_URL_FILTER, UPDATE_STATUS_UNKNOWN, UPDATE_STATUS_FRESH, UPDATE_STATUS_NEEDS_UPDATE
@@ -205,6 +205,7 @@ class HDXSearchPlugin(plugins.SingletonPlugin):
         before_indexing_clean_resource_formats(pkg_dict)
 
         pkg_dict['title_string'] = unicodedata.normalize("NFKD", pkg_dict['title']).replace(r'\xc3', 'I')
+        pkg_dict.pop('resource_grouping', None)
 
         self.__process_dates_in_resource_extra(pkg_dict)
         return pkg_dict
@@ -223,7 +224,7 @@ class HDXSearchPlugin(plugins.SingletonPlugin):
                 new_list = []
                 for value in values:
                     try:
-                        new_value = daterange_parser(value, True)
+                        new_value = DaterangeParser(value ).compute_daterange_string(True)
                         new_list.append(new_value)
                     except:
                         continue
