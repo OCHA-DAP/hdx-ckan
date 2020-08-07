7bfe72f42ab465f35d399124fb27a65e6740fd85
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2020-08-08 01:24:55 +0300

    HDX-7049 expire reset key based on configuration
    - different configuration for normal reset vs inviting user to org
    - changing email templates to mention how long the link/key is valid

--- ckanext-hdx_users/ckanext/hdx_users/actions/update.py
+++ ckanext-hdx_users/ckanext/hdx_users/actions/update.py
@@ -1,7 +1,11 @@
 import ckan.model as model
-import ckanext.hdx_users.model as umodel
+import pylons.config as config
+
 import ckan.logic as logic
-import ckanext.hdx_users.model as user_model
+import ckanext.hdx_users.model as umodel
+
+import ckanext.hdx_users.helpers.reset_password as reset_password
+
 
 NotFound = logic.NotFound
 _check_access = logic.check_access
@@ -32,8 +36,59 @@ def user_fullname_update(context, data_dict):
     if not user_obj:
         raise NotFound('User id not found')
     ue_data_dict = {'user_id': user_obj.id, 'extras': [
-        {'key': user_model.HDX_FIRST_NAME, 'new_value': first_name},
-        {'key': user_model.HDX_LAST_NAME, 'new_value': last_name},
+        {'key': umodel.HDX_FIRST_NAME, 'new_value': first_name},
+        {'key': umodel.HDX_LAST_NAME, 'new_value': last_name},
     ]}
     ue_result = get_action('user_extra_update')(context, ue_data_dict)
     return ue_result
+
+
+def hdx_send_reset_link(context, data_dict):
+    from urlparse import urljoin
+    import ckan.lib.mailer as mailer
+    import ckan.lib.helpers as h
+    import ckanext.hdx_users.controllers.mailer as hdx_mailer
+
+    model = context['model']
+
+    user = None
+    reset_link = None
+    user_fullname = None
+    recipient_mail = None
+
+    id = data_dict.get('id', None)
+    if id:
+        user = model.User.get(id)
+        context['user_obj'] = user
+        if user is None:
+            raise NotFound
+
+    expiration_in_hours = int(config.get('hdx.password.reset_key.expiration_in_hours'))
+    if user:
+        reset_password.create_reset_key(user, expiration_in_hours)
+
+        recipient_mail = user.email if user.email else None
+        user_fullname = user.fullname or ''
+        reset_link = urljoin(config.get('ckan.site_url'),
+                             h.url_for(controller='user', action='perform_reset', id=user.id, key=user.reset_key))
+
+    # body = u"""\
+    #             <p>Dear {fullname}, </p>
+    #             <p>You have requested your password on {site_title} to be reset.</p>
+    #             <p>Please click on the following link to confirm this request:</p>
+    #             <p> <a href=\"{reset_link}\">{reset_link}</a></p>
+    #         """.format(fullname=user_fullname, site_title=config.get('ckan.site_title'),
+    #                    reset_link=reset_link)
+
+    email_data = {
+        'user_fullname': user_fullname,
+        'user_reset_link': reset_link,
+        'expiration_in_hours': expiration_in_hours,
+    }
+    if recipient_mail:
+        subject = u'HDX password reset'
+        hdx_mailer.mail_recipient([{'display_name': user_fullname, 'email': recipient_mail}], subject,
+                                  email_data, footer=recipient_mail,
+                                  snippet='email/content/password_reset.html')
+        # hdx_mailer.mail_recipient([{'display_name': user_fullname, 'email': recipient_mail}], subject, body)
+
