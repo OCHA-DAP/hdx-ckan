0347bc8ce61f387667c0d1d55ff7a716d981fce6
Dan Mihaila <danmihaila@gmail.com>
2020-08-17 17:51:01 +0300

    HDX-7063 resp group page - initial commit

--- ckanext-hdx_org_group/ckanext/hdx_org_group/helpers/country_helper.py
+++ ckanext-hdx_org_group/ckanext/hdx_org_group/helpers/country_helper.py
@@ -1,11 +1,22 @@
 import ckan.model as model
 import ckan.common as common
-
+import ckan.lib.base as base
+import ckan.logic as logic
+import ckan.controllers.group as group
 import ckanext.hdx_search.controllers.search_controller as search_controller
 
 from ckanext.hdx_search.helpers.constants import DEFAULT_SORTING
 
 c = common.c
+render = base.render
+abort = base.abort
+NotFound = logic.NotFound
+NotAuthorized = logic.NotAuthorized
+get_action = logic.get_action
+c = common.c
+request = common.request
+_ = common._
+response = common.response
 
 
 def get_latest_cod_datatset(country_name):
@@ -20,3 +31,4 @@ def get_latest_cod_datatset(country_name):
                                              None, context)
 
     return next(iter(query_result.get('results', [])), None)
+
