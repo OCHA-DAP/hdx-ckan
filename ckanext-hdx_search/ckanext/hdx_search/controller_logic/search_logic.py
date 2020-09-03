3316ca604c8c1f734282b5608af9b96cd036a2df
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2020-09-03 14:25:29 +0300

    HDX-7081 OPS-6569 forcing OR operator on collapse 'fq' (for mobile)

--- ckanext-hdx_search/ckanext/hdx_search/controller_logic/search_logic.py
+++ ckanext-hdx_search/ckanext/hdx_search/controller_logic/search_logic.py
@@ -166,7 +166,8 @@ class SearchLogic(object):
             solr_expand = 'false'
             if use_solr_collapse and not fq_list and not q and not featured_filters_set:
                 fq_list = [
-                    '{{!tag=batch}}{{!collapse field=batch nullPolicy=expand sort="{sort}"}} '.format(sort=sort_by)
+                    '{{!tag=batch q.op=OR}}{{!collapse field=batch nullPolicy=expand sort="{sort}"}} '
+                        .format(sort=sort_by)
                 ]
                 solr_expand = 'true'
 
