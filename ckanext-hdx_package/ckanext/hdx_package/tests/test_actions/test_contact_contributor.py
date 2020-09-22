63e020701885e008b7afa90cfb5d9ada01d1c419
Dan Mihaila <danmihaila@gmail.com>
2020-09-22 11:57:39 +0300

    HDX-7110 fix test_contact_contributor.py

--- ckanext-hdx_package/ckanext/hdx_package/tests/test_actions/test_contact_contributor.py
+++ ckanext-hdx_package/ckanext/hdx_package/tests/test_actions/test_contact_contributor.py
@@ -55,25 +55,26 @@ class TestContactContributor(hdx_test_with_inds_and_orgs.HDXWithIndsAndOrgsTest)
         assert len(mocked_mail_recipient.call_args_list) == 2, '2 separate emails should be sent'
 
         args0, kw_args0 = mocked_mail_recipient.call_args_list[0]
-        first_email_addresses = [recipient.get('email') for recipient in kw_args0.get('recipients_list', [])]
-        first_body = kw_args0.get('body')
+        first_email_addresses = [args0[0][0].get('email')]
+        first_body = kw_args0.get('snippet')
 
         assert len(first_email_addresses) == 1, 'Just 1 org admin should be receiving the 1st email email'
         assert 'test@test.com' in first_email_addresses, 'Org admin email needs to be in recipient email list'
-        assert 'You sent the following message' not in first_body, 'Fullname not "You" should appear in the 1st email'
-        assert 'REPLY ALL function' in first_body, 'Additional notes should be included in the 1st email'
+        assert 'email/content/contact_contributor_request.html' in first_body
+        # assert 'You sent the following message' not in first_body, 'Fullname not "You" should appear in the 1st email'
+        # assert 'REPLY ALL function' in first_body, 'Additional notes should be included in the 1st email'
 
         args1, kw_args1 = mocked_mail_recipient.call_args_list[1]
-        second_email_addresses = [recipient.get('email') for recipient in kw_args1.get('recipients_list', [])]
-        second_body = kw_args1.get('body')
+        second_email_addresses = [args1[0][0].get('email')]
+        second_body = kw_args1.get('snippet')
 
         assert len(second_email_addresses) == 1, 'Just the requester should be receiving the 2nd email'
         assert data_dict.get('email') in second_email_addresses, 'Requester email needs to be in recipient email list'
-        assert 'You sent the following message' in second_body, '"You" not fullname should appear in the 2nd email'
+        assert 'email/content/contact_contributor_request_confirmation_to_user.html' in second_body
 
         pkg_dict = self._get_action('package_show')({}, {"id": "test_dataset_1"})
         maintainer = pkg_dict.get("maintainer")
         assert maintainer
         m_user = model.User.get(maintainer)
-        recipients_list = [recipient for recipient in kw_args0.get('recipients_list', [])]
+        recipients_list = [args0[0][0].get('email')]
         assert {'display_name': m_user.display_name, 'email': m_user.email} in recipients_list
