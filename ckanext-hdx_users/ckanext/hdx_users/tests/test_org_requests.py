4c3ba0d0e3e05c781163f9ff27dc790b7eda5d1c
Dan Mihaila <danmihaila@gmail.com>
2020-09-22 15:39:27 +0300

    HDX-7110 fix test_org_requests.py

--- ckanext-hdx_users/ckanext/hdx_users/tests/test_org_requests.py
+++ ckanext-hdx_users/ckanext/hdx_users/tests/test_org_requests.py
@@ -136,14 +136,17 @@ class TestHDXReqsOrgController(org_group_base.OrgGroupBaseTest):
         args1, kw_args1 = mocked_mail_recipient.call_args_list[1]
 
         req_dict = args0[2]
-        assert postparams.get('name') == str(req_dict.get('org_name'))
-        assert postparams.get('acronym') == str(req_dict.get('org_acronym'))
-        assert postparams.get('org_type') == str(req_dict.get('org_type'))
-        assert postparams.get('url') == str(req_dict.get('org_website'))
-        assert postparams.get('description') == str(req_dict.get('org_description'))
-        assert postparams.get('description_data') == str(req_dict.get('data_description'))
-        assert postparams.get('work_email') == str(req_dict.get('requestor_work_email'))
-        assert postparams.get('your_name') == str(req_dict.get('user_fullname'))
+        assert unicode(postparams.get('name'), "utf-8") == req_dict.get('org_name')
+        assert unicode(postparams.get('acronym'), "utf-8") == req_dict.get('org_acronym')
+        assert unicode(postparams.get('org_type'), "utf-8") == req_dict.get('org_type')
+        assert unicode(postparams.get('url'), "utf-8") == req_dict.get('org_website')
+        assert unicode(postparams.get('description'), "utf-8") == req_dict.get('org_description')
+        assert unicode(postparams.get('description_data'), "utf-8") == req_dict.get('data_description')
+        assert unicode(postparams.get('work_email'), "utf-8") == req_dict.get('requestor_work_email')
+        assert unicode(postparams.get('your_name'), "utf-8") == req_dict.get('user_fullname')
+
+        assert 'email/content/new_org_request_hdx_team_notification.html' in kw_args0.get('snippet')
+        assert 'email/content/new_org_request_confirmation_to_user.html' in kw_args1.get('snippet')
 
         # assert mail_info, 'This needs to contain the email that will be sent'
         # assert 'tester' in mail_info, 'Ckan username needs to be in the email'
