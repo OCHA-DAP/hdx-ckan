dffd0fae260a175c62d3b52f18809810aabad4c1
Dan Mihaila <danmihaila@gmail.com>
2020-11-10 15:02:02 +0200

    HDX-6340 new dataset date range

--- ckanext-hdx_theme/ckanext/hdx_theme/tests/hdx_test_base.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/tests/hdx_test_base.py
@@ -117,7 +117,7 @@ class HdxBaseTest(object):
                     if not data_dict.get('data_update_frequency'):
                         data_dict['data_update_frequency'] = '0'
                     if not data_dict.get('dataset_date'):
-                        data_dict['dataset_date'] = '11/11/2011'
+                        data_dict['dataset_date'] = '[2011-11-11 TO 2011-11-11]'
                     if not data_dict.get('methodology'):
                         data_dict['methodology'] = 'Automatically inserted test methodology'
             return original_package_create(context, data_dict)
