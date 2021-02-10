49eb73bd3f44c43b5559f95513cdbe752601f9f5
Dan Mihaila <danmihaila@gmail.com>
2021-02-10 09:36:16 +0200

    HDX-7234 restore the flag of the resource if PII is failing

--- ckanext-hdx_search/ckanext/hdx_search/actions/actions.py
+++ ckanext-hdx_search/ckanext/hdx_search/actions/actions.py
@@ -129,9 +129,12 @@ def hdx_qa_pii_run(context, data_dict):
     resource_id = data_dict.get("resourceId")
     if resource_id:
         try:
-            resource_dict = get_action("hdx_qa_resource_patch")(context, {"id": resource_id, "pii_report_flag": "QUEUED"})
+            resource_dict = get_action("resource_show")(context, {"id": resource_id})
+            old_pii_report_flag = resource_dict.get('pii_report_flag',"")
+            get_action("hdx_qa_resource_patch")(context, {"id": resource_id, "pii_report_flag": "QUEUED"})
             _run_pii_check(resource_dict, context)
         except Exception, e:
+            get_action("hdx_qa_resource_patch")(context, {"id": resource_id, "pii_report_flag": old_pii_report_flag})
             ex_msg = e.message if hasattr(e, 'message') and e.message else str(e)
             message = e.error_summary if hasattr(e, 'error_summary') and e.error_summary else 'Something went wrong while processing the request:' + str(ex_msg)
             raise logic.ValidationError({'message': message}, error_summary=message)
