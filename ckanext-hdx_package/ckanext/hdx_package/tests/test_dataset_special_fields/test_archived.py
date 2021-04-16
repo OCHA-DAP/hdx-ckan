7022a0f73fe66a8cc6fa02da9b6856d55c6a9d5d
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2021-04-16 13:33:21 +0300

    Keep old values for archived if none is provided

---
+++ ckanext-hdx_package/ckanext/hdx_package/tests/test_dataset_special_fields/test_archived.py
@@ -0,0 +1,62 @@
+import ckan.model as model
+
+from ckan.tests import factories
+
+import ckanext.hdx_theme.tests.hdx_test_with_inds_and_orgs as hdx_test_with_inds_and_orgs
+
+
+class TestArchived(hdx_test_with_inds_and_orgs.HDXWithIndsAndOrgsTest):
+
+    context = {'model': model, 'session': model.Session, 'user': 'editor_user'}
+    context_sysadmin = {'model': model, 'session': model.Session, 'user': 'testsysadmin'}
+
+    @classmethod
+    def setup_class(cls):
+        super(TestArchived, cls).setup_class()
+        factories.User(name='editor_user', email='editor_user@example.com')
+
+        cls._get_action('organization_member_create')(cls.context_sysadmin,
+                                                       {'id': 'hdx-test-org', 'username': 'editor_user',
+                                                        'role': 'editor'})
+
+    def test_archived_after_package_update(self):
+
+        # package_dict = self._get_action('package_show')(self.context_sysadmin, {'id': 'test_private_dataset_1'})
+        # resource_id = package_dict['resources'][0]['id']
+
+        dataset_dict = self._get_action('package_patch')(self.context, {
+            'id': 'test_private_dataset_1',
+            'archived': True,
+            'private': True
+        })
+        assert dataset_dict['archived'] is True
+        assert dataset_dict['private'] is True
+
+        del dataset_dict['archived']
+        dataset_dict['private'] = False
+        dataset_dict['data_update_frequency'] = '7'
+
+        dataset_dict = self._get_action('package_update')(self.context, dataset_dict)
+        assert dataset_dict['private'] is False
+        assert dataset_dict['archived'] is True
+
+        dataset_dict = self._get_action('package_patch')(self.context, {
+            'id': 'test_private_dataset_1',
+            'archived': False,
+        })
+        assert dataset_dict['archived'] is False
+
+    def test_archived_after_resource_update(self):
+
+        dataset_dict = self._get_action('package_patch')(self.context, {
+            'id': 'test_private_dataset_1',
+            'archived': True,
+            'data_update_frequency': '7'
+        })
+        assert dataset_dict['archived'] is True
+
+        resource_dict = dataset_dict['resources'][0]
+
+        resource_dict = self._get_action('resource_update')(self.context, resource_dict)
+        dataset_dict = self._get_action('package_show')(self.context_sysadmin, {'id': 'test_private_dataset_1'})
+        assert dataset_dict['archived'] is True
