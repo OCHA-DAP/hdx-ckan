8b6e499e11a17373aa622e78ea89090b26858300
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2021-04-17 03:08:37 +0300

    HDX-7414 sitemap migration + tests rewrite

--- ckanext-sitemap/ckanext/sitemap/plugin.py
+++ ckanext-sitemap/ckanext/sitemap/plugin.py
@@ -3,14 +3,21 @@ Sitemap plugin for CKAN
 '''
 
 from ckan.plugins import implements, SingletonPlugin
-from ckan.plugins import IRoutes
+from ckan.plugins import IBlueprint
+
+from ckanext.sitemap.view import sitemap
+
 
 class SitemapPlugin(SingletonPlugin):
-    implements(IRoutes, inherit=True)
+    # implements(IRoutes, inherit=True)
+    implements(IBlueprint)
+
+    def get_blueprint(self):
+        return sitemap
 
     def before_map(self, map):
         controller='ckanext.sitemap.controller:SitemapController'
         map.connect('sitemap', '/sitemap.xml', controller=controller, action='view')
         map.connect('sitemap_page', '/sitemap{page}.xml', controller=controller, action='index')
         return map
-        
+
--- ckanext-sitemap/ckanext/sitemap/tests/test_sitemap.py
+++ ckanext-sitemap/ckanext/sitemap/tests/test_sitemap.py
@@ -1,136 +1,126 @@
-'''
-Tests for sitemap.
-'''
-
-import logging
-import unittest
-from StringIO import StringIO
-import json
+import pytest
 from lxml import etree
-import datetime
 
-from pylons import config
-import ckan.config as ckanconfig
-import webtest
-
-from ckan.model import Session, Package, Resource
+import ckan.lib.search as search
 import ckan.model as model
-from ckan.tests import CreateTestData
-import ckan.lib.create_test_data as ctd
-from ckan.lib.helpers import url_for
-from ckan.logic.auth.get import package_show
-from ckan.tests.functional.base import FunctionalTestCase
+import ckan.plugins.toolkit as tk
+import ckan.tests.factories as factories
+import ckan.tests.helpers as test_helpers
+import ckanext.sitemap.tests.testdata as testdata
+
+_get_action = tk.get_action
+config = tk.config
+url_for = tk.url_for
+utf8_parser = etree.XMLParser(encoding='utf-8')
+siteschema = etree.XMLSchema(etree.fromstring(testdata.sitemap.encode('utf-8'), parser=utf8_parser))
+indexschema = etree.XMLSchema(etree.fromstring(testdata.sitemapindex.encode('utf-8'), parser=utf8_parser))
 
 
-import testdata
+@pytest.mark.usefixtures("with_request_context")
+class TestSitemap(object):
 
-log = logging.getLogger(__file__)
+    dataset1_name = 'sitemap_test_dataset_1'
+    dataset2_name = 'sitemap_test_dataset_2'
 
-siteschema = etree.XMLSchema(etree.parse(StringIO(testdata.sitemap)))
-indexschema = etree.XMLSchema(etree.parse(StringIO(testdata.sitemapindex)))
-
-def _get_test_app():
-    config['ckan.legacy_templates'] = False
-    app = ckanconfig.middleware.make_app(config['global_conf'], **config)
-    app = webtest.TestApp(app)
-    return app
-
-class TestSitemap(FunctionalTestCase, unittest.TestCase):
+    dataset1_dict = None  # type: dict
+    dataset2_dict = None  # type: dict
 
     @classmethod
     def setup_class(cls):
-        """
-        Remove any initial sessions.
-        """
-        Session.remove()
-        cls.original_config = config.copy()
-        plugins = set(config['ckan.plugins'].strip().split())
-        plugins.add('sitemap')
-        config['ckan.plugins'] = ' '.join(plugins)
-        cls.app = _get_test_app()
-        CreateTestData.create()
-        url = url_for(controller="ckanext.sitemap.controller:SitemapController",
-                      action='view')
-        cls.cont = cls.app.get(url)
-        cls.content_file = StringIO(cls.cont.body)
+        search.clear_all()
+        test_helpers.reset_db()
+        cls.app = test_helpers._get_test_app()
+
+        sysadmin = 'testsysadmin_sitemap'
+        factories.Sysadmin(name=sysadmin)
+        model.Session.commit()
+        sysadmin_user = model.User.by_name(sysadmin)
+        if sysadmin_user:
+            sysadmin_user.email = sysadmin + '@test-domain.com'
+            sysadmin_user.apikey = sysadmin + '_apikey'
+
+        model.Session.commit()
+        cls.sysadmin_user = {
+            'name': sysadmin,
+            'apikey': sysadmin_user.apikey
+        }
+
+        cls.dataset1_dict = cls._create_package(cls.dataset1_name, sysadmin)
+        cls.dataset2_dict = cls._create_package(cls.dataset2_name, sysadmin, create_org_and_group=False)
 
     @classmethod
-    def teardown_class(cls):
-        """
-        Tear down, remove the session.
-        """
-        CreateTestData.delete()
-        Session.remove()
+    def _create_package(cls, pkg_name, user, create_org_and_group=True):
+        '''
+        :param pkg_name: the name of the new package
+        :type pkg_name: str
+        :param create_org_and_group: whether to create the org and group for the package. Might've been created already.
+        :type create_org_and_group: bool
+        :return: id of newly created package
+        :rtype: str
+        '''
+        if create_org_and_group:
+            factories.Organization(name='test_owner_org',
+                                   org_url='http://example.org/',
+                                   hdx_org_type='academic_research',
+                                   users=[{'name': 'testsysadmin', 'capacity': 'admin'}]
+                                   )
+            factories.Group(name='test_group1')
+        package = {
+            "package_creator": "test function",
+            "private": False,
+            "dataset_date": "[1960-01-01 TO 2012-12-31]",
+            "caveats": "These are the caveats",
+            "license_other": "TEST OTHER LICENSE",
+            "methodology": "This is a test methodology",
+            "dataset_source": "Test data",
+            "license_id": "hdx-other",
+            "name": pkg_name,
+            "notes": "This is a test dataset",
+            "title": "Test Dataset for QA Completed " + pkg_name,
+            "owner_org": "test_owner_org",
+            "groups": [{"name": "test_group1"}],
+            "maintainer": user,
+            "data_update_frequency": "0",
+            "resources": [
+                {
+                    'package_id': pkg_name,
+                    'url': config.get('ckan.site_url', '') + '/storage/f/test_folder/hdx_test.csv',
+                    'resource_type': 'file.upload',
+                    'format': 'CSV',
+                    'name': 'hdx_test.csv',
 
-    @classmethod
-    def _get_action(cls, action_name):
-        return tk.get_action(action_name)
+                }
+            ]
+        }
 
-    def test_controller(self):
-        url = url_for(controller="ckanext.sitemap.controller:SitemapController",
-                      action='view')
-        self.cont = self.app.get(url)
-        self.content_file = StringIO(self.cont.body)
-        assert self.cont.response.status == "200 OK"
+        context = {'model': model, 'session': model.Session, 'user': user}
+        package_dict = _get_action('package_create')(context, package)
+        return package_dict
 
     def test_validity_index(self):
-        url = url_for(controller="ckanext.sitemap.controller:SitemapController",
-                      action='view')
-        self.cont = self.app.get(url)
-        print self.cont.body
-        self.content_file = StringIO(self.cont.body)
-        indexschema.assertValid(etree.parse(self.content_file))
-        assert indexschema.validate(etree.parse(self.content_file))
+        url = url_for('sitemap.view')
+        response = self.app.get(url)
+        content_file = response.body
+        assert indexschema.validate(etree.fromstring(content_file.encode('utf-8'), parser=utf8_parser))
 
     def test_validity_map(self):
-        url = url_for(controller="ckanext.sitemap.controller:SitemapController",
-                      action='index', page=1)
-        self.cont = self.app.get(url)
-        print self.cont.body
-        self.content_file = StringIO(self.cont.body)
-        siteschema.assertValid(etree.parse(self.content_file))
-        assert siteschema.validate(etree.parse(self.content_file))
+        url = url_for('sitemap.index', page=1)
+        response = self.app.get(url)
+        content_file = response.body
+        tree = etree.fromstring(content_file.encode('utf-8'), parser=utf8_parser)
+        assert siteschema.validate(tree)
 
-    def test_packages(self):
-        url = url_for(controller="ckanext.sitemap.controller:SitemapController",
-                      action='index', page=1)
-        self.cont = self.app.get(url)
-        self.content_file = StringIO(self.cont.body)
-        tree = etree.parse(self.content_file)
-        self.assert_("annakarenina" in self.cont.body)
-        log.debug(self.cont.body)
-        pkg = Session.query(Package).all()[0]
-        urli = config.get('ckan.site_url') + url_for(controller='package',
-                                                     action='read',
-                                                     id = pkg.name)
-        self.assert_(tree.getroot()[0][0].text == urli)
-        # Needs to be created today as test data is too
-        pkgdate = pkg.latest_related_revision.timestamp.strftime('%Y-%m-%d')
-        self.assert_(tree.getroot()[0][1].text == pkgdate)
-        resurl = config.get('ckan.site_url') + url_for('resource_read',
-                                                       id=pkg.name,
-                                                       resource_id=pkg.resources[0].id)
-        self.assert_(tree.getroot()[1][0].text == resurl)
+        assert self.dataset1_name in content_file
+        assert self.dataset2_name in content_file
 
-    def _create_pkg(self):
-        model.repo.new_revision()
-        pkg = Package.get('annakarenina')
-        pkg.name = "fookarenina"
-        pkg.add_resource('www.google.com',description='foo', name="foo")
-        Session.add(pkg)
-        Session.commit()
-        return pkg
+        # the datasets are fetched by default in desc order of the update time
+        dataset2_url = config.get('ckan.site_url') + url_for('dataset.read', id=self.dataset2_name)
+        assert tree[0][0].text == dataset2_url
 
+        res_url = config.get('ckan.site_url') + url_for('resource.read',
+                                                        id=self.dataset2_name,
+                                                        resource_id=self.dataset2_dict['resources'][0]['id'])
+        assert tree[1][0].text == res_url
 
-    def test_zcache(self):
-        url = url_for(controller="ckanext.sitemap.controller:SitemapController",
-                      action='view')
-        cont1 = self.app.get(url)
-        cont2 = self.app.get(url)
-        self.assert_(cont1.body == cont2.body)
-        log.debug(cont1.body)
-        pkg = self._create_pkg()
-        log.debug(len(pkg.resources))
-        cont2 = self.app.get(url)
-        log.debug(cont2.body)
-        self.assert_(cont1.body == cont2.body)
+        dataset2_date = self.dataset2_dict['metadata_modified'][:10]
+        assert  tree[0][1].text == dataset2_date
