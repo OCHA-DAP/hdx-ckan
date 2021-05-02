b4c429e451744a4c5a86ffb006b395f4e062450a
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2021-05-03 01:26:19 +0300

    colored page feature

---
+++ ckanext-hdx_theme/ckanext/hdx_theme/views/colored_page.py
@@ -0,0 +1,29 @@
+from flask import Blueprint
+import ckan.plugins.toolkit as tk
+
+render = tk.render
+ValidationError = tk.ValidationError
+
+hdx_colored_page = Blueprint(u'hdx_colored_page', __name__, url_prefix=u'/colored_page')
+
+
+def read(color):
+    color = color if color else 'FFFFFF'
+    _validate(color)
+    template_data = {
+        'color': '#' + color,
+    }
+    return render(u'colored_page/index.html', template_data)
+
+
+def _validate(color):
+    lower_color = color.lower()
+    if len(lower_color) == 6 and '000000' <= lower_color <= 'ffffff':
+        return True
+    if len(lower_color) == 3 and '000' <= lower_color <= 'fff':
+        return True
+
+    raise ValidationError('Color format is incorrect')
+
+
+hdx_colored_page.add_url_rule(u'/<color>', view_func=read)
