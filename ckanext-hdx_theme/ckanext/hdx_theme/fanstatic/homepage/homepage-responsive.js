376466b9621f27692aecfbc0a57a26b61df064fd
Dan Mihaila <danmihaila@gmail.com>
2021-06-28 17:40:31 +0300

    HDX-7638 fix country topline

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/homepage/homepage-responsive.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/homepage/homepage-responsive.js
@@ -14,28 +14,30 @@ function mobileToolTabChange(){
 
 function initMobileCarousel(){
     var carousel = $(".mobile-carousel")[0];
-    var hammer = new Hammer(carousel);
-    var itemW = $(carousel).find('.item').outerWidth(true);
-    var numItems = $(carousel).find('.item').length;
-    var currItem = 1;
-    hammer.on('swipeleft swiperight', function (ev) {
-        if (ev.type=="swipeleft" && currItem < numItems){
-            $(carousel).find('.mobile-carousel-inner').animate({
-                left: "-=" + itemW
-            }, 400);
-            currItem++;
-        }
-        else{
-            if (ev.type=="swiperight" && currItem > 1){
-                $(carousel).find('.mobile-carousel-inner').animate({
-                    left: "+=" + itemW
-                }, 400);
-                currItem--;
-            }
-        }
-        setMobileCarouselPagination(currItem);
-    });
-    initMobileCarouselPagination();
+    if(carousel) {
+      var hammer = new Hammer(carousel);
+      var itemW = $(carousel).find('.item').outerWidth(true);
+      var numItems = $(carousel).find('.item').length;
+      var currItem = 1;
+      hammer.on('swipeleft swiperight', function (ev) {
+          if (ev.type=="swipeleft" && currItem < numItems){
+              $(carousel).find('.mobile-carousel-inner').animate({
+                  left: "-=" + itemW
+              }, 400);
+              currItem++;
+          }
+          else{
+              if (ev.type=="swiperight" && currItem > 1){
+                  $(carousel).find('.mobile-carousel-inner').animate({
+                      left: "+=" + itemW
+                  }, 400);
+                  currItem--;
+              }
+          }
+          setMobileCarouselPagination(currItem);
+      });
+      initMobileCarouselPagination();
+    }
 }
 
 function initMobileCarouselPagination(){
