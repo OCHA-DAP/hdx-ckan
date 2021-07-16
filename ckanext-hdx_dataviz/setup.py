8bf2e0490e7e4fd224ab7e3e8d37c64a7a7ed577
Dan Mihaila <danmihaila@gmail.com>
2021-07-16 11:41:09 +0300

    HDX-7588 hdx_dataviz extension fix setup.py

--- ckanext-hdx_dataviz/setup.py
+++ ckanext-hdx_dataviz/setup.py
@@ -55,7 +55,7 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-        namespace_packages=['ckanext'],
+        # namespace_packages=['ckanext'],
 
     install_requires=[
       # CKAN extensions should not list dependencies here, but in a separate
