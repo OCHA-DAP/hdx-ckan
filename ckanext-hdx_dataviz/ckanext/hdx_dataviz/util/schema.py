2c13f9f3ff5b8904c5eeaab6196722bddb828e45
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2021-08-11 16:39:21 +0300

    HDX-7588 adding 'image_frame_mode' flag

--- ckanext-hdx_dataviz/ckanext/hdx_dataviz/util/schema.py
+++ ckanext-hdx_dataviz/ckanext/hdx_dataviz/util/schema.py
@@ -17,11 +17,18 @@ def showcase_update_schema_wrapper(original_schema):
     def schema_function():
         schema = original_schema()
         schema.update({
-            'in_dataviz_gallery': [_has_dataviz_gallery_permission, default('false'), boolean_validator, get_converter('convert_to_extras')],
-            'in_carousel_section': [_has_dataviz_gallery_permission, default('false'), boolean_validator, get_converter('convert_to_extras')],
-            'dataviz_label': [_has_dataviz_gallery_permission, not_empty_if_in_dataviz_gallery, six.text_type, _check_string_length, get_converter('convert_to_extras')],
-            'data_url': [_has_dataviz_gallery_permission, ignore_empty, _is_https_url, url_validator, get_converter('convert_to_extras')],
-            'priority': [_has_dataviz_gallery_permission, ignore_empty, natural_number_validator, get_converter('convert_to_extras')],
+            'in_dataviz_gallery': [_has_dataviz_gallery_permission, default('false'), boolean_validator,
+                                   get_converter('convert_to_extras')],
+            'image_frame_mode': [_has_dataviz_gallery_permission, default('false'), boolean_validator,
+                                   get_converter('convert_to_extras')],
+            'in_carousel_section': [_has_dataviz_gallery_permission, default('false'), boolean_validator,
+                                    get_converter('convert_to_extras')],
+            'dataviz_label': [_has_dataviz_gallery_permission, not_empty_if_in_dataviz_gallery, six.text_type,
+                              _check_string_length, get_converter('convert_to_extras')],
+            'data_url': [_has_dataviz_gallery_permission, ignore_empty, _is_https_url, url_validator,
+                         get_converter('convert_to_extras')],
+            'priority': [_has_dataviz_gallery_permission, ignore_empty, natural_number_validator,
+                         get_converter('convert_to_extras')],
         })
         return schema
     return schema_function
@@ -32,6 +39,7 @@ def showcase_show_schema_wrapper(original_schema):
         schema = original_schema()
         schema.update({
             'in_dataviz_gallery': [get_converter('convert_from_extras'), default('false'), boolean_validator],
+            'image_frame_mode': [get_converter('convert_from_extras'), default('false'), boolean_validator],
             'in_carousel_section': [get_converter('convert_from_extras'), default('false'), boolean_validator],
             'dataviz_label': [get_converter('convert_from_extras'), ignore_missing],
             'data_url': [get_converter('convert_from_extras'), ignore_empty],
