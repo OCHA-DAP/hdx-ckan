dfb7b40413496cbe470c625a976183f2096e448b
Alexandru Artimon <aalecs@gmail.com>
2021-09-03 12:07:52 +0300

    Fix merge related to HDX-7623

--- ckanext-hdx_org_group/ckanext/hdx_org_group/views/light_group.py
+++ ckanext-hdx_org_group/ckanext/hdx_org_group/views/light_group.py
@@ -166,13 +166,12 @@ def _read(template_file, id, show_switch_to_desktop, show_switch_to_mobile):
     except NotAuthorized:
         abort(403, _('Not authorized to see this page'))
 
-    package_type = 'dataset'
     ignore_capacity_check = False
     country_dict = grp_h.get_country(id)
     country_code = country_dict.get('name', id)
     search_logic = GroupSearchLogic(id=country_code)
     fq = 'groups:"{}"'.format(country_code)
-    search_logic._search(package_type, additional_fq=fq, ignore_capacity_check=ignore_capacity_check)
+    search_logic._search(additional_fq=fq, ignore_capacity_check=ignore_capacity_check)
     archived_url_helper = search_logic.add_archived_url_helper()
     redirect_result = archived_url_helper.redirect_if_needed()
     if redirect_result:
