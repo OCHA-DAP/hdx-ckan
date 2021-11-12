b5e0cad54c1d432f46735fa06fd254292a82a572
Dan Mihaila <danmihaila@gmail.com>
2021-11-12 11:47:09 +0200

    HDX-7972 - package links controller migration to flask

--- ckanext-hdx_theme/ckanext/hdx_theme/plugin.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/plugin.py
@@ -19,6 +19,7 @@ from ckanext.hdx_theme.views.ebola import hdx_ebola
 from ckanext.hdx_theme.views.image_server import hdx_global_file_server, hdx_local_image_server
 from ckanext.hdx_theme.views.custom_settings import hdx_carousel
 from ckanext.hdx_theme.views.quick_links_custom_settings import hdx_quick_links
+from ckanext.hdx_theme.views.package_links_custom_settings import hdx_package_links
 
 # def run_on_startup():
 #     cache_on_startup = config.get('hdx.cache.onstartup', 'true')
@@ -190,10 +191,10 @@ class HDXThemePlugin(plugins.SingletonPlugin):
         #                        'DatavizCustomSettingsController',
         #             action='show')
 
-        map.connect('package_links_settings', '/ckan-admin/packagelinks/show',
-                    controller='ckanext.hdx_theme.controllers.package_links_custom_settings:'
-                               'PackageLinksCustomSettingsController',
-                    action='show')
+        # map.connect('package_links_settings', '/ckan-admin/packagelinks/show',
+        #             controller='ckanext.hdx_theme.controllers.package_links_custom_settings:'
+        #                        'PackageLinksCustomSettingsController',
+        #             action='show')
 
         map.connect('pages_show', '/ckan-admin/pages/show',
                     controller='ckanext.hdx_theme.controllers.custom_settings:CustomSettingsController', action='show_pages')
@@ -209,21 +210,21 @@ class HDXThemePlugin(plugins.SingletonPlugin):
         #             controller='ckanext.hdx_theme.controllers.custom_settings:CustomSettingsController',
         #             action='delete')
 
-        map.connect('update_quick_links_settings', '/ckan-admin/quick-links/update',
-                    controller='ckanext.hdx_theme.controllers.quick_links_custom_settings:DatavizCustomSettingsController',
-                    action='update')
-
-        map.connect('delete_quick_links_settings', '/ckan-admin/quick-links/delete/{id}',
-                    controller='ckanext.hdx_theme.controllers.quick_links_custom_settings:DatavizCustomSettingsController',
-                    action='delete')
-
-        map.connect('update_package_links_settings', '/ckan-admin/package-links/update',
-                    controller='ckanext.hdx_theme.controllers.package_links_custom_settings:PackageLinksCustomSettingsController',
-                    action='update')
-
-        map.connect('delete_package_links_settings', '/ckan-admin/package-links/delete/{id}',
-                    controller='ckanext.hdx_theme.controllers.package_links_custom_settings:PackageLinksCustomSettingsController',
-                    action='delete')
+        # map.connect('update_quick_links_settings', '/ckan-admin/quick-links/update',
+        #             controller='ckanext.hdx_theme.controllers.quick_links_custom_settings:DatavizCustomSettingsController',
+        #             action='update')
+        #
+        # map.connect('delete_quick_links_settings', '/ckan-admin/quick-links/delete/{id}',
+        #             controller='ckanext.hdx_theme.controllers.quick_links_custom_settings:DatavizCustomSettingsController',
+        #             action='delete')
+        #
+        # map.connect('update_package_links_settings', '/ckan-admin/package-links/update',
+        #             controller='ckanext.hdx_theme.controllers.package_links_custom_settings:PackageLinksCustomSettingsController',
+        #             action='update')
+        #
+        # map.connect('delete_package_links_settings', '/ckan-admin/package-links/delete/{id}',
+        #             controller='ckanext.hdx_theme.controllers.package_links_custom_settings:PackageLinksCustomSettingsController',
+        #             action='delete')
 
         # map.connect('image_serve', '/image/{label}',
         #             controller='ckanext.hdx_theme.controllers.image_controller:ImageController', action='org_file')
@@ -382,4 +383,4 @@ class HDXThemePlugin(plugins.SingletonPlugin):
     # IBlueprint
     def get_blueprint(self):
         return [hdx_colored_page, hdx_faqs, hdx_ebola, hdx_global_file_server,
-                hdx_local_image_server, hdx_carousel, hdx_quick_links]
+                hdx_local_image_server, hdx_carousel, hdx_quick_links, hdx_package_links]
