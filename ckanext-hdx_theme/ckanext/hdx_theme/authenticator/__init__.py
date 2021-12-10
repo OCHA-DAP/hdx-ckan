51375679765be22a4af62d01abc56154a40b72d4
Dan Mihaila <danmihaila@gmail.com>
2021-12-10 13:23:30 +0200

    Revert "Revert "HDX-7986 switching to repoze.who-use-beaker + redis sessions""
    This reverts commit 0a3eea00c6cfa6aaf5f718af3956ee66243c8cce.

---
+++ ckanext-hdx_theme/ckanext/hdx_theme/authenticator/__init__.py
@@ -0,0 +1,14 @@
+from zope.interface import implements
+from repoze.who.interfaces import IAuthenticator
+
+
+# Based on PR from ckanext-security to CKAN: https://github.com/ckan/ckan/pull/4656/files
+class BeakerRedisAuth(object):
+    implements(IAuthenticator)
+
+    def authenticate(self, environ, identity):
+        # At this stage, the identity has already been validated from the cookie
+        # and redis (use_beaker middleware). We simply return the user id
+        # from the identity object if it's there, or None if the user's
+        # identity is not verified.
+        return identity.get('repoze.who.userid', None)
