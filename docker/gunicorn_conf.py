7993655e06f25ec8e69ff07f837667609b132643
Serban Teodorescu <teodorescu.serban@gmail.com>
2022-02-21 17:47:24 +0200

    make unit listen on port 5000 and gunicorn on 5001

--- docker/gunicorn_conf.py
+++ docker/gunicorn_conf.py
@@ -1,6 +1,6 @@
 user = 'www-data'
 group = 'www-data'
-bind = '0.0.0.0:5000'
+bind = '0.0.0.0:5001'
 #workers = ${HDX_CKAN_WORKERS}
 #access_log_format = '"%({x-real-ip}i)s" %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 access_log_format = '%({x-real-ip}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(T)s %({host}i)s %({x-forwarded-proto}i)s'
