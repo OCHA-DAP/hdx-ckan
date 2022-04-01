a3d2918a6904d1edb78979b18ab1e9b720627e0e
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2022-04-01 18:11:31 +0300

    HDX-8125 rewriting member controller tests
    - small refactoring in ytp request view
    - small fixes to the member delete action

--- ckanext-hdx_org_group/ckanext/hdx_org_group/views/members.py
+++ ckanext-hdx_org_group/ckanext/hdx_org_group/views/members.py
@@ -155,7 +155,7 @@ def member_delete(id):
         abort(403, _('Unauthorized to delete group %s members') % '')
 
     try:
-        user_id = request.params.get('user')
+        user_id = request.form.get('user')
         if request.method == 'POST':
             get_action('group_member_delete')(
                 context, {'id': id, 'user_id': user_id})
@@ -418,6 +418,6 @@ def _send_membership_confirmation(org_display_name, org_id, role, user_info):
 
 
 hdx_members.add_url_rule(u'/members/<id>', view_func=members)
-hdx_members.add_url_rule(u'/member_delete/<id>', view_func=member_delete)
+hdx_members.add_url_rule(u'/member_delete/<id>', view_func=member_delete, methods=[u'POST'])
 hdx_members.add_url_rule(u'/member_new/<id>', view_func=member_new, methods=[u'POST'])
 hdx_members.add_url_rule(u'/bulk_member_new/<id>', view_func=bulk_member_new, methods=[u'POST', u'GET'])
