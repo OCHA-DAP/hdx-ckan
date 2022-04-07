bd1a2f2ec00bc1c3aec25f51d57b4f76b7ffe34a
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2022-04-07 20:54:13 +0300

    Enabling cached download test in PY3 + update dependency s3filestore to bugfixed version

--- ckanext-hdx_package/ckanext/hdx_package/tests/test_aws/test_cached_download.py
+++ ckanext-hdx_package/ckanext/hdx_package/tests/test_aws/test_cached_download.py
@@ -1,103 +1,103 @@
 import mock
 import six
-if not six.PY3:
-    import ckan.plugins.toolkit as tk
-    import ckanext.s3filestore.view as s3_view
 
-    from ckanext.s3filestore.helpers import CachedDownloadStorageHelper
+import ckan.plugins.toolkit as tk
+import ckanext.s3filestore.view as s3_view
 
-    config = tk.config
-    _get_action = tk.get_action
+from ckanext.s3filestore.helpers import CachedDownloadStorageHelper
 
-    mocked_helper = None  # type: MockedHelper
+config = tk.config
+_get_action = tk.get_action
 
-    def create_storage_helper(*args, **kwargs):
-        global mocked_helper
-        mocked_helper = MockedHelper(*args, **kwargs)
-        return mocked_helper
+mocked_helper = None  # type: MockedHelper
 
-
-    class TestCachedDownload(object):
-
-        download_folder = '/tmp'
-
-        @classmethod
-        def setup_class(cls):
-            cls._change_config(config)
-
-        @classmethod
-        def _change_config(cls, test_config):
-            test_config['hdx.download_with_cache.datasets'] = 'test-dataset1,test-dataset2'
-            test_config['hdx.download_with_cache.folder'] = cls.download_folder
-
-        def test_should_use_download_with_cache(self):
-            assert s3_view._should_use_download_with_cache('test-dataset1')
-            assert s3_view._should_use_download_with_cache('test-dataset2')
-            assert not s3_view._should_use_download_with_cache('test-dataset3')
-
-        @mock.patch('ckanext.s3filestore.view._prepare_cached_response')
-        @mock.patch('ckanext.s3filestore.view._get_cached_download_storage_helper', wraps=create_storage_helper)
-        def test_resource_download_with_cache(self, storage_helper_func_mock, chached_response_mock):
-            chached_response_mock.return_value = None
-
-            url1 = 'http://fake1.url.org'
-            filename1 = 'filename1'
-            resource_dict1 = {'id': 'id1', 'last_modified': '2018-02-14T16:01:58.230736'}
-
-            # controller1 = MockedS3Controller()
-            s3_view._resource_download_with_cache(url1, filename1, resource_dict1)
-            mocked_helper1 = mocked_helper
-            assert mocked_helper1.file_downloaded
-            assert mocked_helper1.folder_created
-
-            # controller1_2 = MockedS3Controller()
-            s3_view._resource_download_with_cache(url1, filename1, resource_dict1)
-            mocked_helper1 = mocked_helper
-            assert not mocked_helper1.file_downloaded
-            assert not mocked_helper1.folder_created
-
-            url2 = 'http://fake2.url.org'
-            filename2 = 'filename1'  # same filename as above
-            resource_dict2 = {'id': 'id1', 'last_modified': '2019-02-14T16:01:58.230736'} # same resource that was updated
-
-            # controller2 = MockedS3Controller()
-            s3_view._resource_download_with_cache(url2, filename2, resource_dict2)
-            mocked_helper1 = mocked_helper
-            assert mocked_helper1.file_downloaded
-            assert not mocked_helper1.folder_created
+def create_storage_helper(*args, **kwargs):
+    global mocked_helper
+    mocked_helper = MockedHelper(*args, **kwargs)
+    return mocked_helper
 
 
-    # class MockedS3Controller(S3Controller):
-    #     def __init__(self):
-    #         super(MockedS3Controller, self).__init__()
-    #
-    #     def _prepare_cached_response(self, full_file_path):
-    #         return None
-    #
-    #     def _get_cached_download_storage_helper(self, filename, url):
-    #         self.mocked_helper = MockedHelper(filename, url)
-    #         return self.mocked_helper
+class TestCachedDownload(object):
+
+    download_folder = '/tmp'
+
+    @classmethod
+    def setup_class(cls):
+        cls._change_config(config)
+
+    @classmethod
+    def _change_config(cls, test_config):
+        test_config['hdx.download_with_cache.datasets'] = 'test-dataset1,test-dataset2'
+        test_config['hdx.download_with_cache.folder'] = cls.download_folder
+
+    def test_should_use_download_with_cache(self):
+        assert s3_view._should_use_download_with_cache('test-dataset1')
+        assert s3_view._should_use_download_with_cache('test-dataset2')
+        assert not s3_view._should_use_download_with_cache('test-dataset3')
+
+    @mock.patch('ckanext.s3filestore.view._prepare_cached_response')
+    @mock.patch('ckanext.s3filestore.view._get_cached_download_storage_helper', wraps=create_storage_helper)
+    def test_resource_download_with_cache(self, storage_helper_func_mock, chached_response_mock):
+        chached_response_mock.return_value = None
+
+        url1 = 'http://fake1.url.org'
+        filename1 = 'filename1'
+        resource_dict1 = {'id': 'id1', 'last_modified': '2018-02-14T16:01:58.230736'}
+
+        # controller1 = MockedS3Controller()
+        s3_view._resource_download_with_cache(url1, filename1, resource_dict1)
+        mocked_helper1 = mocked_helper
+        assert mocked_helper1.file_downloaded
+        assert mocked_helper1.folder_created
+
+        # controller1_2 = MockedS3Controller()
+        s3_view._resource_download_with_cache(url1, filename1, resource_dict1)
+        mocked_helper1 = mocked_helper
+        assert not mocked_helper1.file_downloaded
+        assert not mocked_helper1.folder_created
+
+        url2 = 'http://fake2.url.org'
+        filename2 = 'filename1'  # same filename as above
+        resource_dict2 = {'id': 'id1', 'last_modified': '2019-02-14T16:01:58.230736'} # same resource that was updated
+
+        # controller2 = MockedS3Controller()
+        s3_view._resource_download_with_cache(url2, filename2, resource_dict2)
+        mocked_helper1 = mocked_helper
+        assert mocked_helper1.file_downloaded
+        assert not mocked_helper1.folder_created
 
 
-    class MockedHelper(CachedDownloadStorageHelper):
-        existing_folders = set()
-        existing_files = set()
+# class MockedS3Controller(S3Controller):
+#     def __init__(self):
+#         super(MockedS3Controller, self).__init__()
+#
+#     def _prepare_cached_response(self, full_file_path):
+#         return None
+#
+#     def _get_cached_download_storage_helper(self, filename, url):
+#         self.mocked_helper = MockedHelper(filename, url)
+#         return self.mocked_helper
 
-        def __init__(self, filename, url):
-            super(MockedHelper, self).__init__(filename, url)
-            self.file_downloaded = False
-            self.folder_created = False
 
-        def _folder_exists(self):
-            return self.folder in self.existing_folders
+class MockedHelper(CachedDownloadStorageHelper):
+    existing_folders = set()
+    existing_files = set()
 
-        def _file_exists(self):
-            return self.full_file_path in self.existing_files
+    def __init__(self, filename, url):
+        super(MockedHelper, self).__init__(filename, url)
+        self.file_downloaded = False
+        self.folder_created = False
 
-        def _create_folder(self):
-            self.folder_created = True
-            self.existing_folders.add(self.folder)
+    def _folder_exists(self):
+        return self.folder in self.existing_folders
 
-        def _download_file(self):
-            self.file_downloaded = True
-            self.existing_files.add(self.full_file_path)
+    def _file_exists(self):
+        return self.full_file_path in self.existing_files
+
+    def _create_folder(self):
+        self.folder_created = True
+        self.existing_folders.add(self.folder)
+
+    def _download_file(self):
+        self.file_downloaded = True
+        self.existing_files.add(self.full_file_path)
