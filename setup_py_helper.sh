6b973de6f9871c8600db7847b4182ca89d662089
Serban Teodorescu <teodorescu.serban@gmail.com>
2022-11-21 19:53:10 +0000

    [HDX-8585] decomission py2

--- setup_py_helper.sh
+++ setup_py_helper.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-mv -f requirements-py2.txt requirements-py2-tmp.txt
-grep -v "github" requirements-py2-tmp.txt >> requirements-py2.txt
 mv -f requirements.txt requirements-tmp.txt
 grep -v "github" requirements-tmp.txt >> requirements.txt
 python setup.py develop
-mv -f requirements-py2-tmp.txt requirements-py2.txt
 mv -f requirements-tmp.txt requirements.txt
 
-for p in $(ls -d ckanext-*); do cd /srv/ckan/$p; python setup.py develop; done
-cd /srv/ckan
-
+plugins="hdx_service_checker sitemap ytp-request hdx_hxl_preview hdx_dataviz hdx_pages hdx_theme hdx_package hdx_search hdx_org_group hdx_users hdx_user_extra"
+for p in $plugins; do cd /srv/ckan/ckanext-$p || exit; python setup.py develop; done
+cd /srv/ckan || exit
