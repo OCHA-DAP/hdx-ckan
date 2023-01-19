6da5f1e8784863cf45ff40c0671023d48bcfec01
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2023-01-19 14:39:34 +0200

    HDX-8502 email notifications

--- ckanext-hdx_users/ckanext/hdx_users/actions/update.py
+++ ckanext-hdx_users/ckanext/hdx_users/actions/update.py
@@ -3,11 +3,15 @@ import ckan.plugins.toolkit as tk
 import ckanext.hdx_users.helpers.mailer as hdx_mailer
 import ckanext.hdx_users.helpers.reset_password as reset_password
 import ckanext.hdx_users.model as umodel
+from ckanext.hdx_users.helpers.token_expiration_helper import find_expiring_api_tokens, send_emails_for_expiring_tokens
 
 NotFound = tk.ObjectNotFound
 _check_access = tk.check_access
+_get_or_bust = tk.get_or_bust
+ValidationError = tk.ValidationError
 get_action = tk.get_action
 config = tk.config
+
 
 def token_update(context, data_dict):
     token = data_dict.get('token')
@@ -78,3 +82,39 @@ def hdx_send_reset_link(context, data_dict):
         hdx_mailer.mail_recipient([{'display_name': user_fullname, 'email': recipient_mail}], subject,
                                   email_data, footer=recipient_mail,
                                   snippet='email/content/password_reset.html')
+
+
+def notify_users_about_api_token_expiration(context, data_dict):
+    '''
+    :param days_in_advance: how many days in advance we should look for expiring api tokens
+    :type days_in_advance: int
+    :param expires_on_specified_day: if True then we're looking only for api tokens that will expire on the
+        specified day. Otherwise, we look for tokens that expire from now till the specified day.
+    :type expires_on_specified_day: bool
+    :return: number of api tokens that will expire
+    '''
+    _check_access('notify_users_about_api_token_expiration', context, {})
+    days_in_advance, expires_on_specified_day = __extract_token_expiration_params(data_dict)
+
+    model = context['model']
+    session = context['model'].Session
+
+    token_info_list, period_start_string, period_end_string = \
+        find_expiring_api_tokens(model, session, days_in_advance, expires_on_specified_day)
+    number_of_emails = send_emails_for_expiring_tokens(token_info_list)
+    return {
+        'start_date': period_start_string,
+        'end_date': period_end_string,
+        'emails_sent': number_of_emails,
+    }
+
+
+def __extract_token_expiration_params(data_dict):
+    days_in_advance = _get_or_bust(data_dict, 'days_in_advance')
+    try:
+        days_in_advance = int(days_in_advance)
+        expires_on_specified_day = data_dict.get('expires_on_specified_day') == 'true' \
+                                   or data_dict.get('expires_on_specified_day') is True
+    except ValueError:
+        raise ValidationError('Limit must be a positive integer')
+    return days_in_advance, expires_on_specified_day
