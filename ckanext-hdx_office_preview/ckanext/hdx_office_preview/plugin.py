5d67d104f9c6b0bc4e3247696c381b2fdd13ef5b
Alexandru Artimon <aalecs@gmail.com>
2023-11-18 13:53:43 +0000

    HDX-9173 - Replace data proxy -
    Change name of extension to replace recline views
    Add CSV rendering via Google Docs Preview

--- ckanext-hdx_office_preview/ckanext/hdx_office_preview/plugin.py
+++ ckanext-hdx_office_preview/ckanext/hdx_office_preview/plugin.py
@@ -1,6 +1,7 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import logging
+from ckan.common import config
 
 log = logging.getLogger(__name__)
 class Hdx_Office_PreviewPlugin(plugins.SingletonPlugin):
@@ -11,30 +12,35 @@ class Hdx_Office_PreviewPlugin(plugins.SingletonPlugin):
     def info(self):
         # log.info('Setting up Office Preview!')
         return {
-            'name': 'hdx_office_preview',
-            'title': 'Office Preview',
+            'name': 'recline_view',
+            'title': 'Data Explorer',
             'filterable': True,
             'icon': 'file-text-o',
             'requires_datastore': False,
             'iframed': True,
             'preview_enabled': True,
             'preview_is_framed': True,
-            'default_title': 'Office Preview'
+            'default_title': 'Data Explorer'
         }
 
     def can_view(self, data_dict):
         resource = data_dict['resource']
         format_lower = resource['format'].lower()
         # log.info('Checking if resource format is supported: %s', format_lower)
-        return format_lower in ['xls', 'xlsx', 'doc', 'docx', 'ppt', 'pptx', 'odt', 'ods', 'odp']
+        return format_lower in ['xls', 'xlsx', 'doc', 'docx', 'ppt', 'pptx', 'odt', 'ods', 'odp', 'csv']
 
     def setup_template_variables(self, context, data_dict):
         resource = data_dict['resource']
         format_lower = resource['format'].lower()
         url = resource['url']
-        return {'res': resource, 'format_lower': format_lower, 'url': url}
+        domain = config.get('ckanext.security.domain', '')
+        return {'res': resource, 'format_lower': format_lower, 'url': url, 'domain': domain}
 
     def view_template(self, context, data_dict):
+        resource = data_dict['resource']
+        format_lower = resource['format'].lower()
+        if (format_lower == 'csv'):
+            return 'hdx_google_preview_view.html'
         return 'hdx_office_preview_view.html'
     # IConfigurer
 
