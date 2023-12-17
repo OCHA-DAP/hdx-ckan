0b9dd25e7bbab0c5b89055aa7048ffb88d4b8081
Alexandru Artimon <aalecs@gmail.com>
2023-12-17 10:12:19 +0000

    HDX-9173
    add hxl proxy config

--- ckanext-hdx_office_preview/ckanext/hdx_office_preview/public/hdx_csv_preview.js
+++ ckanext-hdx_office_preview/ckanext/hdx_office_preview/public/hdx_csv_preview.js
@@ -1,7 +1,8 @@
 $(document).ready(() => {
     let resourceUrl = $("#resource-url").html();
+    let hxlProxyUrl = $("#hxl-proxy-url").html();
     console.log(`Loading URL: ${resourceUrl}`);
-    let previewUrl =`https://proxy.hxlstandard.org//api/data-preview.json?rows=20&sheet=0&url=${resourceUrl}`;
+    let previewUrl =`${hxlProxyUrl}/api/data-preview.json?rows=20&sheet=0&url=${resourceUrl}`;
     $.get(previewUrl).done((response) => {
         console.log(response.slice(1));
         let columns = response.slice(0,1)[0].map((item) => { return {"title": item}; });
