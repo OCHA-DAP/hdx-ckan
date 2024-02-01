30fa8df679178adc0c4e9c9e41f6f67d892a1433
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2024-02-01 16:57:49 +0200

    HDX-9524 texts for user-info page

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/constants.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/constants.py
@@ -1,10 +1,13 @@
-from ckanext.hdx_search.helpers.constants import DEFAULT_SORTING
 import six
+
+from ckanext.hdx_search.helpers.constants import DEFAULT_SORTING
+from ckanext.hdx_theme.helpers.ui_constants import CONSTANTS as UI_CONSTANTS
 
 CONSTANTS = {
     'DEFAULT_SORTING': DEFAULT_SORTING,
     'MOBILE_MEDIA': 'only screen and (max-width: 640px)',
     'PY3': six.PY3,
+    'UI_CONSTANTS': UI_CONSTANTS,
 }
 
 
---
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/__init__.py
@@ -0,0 +1,4 @@
+from ckanext.hdx_theme.helpers.ui_constants.onboarding.user_info import CONSTANTS as USER_INFO_CONSTANTS
+CONSTANTS = {
+    'USER_INFO': USER_INFO_CONSTANTS,
+}
