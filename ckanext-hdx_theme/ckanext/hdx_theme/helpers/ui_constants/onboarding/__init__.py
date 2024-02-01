86ab4982dd7046399ddd6be7a1074539645b7d66
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2024-02-01 17:37:36 +0200

    HDX-9524 texts for value-proposition page

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/__init__.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/__init__.py
@@ -1,4 +1,8 @@
 from ckanext.hdx_theme.helpers.ui_constants.onboarding.user_info import CONSTANTS as USER_INFO_CONSTANTS
+from ckanext.hdx_theme.helpers.ui_constants.onboarding.value_proposition import CONSTANTS as VALUE_PROPOSITION_CONSTANTS
+
+
 CONSTANTS = {
     'USER_INFO': USER_INFO_CONSTANTS,
+    'VALUE_PROPOSITION': VALUE_PROPOSITION_CONSTANTS,
 }
