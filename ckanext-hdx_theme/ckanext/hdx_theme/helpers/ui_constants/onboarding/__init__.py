0e293f54838e995ebf3424abfcb1bf484940a01f
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2024-02-01 18:23:45 +0200

    HDX-9524 texts for account_created and verify_email pages

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/__init__.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/__init__.py
@@ -1,8 +1,12 @@
 from ckanext.hdx_theme.helpers.ui_constants.onboarding.user_info import CONSTANTS as USER_INFO_CONSTANTS
 from ckanext.hdx_theme.helpers.ui_constants.onboarding.value_proposition import CONSTANTS as VALUE_PROPOSITION_CONSTANTS
+from ckanext.hdx_theme.helpers.ui_constants.onboarding.verify_email import CONSTANTS as VERIFY_EMAIL_CONSTANTS
+from ckanext.hdx_theme.helpers.ui_constants.onboarding.account_created import CONSTANTS as ACCOUNT_CREATED_CONSTANTS
 
 
 CONSTANTS = {
     'USER_INFO': USER_INFO_CONSTANTS,
     'VALUE_PROPOSITION': VALUE_PROPOSITION_CONSTANTS,
+    'VERIFY_EMAIL': VERIFY_EMAIL_CONSTANTS,
+    'ACCOUNT_CREATED': ACCOUNT_CREATED_CONSTANTS,
 }
