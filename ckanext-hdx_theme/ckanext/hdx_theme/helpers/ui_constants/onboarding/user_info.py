e789ba9345be9cc61d857e9321992d08df65337b
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2024-02-01 16:57:49 +0200

    HDX-9524 texts for user-info page

---
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/user_info.py
@@ -0,0 +1,25 @@
+CONSTANTS = {
+    'STEPS_1': '''Personal details''',
+    'STEPS_2': '''Verify email''',
+    'STEPS_3': '''Account created''',
+    'PAGE_TITLE': '''Please provide the following information''',
+    'MANDATORY_HELP': '''* indicates mandatory fields''',
+
+    'INPUT_FULLNAME_LABEL': '''What is your name?''',
+    'INPUT_FULLNAME_PLACEHOLDER': '''Your name''',
+    'INPUT_EMAIL_LABEL': '''What is your email address?''',
+    'INPUT_EMAIL_PLACEHOLDER': '''Your email address''',
+    'INPUT_EMAIL2_LABEL': '''Re-enter your email address''',
+    'INPUT_EMAIL2_PLACEHOLDER': '''Re-enter your email address''',
+    'INPUT_USERNAME_LABEL': '''Choose a username''',
+    'INPUT_USERNAME_PLACEHOLDER': '''Your username''',
+    'INPUT_USERNAME_HELP': '''Username should be 2 and 100 characters in length, containing only lowercase alphanumeric characters, - and _''',
+    'INPUT_PASSWORD_LABEL': '''Choose a password''',
+    'INPUT_PASSWORD_PLACEHOLDER': '''Your password''',
+    'INPUT_PASSWORD_HELP': '''The password must be a minimum of 10 characters in length and should consist of characters from a minimum of three out of the following four character sets: uppercase letters, lowercase letters, numbers, and punctuation.''',
+    'INPUT_PASSWORD2_LABEL': '''Re-enter a password''',
+    'INPUT_PASSWORD2_PLACEHOLDER': '''Re-enter your password''',
+
+    'CHECKBOX_TERMS_OF_SERVICE': '''I have read and accepted the <a href="{0}" title="HDX Terms of Service">HDX Terms of Service</a>''',
+    'CHECKBOX_NEWSLETTER': '''Do you want to receive email updates from the Centre for Humanitarian Data about HDX features and events?''',
+}
