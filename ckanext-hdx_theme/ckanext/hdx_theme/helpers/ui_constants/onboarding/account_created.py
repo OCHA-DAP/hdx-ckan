07336f95e08bfa6e4f32c20e72e1b5601b0172b6
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2024-02-01 18:23:45 +0200

    HDX-9524 texts for account_created and verify_email pages

---
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/account_created.py
@@ -0,0 +1,11 @@
+from ckanext.hdx_theme.helpers.ui_constants.onboarding.user_info import CONSTANTS as USER_INFO_CONSTANTS
+
+CONSTANTS = {
+    'STEPS_1': USER_INFO_CONSTANTS['STEPS_1'],
+    'STEPS_2': USER_INFO_CONSTANTS['STEPS_2'],
+    'STEPS_3': USER_INFO_CONSTANTS['STEPS_3'],
+
+    'PAGE_TITLE': '''{0}, your HDX account has been created.''',
+
+    'BUTTON_SUBMIT': USER_INFO_CONSTANTS['BUTTON_SUBMIT'],
+}
