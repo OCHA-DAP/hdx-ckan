9290f25abec34bb678e008b6e64802912f7088a9
Dan Mihaila <danmihaila@gmail.com>
2024-02-06 10:51:58 +0200

    HDX-9412 onboarding user-info - adding security validators

--- ckanext-hdx_users/ckanext/hdx_users/logic/schema.py
+++ ckanext-hdx_users/ckanext/hdx_users/logic/schema.py
@@ -6,7 +6,7 @@ Created on July 2nd, 2015
 
 from typing import cast
 import ckan.plugins.toolkit as tk
-from ckan.logic.schema import validator_args
+from ckan.logic.schema import validator_args, user_new_form_schema
 from ckan.types import (Validator, Schema)
 
 unicode_safe = tk.get_validator('unicode_safe')
@@ -36,29 +36,39 @@ def register_details_user_schema(ignore_missing, not_empty, name_validator, user
     return schema
 
 
-@validator_args
-def onboarding_create_user_schema(ignore_missing: Validator, unicode_safe: Validator,
-                                  name_validator: Validator, user_name_validator: Validator,
-                                  user_password_validator: Validator, user_both_passwords_entered: Validator,
-                                  user_passwords_match: Validator,
-                                  ignore_not_sysadmin: Validator,
-                                  not_empty: Validator, strip_value: Validator,
-                                  user_email_validator: Validator, user_about_validator: Validator,
-                                  ignore: Validator, boolean_validator: Validator):
-    return cast(Schema, {
-        'id': [ignore_missing, unicode_safe],
-        'name': [not_empty, name_validator, user_name_validator, unicode_safe],
-        'fullname': [ignore_missing, unicode_safe],
-        'password1': [unicode_safe, user_both_passwords_entered, user_password_validator, user_passwords_match],
-        'password2': [unicode_safe],
-        'password_hash': [ignore_missing, ignore_not_sysadmin, unicode_safe],
-        'email': [not_empty, strip_value, user_email_validator, unicode_safe],
-        # 'about': [ignore_missing, user_about_validator, unicode_safe],
-        'created': [ignore],
-        'sysadmin': [ignore_missing, ignore_not_sysadmin],
-        'reset_key': [ignore],
-        'activity_streams_email_notifications': [ignore_missing,
-                                                 boolean_validator],
-        'state': [ignore_missing],
-    })
+# @validator_args
+# def onboarding_create_user_schema(ignore_missing: Validator, unicode_safe: Validator,
+#                                   name_validator: Validator, user_name_validator: Validator,
+#                                   user_password_validator: Validator, user_both_passwords_entered: Validator,
+#                                   user_passwords_match: Validator,
+#                                   ignore_not_sysadmin: Validator,
+#                                   not_empty: Validator, strip_value: Validator,
+#                                   user_email_validator: Validator, user_about_validator: Validator,
+#                                   ignore: Validator, boolean_validator: Validator):
+#     return cast(Schema, {
+#         'id': [ignore_missing, unicode_safe],
+#         'name': [not_empty, name_validator, user_name_validator, unicode_safe],
+#         'fullname': [ignore_missing, unicode_safe],
+#         'password1': [unicode_safe, user_both_passwords_entered, user_password_validator, user_passwords_match],
+#         'password2': [unicode_safe],
+#         'password_hash': [ignore_missing, ignore_not_sysadmin, unicode_safe],
+#         'email': [not_empty, strip_value, user_email_validator, unicode_safe],
+#         # 'about': [ignore_missing, user_about_validator, unicode_safe],
+#         'created': [ignore],
+#         'sysadmin': [ignore_missing, ignore_not_sysadmin],
+#         'reset_key': [ignore],
+#         'activity_streams_email_notifications': [ignore_missing,
+#                                                  boolean_validator],
+#         'state': [ignore_missing],
+#     })
 
+
+@validator_args
+def onboarding_user_new_form_schema(unicode_safe: Validator,
+                                    not_empty: Validator, strip_value: Validator,
+                                    user_email_validator: Validator, ignore_missing: Validator):
+    schema = user_new_form_schema()
+    schema['email'] = [not_empty, strip_value, user_email_validator, unicode_safe]
+    schema['state'] = [ignore_missing]
+
+    return schema
