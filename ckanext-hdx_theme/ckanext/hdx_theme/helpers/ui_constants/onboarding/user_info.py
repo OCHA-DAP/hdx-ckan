80fd6b6801ed22bba0e882ebb087f3ba72cf1051
Catalin <catalinflorentinion@gmail.com>
2024-03-15 14:15:17 +0200

    HDX-9667 update email updates text

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/user_info.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/user_info.py
@@ -27,7 +27,7 @@ CONSTANTS = {
 
     'CHECKBOX_TERMS_OF_SERVICE': '''I have read and accepted the <a href="{0}" target="_blank" title="HDX Terms of Service">HDX Terms of Service</a>''',
     'CHECKBOX_TERMS_OF_SERVICE_ERROR': '''Please accept the terms of service to continue''',
-    'CHECKBOX_NEWSLETTER': '''Do you want to receive email updates from the Centre for Humanitarian Data about HDX features and events?''',
+    'CHECKBOX_NEWSLETTER': '''I want to receive email updates from the Centre for Humanitarian Data about HDX features and events''',
 
     'BUTTON_SUBMIT': '''Next''',
     'BUTTON_CANCEL': '''Cancel''',
