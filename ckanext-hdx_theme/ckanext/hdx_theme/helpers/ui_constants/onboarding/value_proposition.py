cf42ea53b0efb8a302c8bd63dff9e896a43e636b
yumi-endo <65605692+yumi-endo@users.noreply.github.com>
2024-03-25 19:19:19 -0400

    HDX-9689 - adding '.' at the end of sentences

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/value_proposition.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/value_proposition.py
@@ -2,13 +2,13 @@ CONSTANTS = {
     'PAGE_TITLE': '''HDX account options''',
 
     'ROW_SEARCH_DOWNLOAD_LABEL': '''Search and download data''',
-    'ROW_SEARCH_DOWNLOAD_HELP': '''Explore and use over 20,000 datasets on HDX''',
+    'ROW_SEARCH_DOWNLOAD_HELP': '''Explore and use over 20,000 datasets on HDX.''',
     'ROW_CONTACT_CONTRIBUTOR_LABEL': '''Contact the Contributor''',
-    'ROW_CONTACT_CONTRIBUTOR_HELP': '''Ask questions about data through the ‘Contact the Contributor’ form''',
+    'ROW_CONTACT_CONTRIBUTOR_HELP': '''Ask questions about data through the ‘Contact the Contributor’ form.''',
     'ROW_HDX_CONNECT_LABEL': '''HDX Connect request''',
-    'ROW_HDX_CONNECT_HELP': '''Request access to sensitive data through the ‘HDX Connect’ form''',
+    'ROW_HDX_CONNECT_HELP': '''Request access to sensitive data through the ‘HDX Connect’ form.''',
     'ROW_ADD_DATA_LABEL': '''Add data''',
-    'ROW_ADD_DATA_HELP': '''Data is shared on HDX through approved organisations. Request to join or create an organisation to share data''',
+    'ROW_ADD_DATA_HELP': '''Data is shared on HDX through approved organisations. Request to join or create an organisation to share data.''',
 
     'COLUMN_NO_ACCOUNT_LABEL': '''No account needed''',
     'COLUMN_NO_ACCOUNT_BUTTON': '''SEARCH DATA''',
