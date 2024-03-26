1827cbf9468febfd9bcd5b24f5f881285f344418
yumi-endo <65605692+yumi-endo@users.noreply.github.com>
2024-03-26 11:40:48 -0400

    HDX-9689-Update verify_email.py

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/verify_email.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/verify_email.py
@@ -6,6 +6,6 @@ CONSTANTS = {
     'STEPS_3': USER_INFO_CONSTANTS['STEPS_3'],
 
     'PAGE_TITLE': '''Verify your email address''',
-    'BODY_MAIN_TEXT': '''We have sent an email to {0} so that you can verify your email address.''',
-    'BODY_MAIN_TEXT_WITHOUT_EMAIL': '''We have sent an email so that you can verify your email address.'''
+    'BODY_MAIN_TEXT': '''We have sent an email to {0} so that you can verify your email address. If you don’t see the email, please check your spam or junk folder.''',
+    'BODY_MAIN_TEXT_WITHOUT_EMAIL': '''We have sent an email so that you can verify your email address. If you don’t see the email, please check your spam or junk folder.'''
 }
