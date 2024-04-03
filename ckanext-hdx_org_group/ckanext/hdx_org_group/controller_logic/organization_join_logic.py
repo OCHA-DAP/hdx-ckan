a7cf334c7db1d597b7f4eccdae0955c0bdde27f7
Dan Mihaila <danmihaila@gmail.com>
2024-04-03 16:53:15 +0300

    HDX-9435 req org join backend

---
+++ ckanext-hdx_org_group/ckanext/hdx_org_group/controller_logic/organization_join_logic.py
@@ -0,0 +1,44 @@
+import logging
+from typing import cast
+
+import ckan.model as model
+import ckan.plugins.toolkit as tk
+from ckan.common import current_user
+from ckan.types import Context
+
+get_action = tk.get_action
+check_access = tk.check_access
+config = tk.config
+h = tk.h
+NotAuthorized = tk.NotAuthorized
+
+log = logging.getLogger(__name__)
+
+
+class OrgJoinLogic(object):
+    def __init__(self, context: Context):
+        self.active_org_list = []
+        self.inactive_org_list = []
+        self.all_org_list = []
+        self.context = context
+
+
+    def read(self):
+        self._fetch_org_list_metadata()
+        return self
+
+    def _fetch_org_list_metadata(self):
+        '''
+        :returns: populated self object
+        :rtype:
+        '''
+
+        self.all_org_list = get_action(u'cached_organization_list')(self.context, {})
+
+        if len(self.all_org_list) > 0:
+            for o in self.all_org_list:
+                if not o.get('closed_organization'):
+                    self.active_org_list.append({o.get('id'): o.get('display_name')})
+                else:
+                    self.inactive_org_list.append({o.get('id'): o.get('display_name')})
+
