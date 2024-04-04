1008093c0034fc59f16ddd5280099173df682d00
Catalin <catalinflorentinion@gmail.com>
2024-04-04 18:51:18 +0300

    HDX-9436 confirm org choice page

---
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/confirm_organisation_choice.py
@@ -0,0 +1,18 @@
+from ckanext.hdx_theme.helpers.ui_constants.onboarding.request_join_organisation import \
+    CONSTANTS as REQUEST_JOIN_ORGANISATION_CONSTANTS
+
+CONSTANTS = {
+    'STEPS_1': REQUEST_JOIN_ORGANISATION_CONSTANTS['STEPS_1'],
+    'STEPS_2': REQUEST_JOIN_ORGANISATION_CONSTANTS['STEPS_2'],
+    'STEPS_3': REQUEST_JOIN_ORGANISATION_CONSTANTS['STEPS_3'],
+
+    'PAGE_TITLE': '''Please confirm your choice of organisation''',
+
+    'BODY_CHOICE_TEXT': '''You have chosen to request to join:''',
+    'BODY_ABOUT_TEXT': '''About this organisation:''',
+    'BODY_MEMBER_SINCE_TEXT': '''Member since {0}''',
+
+    'BUTTON_SUBMIT': '''Yes - Choose this organisation''',
+    'BUTTON_CHOOSE_DIFFERENT_ORG': '''No - Choose a different organisation''',
+    'BUTTON_CREATE_NEW_ORG': '''create a new one''',
+}
