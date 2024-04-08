40d00b9d6d397295be3ae33f429a8a328dd16685
Catalin <catalinflorentinion@gmail.com>
2024-04-08 23:57:02 +0300

    HDX-9447 build page 11 - request submitted

---
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/completed_organisation_create.py
@@ -0,0 +1,13 @@
+from ckanext.hdx_theme.helpers.ui_constants.onboarding.request_create_organisation import \
+    CONSTANTS as REQUEST_CREATE_ORGANISATION_CONSTANTS
+
+CONSTANTS = {
+    'STEPS_1': REQUEST_CREATE_ORGANISATION_CONSTANTS['STEPS_1'],
+    'STEPS_2': REQUEST_CREATE_ORGANISATION_CONSTANTS['STEPS_2'],
+
+    'PAGE_TITLE': '''Request submitted''',
+
+    'BODY_MAIN_TEXT': '''Thank you for requesting to create an organisation on HDX. We will be in touch within two working days with a response.''',
+
+    'BUTTON_SUBMIT': '''Go to HDX Homepage''',
+}
