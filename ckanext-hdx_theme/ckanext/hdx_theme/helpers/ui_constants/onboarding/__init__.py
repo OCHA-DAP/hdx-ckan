21237a582cfafd45ff0f9f70996460868c7a133e
Catalin <catalinflorentinion@gmail.com>
2024-04-08 23:47:04 +0300

    HDX-9447 build page 11 - request submitted

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/__init__.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/__init__.py
@@ -6,6 +6,7 @@ from ckanext.hdx_theme.helpers.ui_constants.onboarding.request_join_organisation
 from ckanext.hdx_theme.helpers.ui_constants.onboarding.confirm_organisation_choice import CONSTANTS as CONFIRM_ORGANISATION_CHOICE_CONSTANTS
 from ckanext.hdx_theme.helpers.ui_constants.onboarding.reason_request_organisation import CONSTANTS as REASON_REQUEST_ORGANISATION_CONSTANTS
 from ckanext.hdx_theme.helpers.ui_constants.onboarding.completed_organisation_request import CONSTANTS as COMPLETED_ORGANISATION_REQUEST_CONSTANTS
+from ckanext.hdx_theme.helpers.ui_constants.onboarding.request_create_organisation import CONSTANTS as REQUEST_CREATE_ORGANISATION_CONSTANTS
 from ckanext.hdx_theme.helpers.ui_constants.onboarding.email_subjects import CONSTANTS as EMAIL_SUBJECTS_CONSTANTS
 
 
@@ -18,5 +19,6 @@ CONSTANTS = {
     'CONFIRM_ORGANISATION_CHOICE': CONFIRM_ORGANISATION_CHOICE_CONSTANTS,
     'REASON_REQUEST_ORGANISATION': REASON_REQUEST_ORGANISATION_CONSTANTS,
     'COMPLETED_ORGANISATION_REQUEST': COMPLETED_ORGANISATION_REQUEST_CONSTANTS,
+    'REQUEST_CREATE_ORGANISATION': REQUEST_CREATE_ORGANISATION_CONSTANTS,
     'EMAIL_SUBJECTS': EMAIL_SUBJECTS_CONSTANTS,
 }
