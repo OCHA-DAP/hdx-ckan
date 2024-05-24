824aabdfb5f5d4a7d909c5cf229a72fe311b16ce
Catalin <catalinflorentinion@gmail.com>
2024-05-24 17:30:02 +0300

    HDX-9839 Signals landing page

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/landing_pages/hapi.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/landing_pages/hapi.py
@@ -1,18 +1,11 @@
 CONSTANTS = {
-    'HERO_SECTION_TITLE': '''The HDX Humanitarian API (HAPI) is a way to access standardised indicators from multiple
-    sources to automate workflows and visualisations''',
-    'HERO_SECTION_DESCRIPTION': '''HAPI is in beta phase, and we are seeking feedback. To share your thoughts or join
-    our Slack channel, send an email to <a href="{0}" title="Contact us">hdx@un.org</a>.''',
+    'HERO_SECTION_TITLE': '''The HDX Humanitarian API (HAPI) is a way to access standardised indicators from multiple sources to automate workflows and visualisations''',
+    'HERO_SECTION_DESCRIPTION': '''HAPI is in beta phase, and we are seeking feedback. To share your thoughts or join our Slack channel, send an email to <a href="{0}" title="Contact us">hdx@un.org</a>.''',
     'HERO_SECTION_LABEL': '''HDX HAPI Beta''',
 
     'DATA_COVERAGE_SECTION_TITLE': '''Data Coverage''',
-    'DATA_COVERAGE_SECTION_DESCRIPTION': '''Our initial coverage aligns with the data included in the <a href="{0}"
-    title="HDX Data Grids">HDX Data Grids</a>, which places the most important crisis data into six categories and 20
-    sub-categories. Our beta version of HAPI covers 57 indicators across 22 countries with a <a href="{1}"
-    title="Humanitarian Response Plan">Humanitarian Response Plan</a>.''',
-    'DATA_COVERAGE_SECTION_PARAGRAPH': '''Refer to the <a href="{0}" target="_blank" title="HAPI - The Humanitarian
-    API">documentation</a> for the latest coverage. <a href="{1}" title="Contact us">Contact us</a> to request
-    additional indicators in future versions of HAPI. ''',
+    'DATA_COVERAGE_SECTION_DESCRIPTION': '''Our initial coverage aligns with the data included in the <a href="{0}" title="HDX Data Grids">HDX Data Grids</a>, which places the most important crisis data into six categories and 20 sub-categories. Our beta version of HAPI covers 57 indicators across 22 countries with a <a href="{1}" title="Humanitarian Response Plan">Humanitarian Response Plan</a>.''',
+    'DATA_COVERAGE_SECTION_PARAGRAPH': '''Refer to the <a href="{0}" target="_blank" title="HAPI - The Humanitarian API">documentation</a> for the latest coverage. <a href="{1}" title="Contact us">Contact us</a> to request additional indicators in future versions of HAPI.''',
 
     'BE_INSPIRED_SECTION_TITLE': '''Be Inspired''',
     'BE_INSPIRED_SECTION_DESCRIPTION': '''Take a look at visualisations and code examples''',
@@ -21,18 +14,19 @@ CONSTANTS = {
     'BE_INSPIRED_CARD_TEXT_POWER_BI': '''A tutorial to demonstrate how easy it is to bring in data.''',
     'BE_INSPIRED_CARD_BUTTON_POWER_BI': '''Learn more''',
     'BE_INSPIRED_CARD_BUTTON_LINK_POWER_BI': '''#''',
+
     'BE_INSPIRED_CARD_TITLE_API': '''API Sandbox''',
     'BE_INSPIRED_CARD_TEXT_API': '''A sandbox environment to help construct queries and get a data response.''',
     'BE_INSPIRED_CARD_BUTTON_API': '''Learn more''',
     'BE_INSPIRED_CARD_BUTTON_LINK_API': '''https://stage.hapi-humdata-org.ahconu.org/docs#/Locations%20and%20Administrative%20Divisions/get_locations_api_v1_metadata_location_get''',
+
     'BE_INSPIRED_CARD_TITLE_VISUALIZATION': '''Visualisation''',
-    'BE_INSPIRED_CARD_TEXT_VISUALIZATION': '''A dashboard with a selection of key figures, charts and a map for all
-    countries in HAPI.''',
+    'BE_INSPIRED_CARD_TEXT_VISUALIZATION': '''A dashboard with a selection of key figures, charts and a map for all countries in HAPI.''',
     'BE_INSPIRED_CARD_BUTTON_VISUALIZATION': '''Learn more''',
     'BE_INSPIRED_CARD_BUTTON_LINK_VISUALIZATION': '''#''',
+
     'BE_INSPIRED_CARD_TITLE_CHATBOT': '''Chatbot''',
-    'BE_INSPIRED_CARD_TEXT_CHATBOT': '''An early stage AI chatbot developed with DataKind to ask questions about the
-    data.''',
+    'BE_INSPIRED_CARD_TEXT_CHATBOT': '''An early stage AI chatbot developed with DataKind to ask questions about the data.''',
     'BE_INSPIRED_CARD_BUTTON_CHATBOT': '''Learn more''',
     'BE_INSPIRED_CARD_BUTTON_LINK_CHATBOT': '''#''',
 
--- ckanext-hdx_theme/ckanext/hdx_theme/views/landing_pages.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/views/landing_pages.py
@@ -3,7 +3,10 @@ import flask
 import ckan.plugins.toolkit as tk
 import ckanext.hdx_theme.helpers.faq_wordpress as fw
 from ckan.common import config
-from ckanext.hdx_theme.helpers.ui_constants.landing_pages.hapi import DATA_COVERAGE_CONSTANTS, SECTIONS_CONSTANTS
+from ckanext.hdx_theme.helpers.ui_constants.landing_pages.hapi import \
+    DATA_COVERAGE_CONSTANTS as HAPI_DATA_COVERAGE_CONSTANTS, SECTIONS_CONSTANTS as HAPI_SECTIONS_CONSTANTS
+from ckanext.hdx_theme.helpers.ui_constants.landing_pages.signals import \
+    DATA_COVERAGE_CONSTANTS as SIGNALS_DATA_COVERAGE_CONSTANTS, SECTIONS_CONSTANTS as SIGNALS_SECTIONS_CONSTANTS
 
 abort = tk.abort
 g = tk.g
@@ -21,18 +24,18 @@ def hapi():
     data = fw.faq_for_category(wp_category_terms)
 
     partners = [
-        ('acled', 'ACLED'),
         ('european_comission', 'European Commission'),
+        ('acled', 'ACLED'),
         ('inform', 'INFORM'),
         ('ipc', 'IPC'),
-        ('ocha', 'OCHA'),
+        ('unfpa', 'UNFPA'),
         ('ophi', 'OPHI'),
         ('unchr', 'UNHCR'),
-        ('unfpa', 'UNFPA'),
+        ('ocha', 'OCHA'),
         ('wfp', 'WFP')
     ]
-    sections = SECTIONS_CONSTANTS
-    data_coverage = DATA_COVERAGE_CONSTANTS
+    sections = HAPI_SECTIONS_CONSTANTS
+    data_coverage = HAPI_DATA_COVERAGE_CONSTANTS
 
     template_data = {
         'partners': partners,
@@ -45,7 +48,26 @@ def hapi():
 
 
 def signals():
-    template_data = {}
+    wp_category_terms = config.get('hdx.wordpress.category.signals')
+    data = fw.faq_for_category(wp_category_terms)
+
+    partners = [
+        ('european_comission', 'European Commission'),
+        ('acled', 'ACLED'),
+        ('ipc', 'IPC'),
+        ('idmc', 'IDMC'),
+        ('wfp', 'WFP')
+    ]
+
+    sections = SIGNALS_SECTIONS_CONSTANTS
+    data_coverage = SIGNALS_DATA_COVERAGE_CONSTANTS
+
+    template_data = {
+        'partners': partners,
+        'sections': sections,
+        'data_coverage': data_coverage,
+        'faq_data': data['faq_data'],
+    }
 
     return render('landing_pages/signals.html', extra_vars=template_data)
 
