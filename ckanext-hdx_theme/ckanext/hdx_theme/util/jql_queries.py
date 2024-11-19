eee6bbcc91c8ba5adf61fd8f58ec6d0932b78294
Dan Mihaila <danmihaila@gmail.com>
2024-11-19 15:44:04 +0200

    HDX-10191 include common header in jql queries

--- ckanext-hdx_theme/ckanext/hdx_theme/util/jql_queries.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/util/jql_queries.py
@@ -303,6 +303,8 @@ function main() {{
       {{event: "page view"}}
     ]
   }})
+  ''' + COMMON_FILTER + \
+  '''
   .filter(event => event.properties["org id"] == "{}")
   .groupByUser(["name", mixpanel.numeric_bucket('time', mixpanel.monthly_time_buckets)], mixpanel.reducer.count()) // unique
   .map(function(r){{
