8c42b73f6f04c1911d13d61c3bcf0ced24adb1c7
Dan Mihaila <danmihaila@gmail.com>
2025-03-05 14:35:12 +0200

    HDX-10466 data completeness

--- ckanext-hdx_package/ckanext/hdx_package/helpers/extras.py
+++ ckanext-hdx_package/ckanext/hdx_package/helpers/extras.py
@@ -2,7 +2,8 @@ import ckan.plugins.toolkit as tk
 
 
 ALLOWED_EXTRAS = {
-    'review_date': None,
+    'dataset_date': None,
+    # 'review_date': None,
     'data_update_frequency': None,
     'is_requestdata_type': [tk.get_validator('boolean_validator')],
 }
