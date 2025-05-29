dc24fbce753e81b383523ca8617ceec040514928
Catalin <catalinflorentinion@gmail.com>
2025-05-29 14:02:24 +0300

    HDX-10665 implement notifications hub UI

--- ckanext-hdx_users/ckanext/hdx_users/views/dashboard.py
+++ ckanext-hdx_users/ckanext/hdx_users/views/dashboard.py
@@ -2,6 +2,8 @@ import logging
 
 from flask import Blueprint
 
+from ckan.common import current_user
+
 import ckan.lib.base as base
 import ckan.plugins.toolkit as tk
 from ckanext.hdx_users.controller_logic.dashboard_dataset_logic import DashboardDatasetLogic
@@ -41,4 +43,19 @@ def datasets():
         })
 
 
+def notifications():
+    """
+    Notifications tab
+    """
+
+    if not current_user.is_authenticated:
+        h.flash_error(_(u'Not authorized to see this page'))
+        return h.redirect_to(u'home.index')
+
+    return render('user/dashboard_notifications.html', extra_vars={
+        'user_dict': current_user,
+    })
+
+
 hdx_user_dashboard.add_url_rule(u'/datasets', view_func=datasets)
+hdx_user_dashboard.add_url_rule(u'/notifications', view_func=notifications)
