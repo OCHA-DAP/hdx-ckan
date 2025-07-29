6981903305f065e806e54ad352b2dacee2b7bf83
Alexandru Artimon <aalecs@gmail.com>
2025-07-29 08:39:43 +0000

    HDX-11011 - Limit the number of Search sub-pages we are letting SEO bots index
    Refactor logic from the snipped used to set canonical into a helper function

--- ckanext-hdx_theme/ckanext/hdx_theme/plugin.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/plugin.py
@@ -196,6 +196,7 @@ class HDXThemePlugin(plugins.SingletonPlugin):
             'get_last_modifier_user': hdx_helpers.get_last_modifier_user,
             'get_filtered_params_list': hdx_helpers.get_filtered_params_list,
             'filter_search_seo_category_keys': hdx_helpers.filter_search_seo_category_keys,
+            'generate_canonical_link': hdx_helpers.generate_canonical_link,
             # 'get_last_revision_package': hdx_helpers.get_last_revision_package,
             # 'get_last_revision_group': hdx_helpers.get_last_revision_group,
             'get_group_followers': hdx_helpers.get_group_followers,
