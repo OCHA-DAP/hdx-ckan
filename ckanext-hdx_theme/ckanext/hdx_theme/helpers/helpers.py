bc4c8db63b8e332d72b9b83912bc208d93dd973e
Catalin <catalinflorentinion@gmail.com>
2025-08-01 12:53:40 +0300

    add debugging logs

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/helpers.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/helpers.py
@@ -1039,6 +1039,8 @@ def hdx_supports_notifications(object_type: ObjectType, object_id: str,
                                object_dict: Optional[dict[str, Any]] = None) -> bool:
     supports_notifications = False
 
+    log.info(f'Checking if notifications are supported for object_id: {object_id} of type: {object_type}')
+
     if object_id:
         if object_type in (ObjectType.DATASET, ObjectType.GROUP, ObjectType.ORGANIZATION, ObjectType.CRISIS):
             supports_notifications = _check_notifications_enabled_for_object(object_type, object_id, object_dict)
@@ -1053,24 +1055,31 @@ def hdx_supports_notifications(object_type: ObjectType, object_id: str,
 def _check_notifications_enabled_for_object(object_type: ObjectType, object_id: str,
                                             object_dict: Optional[dict[str, Any]] = None) -> bool:
     object_identifier = f"{object_type}_{object_id}"
+    log.info(f'Checking notifications for object: {object_identifier}')
 
     if object_type == ObjectType.DATASET and object_dict:
         is_hdx_connect = str(object_dict.get('is_requestdata_type', False)).lower() == 'true'
         is_private = str(object_dict.get('private', False)).lower() == 'true'
         is_archived = str(object_dict.get('archived', False)).lower() == 'true'
         is_update_frequency_never = object_dict.get('data_update_frequency', '') == UPDATE_FREQ_NEVER
+
+        log.debug(f'Object properties - is_hdx_connect: {is_hdx_connect}, is_private: {is_private}, '
+                  f'is_archived: {is_archived}, is_update_frequency_never: {is_update_frequency_never}')
+
         if is_hdx_connect or is_private or is_archived or is_update_frequency_never:
+            log.info(f'Notifications disabled for object: {object_identifier}')
             return False
 
     if config.get('hdx.notifications.enabled_objects_csv'):
-        log.info(f'using cache for enabled objects for notifications')
+        log.info('Using cache for enabled objects for notifications')
         objects_with_notifications = cached_objects_with_notifications()
         return object_identifier in objects_with_notifications
     elif config.get('hdx.notifications.disabled_objects_csv'):
-        log.info(f'using cache for disabled objects for notifications')
+        log.info('Using cache for disabled objects for notifications')
         objects_without_notifications = cached_objects_without_notifications()
         return object_identifier not in objects_without_notifications
 
+    log.info(f'No notifications configuration found for object: {object_identifier}')
     return False
 
 
