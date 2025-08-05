bcbca9c07796c8db19948691ce209ded86229da9
Alexandru Artimon <aalecs@gmail.com>
2025-08-05 13:23:32 +0000

    HDX-11023 - Implement text changes

--- ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/value_proposition.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/helpers/ui_constants/onboarding/value_proposition.py
@@ -1,28 +1,24 @@
 CONSTANTS = {
     'PAGE_TITLE': '''Choose the way you want to use the Humanitarian Data Exchange''',
 
-    'FEATURE_PUBLIC_DATASETS': '''Access all public humanitarian datasets on HDX''',
-    'FEATURE_VIZ_TOOLS': '''Use data visualisation tools''',
+    'FEATURE_PUBLIC_DATASETS': '''Access all public datasets''',
+    'FEATURE_VIZ_TOOLS': '''Explore data visualizations''',
     'FEATURE_REGISTRATION_REQUIRED': '''No registration required''',
 
-    'FEATURE_CONTACT_CONTRIBUTOR': '''Ask questions to data contributors''',
-    'FEATURE_REQUEST_DATA': '''Request access to sensitive datasets''',
-    'FEATURE_MANAGE_NOTIFICATIONS': '''COMING SOON - Manage notifications about data updates''',
+    'FEATURE_CONTACT_CONTRIBUTOR': '''Contact data contributors with questions''',
+    'FEATURE_REQUEST_DATA': '''Become a member of an organization''',
+    'FEATURE_MANAGE_NOTIFICATIONS': '''Manage your user dashboard''',
 
-    'STEP_CREATE_ACCOUNT': '''First <a href="{0}" id="{1}" data-module="{2}" data-value-proposition-page="{3}">create an individual HDX account</a>''',
-    'STEP_APPLICATION_REVIEWED': '''Your application will be reviewed''',
-    'STEP_SAMPLE_DATA': '''Provide sample data for verification''',
-
-    'COLUMN_NO_ACCOUNT_LABEL': '''Search and download data''',
+    'COLUMN_NO_ACCOUNT_LABEL': '''Search and download open data''',
     'COLUMN_NO_ACCOUNT_HELP': '''Explore and use thousands of datasets on HDX without an account''',
     'COLUMN_NO_ACCOUNT_BUTTON': '''SEARCH DATA''',
 
-    'COLUMN_INDIVIDUAL_ACCOUNT_LABEL': '''Create an HDX Account''',
+    'COLUMN_INDIVIDUAL_ACCOUNT_LABEL': '''Create an HDX account''',
     'COLUMN_INDIVIDUAL_ACCOUNT_HELP': '''Access additional features with an individual HDX account''',
     'COLUMN_INDIVIDUAL_ACCOUNT_BUTTON': '''SIGN UP''',
 
     'COLUMN_INDIVIDUAL_ACCOUNT_WITH_ORG_LABEL': '''Share data''',
-    'COLUMN_INDIVIDUAL_ACCOUNT_WITH_ORG_HELP': '''For organisations that need to publish data on HDX''',
+    'COLUMN_INDIVIDUAL_ACCOUNT_WITH_ORG_HELP': '''If your organization would like to share data on HDX, please send an email to <a href="mailto:hdx@un.org">hdx@un.org</a> with some details and we will respond within 24 hours. ''',
     'COLUMN_INDIVIDUAL_ACCOUNT_WITH_ORG_INFO': '''If you already have an individual account, <a href="{0}" id="{1}" title="log in">log in</a> and click "Add Data"''',
 
     'ALREADY_HAVE_ACCOUNT_HELP': '''Already have an account? <a href="{0}" title="Login" id="{1}">Login</a>''',
