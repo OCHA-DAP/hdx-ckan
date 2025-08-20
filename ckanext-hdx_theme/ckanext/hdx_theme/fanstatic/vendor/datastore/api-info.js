3622d3e54d4593d53c6e703c7eeafda9f3ae3a7e
Catalin <catalinflorentinion@gmail.com>
2025-08-20 16:22:52 +0300

    HDX-10971 add api button, remove download icon

--- ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/vendor/datastore/api-info.js
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/vendor/datastore/api-info.js
@@ -63,8 +63,11 @@ this.ckan.module('api-info', function (jQuery) {
 
       this.loadTemplate().done(function (html) {
         module.modal = jQuery(html);
-        module.modal.find('.modal-header :header').append('<button class="btn-close float-end" data-bs-dismiss="modal"></button>');
+        module.modal.find('.modal-header').append('<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>');
+        module.modal.find('.btn-close').attr("aria-label", module._('Close'));
         module.modal.modal().appendTo(sandbox.body);
+        module.modal.modal('show');
+        // hljs.highlightAll();
       });
     },
 
