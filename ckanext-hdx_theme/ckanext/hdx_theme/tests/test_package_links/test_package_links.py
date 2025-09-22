22d20d37db74dbec3f833a5d2af27b2cd176225f
Catalin <catalinflorentinion@gmail.com>
2025-09-22 20:17:46 +0300

    HDX-11115 add authentication checks and tests for quick links and package links

--- ckanext-hdx_theme/ckanext/hdx_theme/tests/test_package_links/test_package_links.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/tests/test_package_links/test_package_links.py
@@ -12,7 +12,6 @@ def test_package_links_actions_end_to_end():
     context: Context = {'model': model, 'session': model.Session, 'user': sysadmin['name']}
     get_action = tk.get_action
 
-    # Show should be empty at start
     shown = get_action('hdx_package_links_settings_show')(context, {})
     assert isinstance(shown, list)
     assert shown == []
@@ -33,7 +32,6 @@ def test_package_links_actions_end_to_end():
     assert isinstance(result, str)
     assert result == '[{"title": "My Package Link", "url": "/dataset/my-dataset", "order": 2, "newTab": false, "label": "Datasets", "package_list": "pkg-1,pkg-2", "buttonText": "Open"}]'
 
-    # Show should return the newly created item
     shown_after_create = get_action('hdx_package_links_settings_show')(context, {})
     assert len(shown_after_create) == 1
     created = shown_after_create[0]
@@ -45,7 +43,6 @@ def test_package_links_actions_end_to_end():
     assert created['package_list'] == 'pkg-1,pkg-2'
     assert created['buttonText'] == 'Open'
 
-    # Delete the item by updating the config to an empty list
     delete_result = get_action('hdx_package_links_settings_update')(
         context,
         {'hdx.package_links.config': []},
@@ -53,7 +50,6 @@ def test_package_links_actions_end_to_end():
     assert isinstance(delete_result, str)
     assert delete_result == '[]'
 
-    # Show should be empty again
     shown_after_delete = get_action('hdx_package_links_settings_show')(context, {})
     assert shown_after_delete == []
 
@@ -63,11 +59,9 @@ def test_package_links_guest_user_cannot_update():
     get_action = tk.get_action
     guest_context: Context = {'model': model, 'session': model.Session}
 
-    # Guest can view
     shown = get_action('hdx_package_links_settings_show')(guest_context, {})
     assert isinstance(shown, list)
 
-    # Guest cannot update
     with pytest.raises(tk.NotAuthorized):
         get_action('hdx_package_links_settings_update')(
             guest_context,
@@ -81,13 +75,90 @@ def test_package_links_regular_user_cannot_update():
     user_context: Context = {'model': model, 'session': model.Session, 'user': user['name']}
     get_action = tk.get_action
 
-    # Regular user can view
     shown = get_action('hdx_package_links_settings_show')(user_context, {})
     assert isinstance(shown, list)
 
-    # Regular user cannot update
     with pytest.raises(tk.NotAuthorized):
         get_action('hdx_package_links_settings_update')(
             user_context,
             {'hdx.package_links.config': []},
         )
+
+
+@pytest.mark.usefixtures('hdx_clean_db', 'clean_index', 'with_request_context')
+def test_package_links_endpoints_require_auth_token(app):
+    token = factories.APIToken(expires_in=2, unit=60 * 60)['token']
+
+    res_show_guest = app.get('/ckan-admin/package-links/show', expect_errors=True)
+    assert res_show_guest.status_code == 403
+
+    res_update_guest = app.post('/ckan-admin/package-links/update', params={
+        'title': 'X',
+        'url': '/dataset/x',
+        'order': '1',
+        'label': 'Label',
+        'package_list': 'pkg-a',
+    }, expect_errors=True)
+    assert res_update_guest.status_code == 403
+
+    res_delete_guest = app.post('/ckan-admin/package-links/delete/some-id', expect_errors=True)
+    assert res_delete_guest.status_code == 403
+
+    user_headers = {'Authorization': token}
+
+    res_show_user = app.get('/ckan-admin/package-links/show', headers=user_headers, expect_errors=True)
+    assert res_show_user.status_code == 403
+
+    res_update_user = app.post('/ckan-admin/package-links/update', params={
+        'title': 'Y',
+        'url': '/dataset/y',
+        'order': '2',
+        'label': 'Label',
+        'package_list': 'pkg-b',
+    }, headers=user_headers, expect_errors=True)
+    assert res_update_user.status_code == 403
+
+    res_delete_user = app.post('/ckan-admin/package-links/delete/some-id', headers=user_headers, expect_errors=True)
+    assert res_delete_user.status_code == 403
+
+
+@pytest.mark.usefixtures('hdx_clean_db')
+def test_package_links_update_and_delete_via_http_with_token(app):
+    sysadmin = factories.Sysadmin()
+    api_token = factories.APIToken(user=sysadmin['id'], expires_in=2, unit=60 * 60)
+    admin_headers = {'Authorization': api_token['token']}
+
+    get_action = tk.get_action
+    context: Context = {'model': model, 'session': model.Session, 'user': sysadmin['name']}
+
+    assert get_action('hdx_package_links_settings_show')(context, {}) == []
+
+    create_resp = app.post('/ckan-admin/package-links/update', params={
+        'title': 'My Package Link',
+        'url': '/dataset/my-dataset',
+        'order': '2',
+        'newTab': 'false',
+        'label': 'Datasets',
+        'package_list': 'pkg-1, pkg-2',
+        'buttonText': 'Open',
+    }, headers=admin_headers)
+    assert create_resp.status_code == 200
+    assert 'My Package Link' in create_resp.text
+
+    shown = get_action('hdx_package_links_settings_show')(context, {})
+    assert isinstance(shown, list) and len(shown) == 1
+    created = shown[0]
+    assert created['title'] == 'My Package Link'
+    assert created['url'] == '/dataset/my-dataset'
+    assert created['order'] == 2
+    assert created['newTab'] is False
+    assert created['label'] == 'Datasets'
+    assert created['package_list'] == 'pkg-1,pkg-2'
+    assert created.get('buttonText') == 'Open'
+    assert created.get('id')
+
+    del_resp = app.post(f"/ckan-admin/package-links/delete/{created['id']}", headers=admin_headers)
+    assert del_resp.status_code == 200
+
+    shown_after_delete = get_action('hdx_package_links_settings_show')(context, {})
+    assert shown_after_delete == []
--- ckanext-hdx_theme/ckanext/hdx_theme/tests/test_quick_links/test_quick_links.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/tests/test_quick_links/test_quick_links.py
@@ -13,7 +13,6 @@ def test_quick_links_actions_end_to_end():
 
     get_action = tk.get_action
 
-    # Show should be empty at start
     shown = get_action('hdx_quick_links_settings_show')(context, {})
     assert isinstance(shown, list)
     assert shown == []
@@ -33,7 +32,6 @@ def test_quick_links_actions_end_to_end():
     assert isinstance(result, str)
     assert result == '[{"title": "My Link", "url": "/my-link", "order": 1, "newTab": true, "archived": false, "buttonText": "Go"}]'
 
-    # Show should return the newly created item with a generated id
     shown_after_create = get_action('hdx_quick_links_settings_show')(context, {})
     assert len(shown_after_create) == 1
     created = shown_after_create[0]
@@ -43,7 +41,6 @@ def test_quick_links_actions_end_to_end():
     assert created['archived'] is False
     assert created['buttonText'] == 'Go'
 
-    # Delete the item by updating the config to an empty list
     delete_result = get_action('hdx_quick_links_settings_update')(
         context,
         {'hdx.quick_links.config': []},
@@ -51,7 +48,6 @@ def test_quick_links_actions_end_to_end():
     assert isinstance(delete_result, str)
     assert delete_result == '[]'
 
-    # Show should be empty again
     shown_after_delete = get_action('hdx_quick_links_settings_show')(context, {})
     assert shown_after_delete == []
 
@@ -61,11 +57,9 @@ def test_quick_links_guest_user_cannot_update():
     get_action = tk.get_action
     guest_context: Context = {'model': model, 'session': model.Session}
 
-    # Guest can view
     shown = get_action('hdx_quick_links_settings_show')(guest_context, {})
     assert isinstance(shown, list)
 
-    # Guest cannot update
     with pytest.raises(tk.NotAuthorized):
         get_action('hdx_quick_links_settings_update')(
             guest_context,
@@ -79,13 +73,84 @@ def test_quick_links_regular_user_cannot_update():
     user_context: Context = {'model': model, 'session': model.Session, 'user': user['name']}
     get_action = tk.get_action
 
-    # Regular user can view
     shown = get_action('hdx_quick_links_settings_show')(user_context, {})
     assert isinstance(shown, list)
 
-    # Regular user cannot update
     with pytest.raises(tk.NotAuthorized):
         get_action('hdx_quick_links_settings_update')(
             user_context,
             {'hdx.quick_links.config': []},
         )
+
+
+@pytest.mark.usefixtures('hdx_clean_db', 'clean_index', 'with_request_context')
+def test_quick_links_endpoints_require_auth_token(app):
+    token = factories.APIToken(expires_in=2, unit=60 * 60)['token']
+
+    res_show_guest = app.get('/ckan-admin/quick-links/show', expect_errors=True)
+    assert res_show_guest.status_code == 403
+
+    res_update_guest = app.post('/ckan-admin/quick-links/update', params={
+        'title': 'X',
+        'url': '/x',
+        'order': '1',
+    }, expect_errors=True)
+    assert res_update_guest.status_code == 403
+
+    res_delete_guest = app.post('/ckan-admin/quick-links/delete/some-id', expect_errors=True)
+    assert res_delete_guest.status_code == 403
+
+    user_headers = {'Authorization': token}
+
+    res_show_user = app.get('/ckan-admin/quick-links/show', headers=user_headers, expect_errors=True)
+    assert res_show_user.status_code == 403
+
+    res_update_user = app.post('/ckan-admin/quick-links/update', params={
+        'title': 'Y',
+        'url': '/y',
+        'order': '2',
+    }, headers=user_headers, expect_errors=True)
+    assert res_update_user.status_code == 403
+
+    res_delete_user = app.post('/ckan-admin/quick-links/delete/some-id', headers=user_headers, expect_errors=True)
+    assert res_delete_user.status_code == 403
+
+
+@pytest.mark.usefixtures('hdx_clean_db')
+def test_quick_links_update_and_delete_via_http_with_token(app):
+    sysadmin = factories.Sysadmin()
+    api_token = factories.APIToken(user=sysadmin['id'], expires_in=2, unit=60 * 60)
+    admin_headers = {'Authorization': api_token['token']}
+
+    get_action = tk.get_action
+    context: Context = {'model': model, 'session': model.Session, 'user': sysadmin['name']}
+
+    assert get_action('hdx_quick_links_settings_show')(context, {}) == []
+
+    create_resp = app.post('/ckan-admin/quick-links/update', params={
+        'title': 'My Link',
+        'url': '/my-link',
+        'order': '1',
+        'newTab': 'true',
+        'archived': 'false',
+        'buttonText': 'Go',
+    }, headers=admin_headers)
+    assert create_resp.status_code == 200
+    assert 'My Link' in create_resp.text
+
+    shown = get_action('hdx_quick_links_settings_show')(context, {})
+    assert isinstance(shown, list) and len(shown) == 1
+    created = shown[0]
+    assert created['title'] == 'My Link'
+    assert created['url'] == '/my-link'
+    assert created['order'] == 1
+    assert created['newTab'] is True
+    assert created['archived'] is False
+    assert created.get('buttonText') == 'Go'
+    assert created.get('id')
+
+    del_resp = app.post(f"/ckan-admin/quick-links/delete/{created['id']}", headers=admin_headers)
+    assert del_resp.status_code == 200
+
+    shown_after_delete = get_action('hdx_quick_links_settings_show')(context, {})
+    assert shown_after_delete == []
