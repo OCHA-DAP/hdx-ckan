ad0a922e01e6a305239be4284ae4abcc22912a78
Dan Mihaila <danmihaila@gmail.com>
2025-09-25 17:27:04 +0300

    HDX-11146 datastore libraries sync

--- setup_py_helper.sh
+++ setup_py_helper.sh
@@ -2,9 +2,16 @@
 
 mv -f requirements.txt requirements-tmp.txt
 grep -v "github" requirements-tmp.txt >> requirements.txt
-python setup.py develop
+#pip install --upgrade build "setuptools<81"
+pip install --editable .
 mv -f requirements-tmp.txt requirements.txt
 
 plugins="hdx_service_checker sitemap ytp-request hdx_hxl_preview hdx_dataviz hdx_pages hdx_theme hdx_package hdx_search hdx_org_group hdx_users hdx_user_extra hdx_office_preview"
-for p in $plugins; do cd /srv/ckan/ckanext-$p || exit; python setup.py develop; done
+for p in $plugins; do
+    cd /srv/ckan/ckanext-$p || exit
+#    pip install --upgrade build
+    pip install --editable .
+done
 cd /srv/ckan || exit
+
+
