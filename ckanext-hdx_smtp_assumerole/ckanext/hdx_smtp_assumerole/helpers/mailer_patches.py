50f9e73120095a543560cb3763090ff234f90762
Serban Teodorescu <teodorescu.serban@gmail.com>
2025-12-02 14:31:04 +0200

    HDX-10015 Add display name to From and Reply-To

--- ckanext-hdx_smtp_assumerole/ckanext/hdx_smtp_assumerole/helpers/mailer_patches.py
+++ ckanext-hdx_smtp_assumerole/ckanext/hdx_smtp_assumerole/helpers/mailer_patches.py
@@ -49,7 +49,13 @@ def _build_mime_message_with_attachments(
     :return: MIMEMultipart message object
     """
     msg = MIMEMultipart()
-    msg['From'] = mail_from
+
+    # Build From header with display name
+    # Use ckan.site_title or default to "Humanitarian Data Exchange (HDX)"
+    site_title = tk.config.get('ckan.site_title', 'Humanitarian Data Exchange (HDX)')
+    msg['From'] = f'"{site_title}" <{mail_from}>'
+    msg['Reply-To'] = f'"{site_title}" <{mail_from}>'
+
     msg['Subject'] = subject
 
     # Add To header with display name
@@ -61,7 +67,7 @@ def _build_mime_message_with_attachments(
     # Add custom headers
     if headers:
         for key, value in headers.items():
-            if key not in ['From', 'To', 'Subject'] and value:
+            if key not in ['From', 'To', 'Subject', 'Reply-To'] and value:
                 msg[key] = value
 
     # Add body
