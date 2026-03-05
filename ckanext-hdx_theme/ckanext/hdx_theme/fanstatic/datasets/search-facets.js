d93c69a43e5b58e7555b889635a3db3e14030f1d
Catalin <catalinflorentinion@gmail.com>
2026-03-05 14:14:39 +0200

    HDX-11413 add dataset-search-facets script and update facet HTML for improved interaction handling

---
+++ ckanext-hdx_theme/ckanext/hdx_theme/fanstatic/datasets/search-facets.js
@@ -0,0 +1,24 @@
+$(document).on('click', '.facet[data-filter-url]', function (e) {
+  if ($(e.target).closest('a').length) return;
+
+  e.preventDefault();
+  e.stopPropagation();
+
+  var url = $(this).data('filter-url');
+  if (url) window.location.href = url;
+});
+
+$(document).on('click', '.facet-option-explanation', function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+});
+
+$(document).on('click', '.parent-facet', function (e) {
+  if (this.disabled) return;
+
+  e.preventDefault();
+  e.stopPropagation();
+
+  var url = $(this).closest('label.facet[data-filter-url]').data('filter-url');
+  if (url) window.location.href = url;
+});
