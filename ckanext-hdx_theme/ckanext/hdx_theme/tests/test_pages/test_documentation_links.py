14acd0efeb04c846d0c93a357d4d0683d7fd3dba
Catalin <catalinflorentinion@gmail.com>
2026-06-08 15:57:48 +0300

    update capitalization in test assertions for documentation links

--- ckanext-hdx_theme/ckanext/hdx_theme/tests/test_pages/test_documentation_links.py
+++ ckanext-hdx_theme/ckanext/hdx_theme/tests/test_pages/test_documentation_links.py
@@ -19,7 +19,7 @@ def test_documentation_links_rendered(app):
     assert f'href="{DOCUMENTATION_LINKS["QA_PROCESS"]}"' in response.body
     assert f'href="{DOCUMENTATION_LINKS["RESOURCES_FOR_DEVELOPERS"]}"' in response.body
     assert f'href="{DOCUMENTATION_LINKS["DATA_LICENSES"]}"' in response.body
-    assert 'TERMS OF SERVICE' in response.body
-    assert 'QA PROCESS' in response.body
-    assert 'BUILD WITH HDX' in response.body
-    assert 'DATA LICENSES' in response.body
+    assert 'Terms of Service' in response.body
+    assert 'QA Process' in response.body
+    assert 'Build with HDX' in response.body
+    assert 'Data Licenses' in response.body
